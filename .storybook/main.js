1bbf1c479cbbf5c08efb91a2b971a63107b61248
Benoît Grélard <benoit.grelard@gmail.com>
2020-07-01 11:21:45 +0100

    Make storybook follow to package sources

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,27 @@
+const path = require('path');
+const { merge } = require('lodash');
+
 module.exports = {
   stories: ['../packages/**/*.stories.tsx'],
   addons: ['@storybook/preset-typescript'],
+
+  // we need to add aliases to webpack so it knows how to follow
+  // to the source of the packages rather than the built version (dist)
+  webpackFinal: async (config) =>
+    merge(config, {
+      resolve: {
+        alias: convertTsConfigPathsToWebpackAliases(),
+      },
+    }),
 };
+
+function convertTsConfigPathsToWebpackAliases() {
+  const rootDir = path.resolve(__dirname, '../');
+  const tsconfig = require('../tsconfig.json');
+  const tsconfigPaths = Object.entries(tsconfig.compilerOptions.paths);
+
+  return tsconfigPaths.reduce((aliases, [realPath, mappedPath]) => {
+    aliases[realPath] = path.join(rootDir, mappedPath[0]);
+    return aliases;
+  }, {});
+}
