8a4439d114a0ada478cfa847f3d4b2a8c181e056
Colm Tuite <colmtuite@Colms-iMac.local>
2020-07-05 10:11:48 +0100

    Add header to homepage.

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,11 +1,106 @@
 import React from 'react';
-import { Container, Box, Text, Flex, Grid, Divider, Badge, Heading, Button } from '@modulz/radix';
+import { Container, Box, Text, Flex, Link, Grid, Divider, Badge, Heading, Button } from '@modulz/radix';
 import { DatePicker } from '../components/DatePicker';
-import Link from 'next/link';
 
 export default function Home() {
   return (
     <Box>
+      <Box
+        as="header"
+        sx={{
+          mb: 8,
+          py: 3,
+          px: 6,
+        }}
+      >
+        <Flex
+          sx={{
+            justifyContent: 'space-between',
+          }}
+        >
+          <Box>Components</Box>
+          <Flex as="nav">
+            <Link href="/demo-page" variant="fade" sx={{ ml: 5 }}>
+              <Flex
+                sx={{
+                  alignItems: 'center',
+                }}
+              >
+                <Text as="span" size={3} sx={{ color: 'inherit', mr: 1 }} style={{ lineHeight: 1 }}>
+                  Github
+                </Text>
+                <svg
+                  width="15"
+                  height="15"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  style={{ verticalAlign: 'middle' }}
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </Flex>
+            </Link>
+            <Link href="/demo-page" variant="fade" sx={{ ml: 5 }}>
+              <Flex
+                sx={{
+                  alignItems: 'center',
+                }}
+              >
+                <Text as="span" size={3} sx={{ color: 'inherit', mr: 1 }} style={{ lineHeight: 1 }}>
+                  Twitter
+                </Text>
+                <svg
+                  width="15"
+                  height="15"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  style={{ verticalAlign: 'middle' }}
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </Flex>
+            </Link>
+            <Link href="/demo-page" variant="fade" sx={{ ml: 5 }}>
+              <Flex
+                sx={{
+                  alignItems: 'center',
+                }}
+              >
+                <Text as="span" size={3} sx={{ color: 'inherit', mr: 1 }} style={{ lineHeight: 1 }}>
+                  Spectrum
+                </Text>
+                <svg
+                  width="15"
+                  height="15"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  style={{ verticalAlign: 'middle' }}
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </Flex>
+            </Link>
+          </Flex>
+        </Flex>
+      </Box>
       <Box mt={6} mb={8}>
         <Container size={2}>
           <Heading size={5} mb={4} sx={{ textAlign: 'center', fontWeight: 500, letterSpacing: '-.052em' }}>
@@ -16,23 +111,70 @@ export default function Home() {
             An open-source component library for building accessible design systems.
           </Heading>
 
-          <Flex>
-            <Box>
+          <Flex
+            sx={{
+              py: 8,
+              justifyContent: 'center',
+            }}
+          >
+            <Box
+              sx={{
+                mr: 7,
+              }}
+            >
               <Link href="/demo-page">
-                <a>
-                  <Text as="span" size={5} sx={{ color: 'blue600', lineHeight: '1' }}>
+                <Flex
+                  sx={{
+                    alignItems: 'center',
+                  }}
+                >
+                  <Text as="span" size={5} sx={{ color: 'inherit', mr: 1 }} style={{ lineHeight: 1 }}>
                     Documentation
                   </Text>
-                </a>
+                  <svg
+                    width="15"
+                    height="15"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    style={{ verticalAlign: 'middle' }}
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </Flex>
               </Link>
             </Box>
             <Box>
               <Link href="/demo-page">
-                <a>
-                  <Text as="span" size={5} sx={{ color: 'blue600', lineHeight: '1' }}>
+                <Flex
+                  sx={{
+                    alignItems: 'center',
+                  }}
+                >
+                  <Text as="span" size={5} sx={{ color: 'inherit', mr: 1 }} style={{ lineHeight: 1 }}>
                     Components
                   </Text>
-                </a>
+                  <svg
+                    width="15"
+                    height="15"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    style={{ verticalAlign: 'middle' }}
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </Flex>
               </Link>
             </Box>
           </Flex>
