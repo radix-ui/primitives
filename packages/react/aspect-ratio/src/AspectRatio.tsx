5f8a4655b8242013d239406806a5ab618dfba521
Chance Strickland <hi@chancedigital.io>
2020-07-09 11:36:50 -0700

    aspect ratio

--- packages/react/aspect-ratio/src/AspectRatio.tsx
+++ packages/react/aspect-ratio/src/AspectRatio.tsx
@@ -2,49 +2,88 @@ import * as React from 'react';
 import { cssReset } from '@interop-ui/utils';
 import { forwardRef } from '@interop-ui/react-utils';
 
-const DEFAULT_TAG = 'div';
+const WRAPPER_DEFAULT_TAG = 'div';
 
-type AspectRatioDOMProps = React.ComponentPropsWithoutRef<typeof DEFAULT_TAG>;
+type AspectRatioDOMProps = React.ComponentProps<typeof WRAPPER_DEFAULT_TAG>;
 type AspectRatioOwnProps = { ratio?: string };
 type AspectRatioProps = AspectRatioDOMProps & AspectRatioOwnProps;
 
-const AspectRatio = forwardRef<typeof DEFAULT_TAG, AspectRatioProps>(function AspectRatio(
+const AspectRatioContainer = forwardRef<typeof WRAPPER_DEFAULT_TAG, AspectRatioProps>(
+  function AspectRatioContainer(props, forwardedRef) {
+    const { as: Comp = WRAPPER_DEFAULT_TAG, ratio = '1:1', style, ...aspectRatioProps } = props;
+
+    const [n1, n2] = ratio.split(':');
+    const paddingBottom = 100 / (Number(n1) / Number(n2));
+
+    return (
+      <Comp
+        {...aspectRatioProps}
+        ref={forwardedRef}
+        style={{
+          paddingBottom: `${paddingBottom}%`,
+          ...style,
+        }}
+      />
+    );
+  }
+);
+
+AspectRatioContainer.displayName = 'AspectRatio.Container';
+
+const INNER_DEFAULT_TAG = 'div';
+
+type AspectRatioInnerDOMProps = React.ComponentProps<typeof WRAPPER_DEFAULT_TAG>;
+type AspectRatioInnerOwnProps = {};
+type AspectRatioInnerProps = AspectRatioInnerDOMProps & AspectRatioInnerOwnProps;
+
+const AspectRatioInner = forwardRef<typeof INNER_DEFAULT_TAG, AspectRatioInnerProps>(
+  function AspectRatioInner(props, forwardedRef) {
+    const { as: Comp = INNER_DEFAULT_TAG, ...innerProps } = props;
+
+    return <Comp ref={forwardedRef} {...innerProps} />;
+  }
+);
+
+AspectRatioInner.displayName = 'AspectRatio.Inner';
+
+const AspectRatio = forwardRef<typeof WRAPPER_DEFAULT_TAG, AspectRatioProps>(function AspectRatio(
   props,
   forwardedRef
 ) {
-  const { as: Comp = DEFAULT_TAG, ratio = '1:1', children, style, ...aspectRatioProps } = props;
-
-  const [n1, n2] = ratio.split(':');
-  const paddingBottom = 100 / (Number(n1) / Number(n2));
+  const { children, ...aspectRatioProps } = props;
 
   return (
-    <Comp
-      {...aspectRatioProps}
-      ref={forwardedRef}
-      style={{
-        ...cssReset(DEFAULT_TAG),
-        position: 'relative',
-        width: '100%',
-        ...style,
-        paddingBottom: `${paddingBottom}%`,
-      }}
-    >
-      <div
-        style={{
-          position: 'absolute',
-          left: 0,
-          top: 0,
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        {children}
-      </div>
-    </Comp>
+    <AspectRatioContainer {...aspectRatioProps} ref={forwardedRef}>
+      <AspectRatioInner>{children}</AspectRatioInner>
+    </AspectRatioContainer>
   );
-});
+}) as IAspectRatio;
 
 AspectRatio.displayName = 'AspectRatio';
 
-export type { AspectRatioProps };
-export { AspectRatio };
+AspectRatio.Container = AspectRatioContainer;
+AspectRatio.Inner = AspectRatioInner;
+
+interface IAspectRatio extends React.ForwardRefExoticComponent<AspectRatioProps> {
+  Container: typeof AspectRatioContainer;
+  Inner: typeof AspectRatioInner;
+}
+
+const styles = {
+  container: {
+    ...cssReset(WRAPPER_DEFAULT_TAG),
+    position: 'relative',
+    width: '100%',
+  },
+  inner: {
+    ...cssReset(INNER_DEFAULT_TAG),
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: '100%',
+    height: '100%',
+  },
+};
+
+export { AspectRatio, styles };
+export type { AspectRatioProps, AspectRatioInnerProps };
