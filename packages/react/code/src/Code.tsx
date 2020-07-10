c5349761513c3bff92835aa18585cd13176db4c7
Chance Strickland <hi@chancedigital.io>
2020-07-10 14:45:27 -0700

    add code component

--- packages/react/code/src/Code.tsx
+++ packages/react/code/src/Code.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
+import { cssReset } from '@interop-ui/utils';
+import { forwardRef } from '@interop-ui/react-utils';
 
-type CodeDOMProps = React.ComponentProps<'div'>;
+const DEFAULT_TAG = 'code';
+
+type CodeDOMProps = React.ComponentPropsWithoutRef<typeof DEFAULT_TAG>;
 type CodeOwnProps = {};
 type CodeProps = CodeDOMProps & CodeOwnProps;
 
-const Code = React.forwardRef<HTMLDivElement, CodeProps>(function Code(props, forwardedRef) {
-  return <div ref={forwardedRef} />;
+const Code = forwardRef<typeof DEFAULT_TAG, CodeProps>(function Code(props, forwardedRef) {
+  const { as: Comp = DEFAULT_TAG, ...codeProps } = props;
+  return <Comp data-interop-part-code="" ref={forwardedRef} {...codeProps} />;
 });
 
 Code.displayName = 'Code';
 
-export { Code };
+const styles = {
+  code: {
+    ...cssReset(DEFAULT_TAG),
+    lineHeight: '1',
+  },
+};
+
+export { Code, styles };
 export type { CodeProps };
