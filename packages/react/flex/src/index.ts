bf9bc3540e327ee4803a72e9343db8a9905303ab
Chance Strickland <hi@chancedigital.io>
2020-07-10 16:07:44 -0700

    add grid

--- packages/react/flex/src/index.ts
+++ packages/react/flex/src/index.ts
@@ -1,1 +1,1 @@
-export { Flex } from './Flex';
+export * from './Flex';
--- packages/react/grid/src/Grid.tsx
+++ packages/react/grid/src/Grid.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
+import { cssReset, interopDataAttrObj } from '@interop-ui/utils';
+import { forwardRef } from '@interop-ui/react-utils';
 
-type GridDOMProps = React.ComponentProps<'div'>;
+const DEFAULT_TAG = 'div';
+
+type GridDOMProps = React.ComponentPropsWithoutRef<typeof DEFAULT_TAG>;
 type GridOwnProps = {};
 type GridProps = GridDOMProps & GridOwnProps;
 
-const Grid = React.forwardRef<HTMLDivElement, GridProps>(function Grid(props, forwardedRef) {
-  return <div ref={forwardedRef} />;
+const Grid = forwardRef<typeof DEFAULT_TAG, GridProps>(function Grid(props, forwardedRef) {
+  const { as: Comp = DEFAULT_TAG, ...gridProps } = props;
+  return <Comp {...interopDataAttrObj('Grid')} ref={forwardedRef} {...gridProps} />;
 });
 
 Grid.displayName = 'Grid';
 
-export { Grid };
+const styles = {
+  grid: {
+    ...cssReset(DEFAULT_TAG),
+    display: 'grid',
+  },
+};
+
+export { Grid, styles };
 export type { GridProps };
