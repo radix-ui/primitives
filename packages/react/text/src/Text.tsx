21139a400840b02b1da3526ac449808a43bf2548
Chance Strickland <hi@chancedigital.io>
2020-07-13 16:42:19 -0700

    add text

---
+++ packages/react/text/src/Text.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { cssReset, interopDataAttrObj } from '@interop-ui/utils';
+import { forwardRef } from '@interop-ui/react-utils';
+
+const DEFAULT_TAG = 'span';
+
+type TextDOMProps = React.ComponentPropsWithoutRef<typeof DEFAULT_TAG>;
+type TextOwnProps = {};
+type TextProps = TextDOMProps & TextOwnProps;
+
+const Text = forwardRef<typeof DEFAULT_TAG, TextProps>(function Text(props, forwardedRef) {
+  const { as: Comp = DEFAULT_TAG, ...textProps } = props;
+  return <Comp {...interopDataAttrObj('Text')} ref={forwardedRef} {...textProps} />;
+});
+
+Text.displayName = 'Text';
+
+const styles = {
+  text: {
+    ...cssReset(DEFAULT_TAG),
+  },
+};
+
+export { Text, styles };
+export type { TextProps };
