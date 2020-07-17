4f4128cde1660ca3f2cdec6497851061d740eb77
Jenna Smith <jenna@growldesign.co.uk>
2020-07-17 19:18:29 +0100

    Add `Collapsible` tests

---
+++ packages/react/collapsible/src/Collapsible.test.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { Collapsible } from './Collapsible';
+
+const BUTTON_TEXT = 'Button';
+const CONTENT_TEXT = 'Content';
+
+const CollapsibleTest = (props: React.ComponentProps<typeof Collapsible>) => (
+  <Collapsible {...props}>
+    <Collapsible.Button>{BUTTON_TEXT}</Collapsible.Button>
+    <Collapsible.Content>{CONTENT_TEXT}</Collapsible.Content>
+  </Collapsible>
+);
+
+describe('given a default Collapsible', () => {
+  let query: ReturnType<typeof render>;
+  let button: HTMLElement;
+
+  beforeEach(() => {
+    query = render(<CollapsibleTest />);
+    button = query.getByText(BUTTON_TEXT);
+  });
+
+  it('should render a button', () => {
+    expect(button).toBeInTheDocument();
+  });
+
+  describe('when clicking the button', () => {
+    let content: HTMLElement;
+
+    beforeEach(() => {
+      fireEvent.click(button);
+      content = query.getByText(CONTENT_TEXT);
+    });
+
+    it('should open the content', () => {
+      expect(content).toBeVisible();
+    });
+
+    describe('and clicking the button again', () => {
+      beforeEach(() => {
+        fireEvent.click(button);
+      });
+
+      it('should close the content', () => {
+        expect(content).not.toBeVisible();
+      });
+    });
+  });
+});
+
+describe('given a disabled Collapsible', () => {
+  let query: ReturnType<typeof render>;
+
+  beforeEach(() => {
+    query = render(<CollapsibleTest disabled />);
+  });
+
+  it('should render a disabled button', () => {
+    const button = query.getByText(BUTTON_TEXT);
+    expect(button).toBeDisabled();
+  });
+});
+
+describe('given an open uncontrolled Collapsible', () => {
+  let query: ReturnType<typeof render>;
+  let content: HTMLElement;
+  const onToggle = jest.fn();
+
+  beforeEach(() => {
+    query = render(<CollapsibleTest defaultIsOpen={true} onToggle={onToggle} />);
+    content = query.getByText(CONTENT_TEXT);
+  });
+
+  it('should render with open content', () => {
+    expect(content).toBeVisible();
+  });
+
+  describe('when clicking the button', () => {
+    beforeEach(() => {
+      const button = query.getByText(BUTTON_TEXT);
+      fireEvent.click(button);
+    });
+
+    it('should call `onToggle` prop with `false` value', () => {
+      expect(onToggle).toHaveBeenCalledWith(false);
+    });
+
+    it('should close the content', () => {
+      expect(content).not.toBeVisible();
+    });
+  });
+});
+
+describe('given an open controlled Collapsible', () => {
+  let query: ReturnType<typeof render>;
+  let content: HTMLElement;
+  const onToggle = jest.fn();
+
+  beforeEach(() => {
+    query = render(<CollapsibleTest isOpen={true} onToggle={onToggle} />);
+    content = query.getByText(CONTENT_TEXT);
+  });
+
+  it('should render with open content', () => {
+    expect(content).toBeVisible();
+  });
+
+  describe('when clicking the button', () => {
+    beforeEach(() => {
+      const button = query.getByText(BUTTON_TEXT);
+      fireEvent.click(button);
+    });
+
+    it('should call `onToggle` prop with `false` value', () => {
+      expect(onToggle).toHaveBeenCalledWith(false);
+    });
+
+    it('should not close the content', () => {
+      expect(content).toBeVisible();
+    });
+  });
+});
