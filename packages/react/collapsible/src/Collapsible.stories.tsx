2a4d33a4401b838d8e9bc44c1288238a34f3130f
Jenna Smith <jenna@growldesign.co.uk>
2020-07-20 13:27:41 +0100

    Remove debug classes from story

--- packages/react/collapsible/src/Collapsible.stories.tsx
+++ packages/react/collapsible/src/Collapsible.stories.tsx
@@ -4,8 +4,8 @@ import { Collapsible } from './Collapsible';
 export default { title: 'Collapsible' };
 
 export const Basic = () => (
-  <Collapsible className="test">
-    <Collapsible.Button className="button">Button</Collapsible.Button>
-    <Collapsible.Content className="content">Content 1</Collapsible.Content>
+  <Collapsible>
+    <Collapsible.Button>Button</Collapsible.Button>
+    <Collapsible.Content>Content 1</Collapsible.Content>
   </Collapsible>
 );
