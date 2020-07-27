188f9b07cf745c9addc771ac2850dfd813382537
Chance Strickland <hi@chancedigital.io>
2020-07-27 14:45:21 -0700

    visually hidden: inline styles by default

--- packages/react/visually-hidden/src/VisuallyHidden.tsx
+++ packages/react/visually-hidden/src/VisuallyHidden.tsx
@@ -3,17 +3,26 @@ import { forwardRef } from '@interop-ui/react-utils';
 
 const DEFAULT_TAG = 'span';
 
-type VisuallyHiddenOwnProps = {};
+type VisuallyHiddenParts = 'visuallyHidden';
+type VisuallyHiddenOwnProps = {
+  bypassInlineStyles?: boolean;
+};
 type VisuallyHiddenDOMProps = React.ComponentPropsWithoutRef<typeof DEFAULT_TAG>;
 type VisuallyHiddenProps = VisuallyHiddenOwnProps & VisuallyHiddenDOMProps;
 
 const VisuallyHidden = forwardRef<typeof DEFAULT_TAG, VisuallyHiddenProps>(
-  ({ as: Comp = DEFAULT_TAG, ...props }, forwardedRef) => <Comp {...props} ref={forwardedRef} />
+  ({ as: Comp = DEFAULT_TAG, bypassInlineStyles = false, style, ...props }, forwardedRef) => (
+    <Comp
+      {...props}
+      ref={forwardedRef}
+      style={bypassInlineStyles ? style : { ...styles.visuallyHidden, ...style }}
+    />
+  )
 );
 
 VisuallyHidden.displayName = 'VisuallyHidden';
 
-const styles = {
+const styles: { [part in VisuallyHiddenParts]?: React.CSSProperties } = {
   visuallyHidden: {
     // See: https://github.com/twbs/bootstrap/blob/master/scss/mixins/_screen-reader.scss
     position: 'absolute',
