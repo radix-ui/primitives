45b6081c720eadb564c45cf27ab781f1d4540f4e
Jenna Smith <jenna@growldesign.co.uk>
2020-07-31 10:43:15 +0100

    Add interop attributes and default styles to `ToggleTip`

--- packages/react/toggletip/src/Toggletip.tsx
+++ packages/react/toggletip/src/Toggletip.tsx
@@ -1,17 +1,28 @@
 import * as React from 'react';
+import { cssReset, interopDataAttrObj } from '@interop-ui/utils';
+import { forwardRef, PrimitiveStyles } from '@interop-ui/react-utils';
 
-type ToggletipDOMProps = React.ComponentProps<'div'>;
+const DEFAULT_TAG = 'div';
+
+type ToggletipDOMProps = React.ComponentProps<typeof DEFAULT_TAG>;
 type ToggletipOwnProps = {};
 type ToggletipProps = ToggletipDOMProps & ToggletipOwnProps;
 
-const Toggletip = React.forwardRef<HTMLDivElement, ToggletipProps>(function Toggletip(
+const Toggletip = forwardRef<typeof DEFAULT_TAG, ToggletipProps>(function Toggletip(
   props,
   forwardedRef
 ) {
-  return <div ref={forwardedRef} />;
+  const { as: Comp = DEFAULT_TAG, ...toggleTipProps } = props;
+  return <Comp {...toggleTipProps} {...interopDataAttrObj('ToggleTip')} ref={forwardedRef} />;
 });
 
 Toggletip.displayName = 'Toggletip';
 
-export { Toggletip };
+const styles: PrimitiveStyles = {
+  toggleTip: {
+    ...cssReset(DEFAULT_TAG),
+  },
+};
+
+export { styles, Toggletip };
 export type { ToggletipProps };
