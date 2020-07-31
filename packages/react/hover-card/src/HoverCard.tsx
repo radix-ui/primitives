b3edffd22f8f5d1ae3542a86bb6aafd4deb83259
Jenna Smith <jenna@growldesign.co.uk>
2020-07-31 10:37:12 +0100

    Add interop attributes and default styles to `HoverCard`

--- packages/react/hover-card/src/HoverCard.tsx
+++ packages/react/hover-card/src/HoverCard.tsx
@@ -1,17 +1,28 @@
 import * as React from 'react';
+import { cssReset, interopDataAttrObj } from '@interop-ui/utils';
+import { forwardRef, PrimitiveStyles } from '@interop-ui/react-utils';
 
-type HoverCardDOMProps = React.ComponentProps<'div'>;
+const DEFAULT_TAG = 'div';
+
+type HoverCardDOMProps = React.ComponentProps<typeof DEFAULT_TAG>;
 type HoverCardOwnProps = {};
 type HoverCardProps = HoverCardDOMProps & HoverCardOwnProps;
 
-const HoverCard = React.forwardRef<HTMLDivElement, HoverCardProps>(function HoverCard(
+const HoverCard = forwardRef<typeof DEFAULT_TAG, HoverCardProps>(function HoverCard(
   props,
   forwardedRef
 ) {
-  return <div ref={forwardedRef} />;
+  const { as: Comp = DEFAULT_TAG, ...hoverCardProps } = props;
+  return <Comp {...hoverCardProps} {...interopDataAttrObj('HoverCard')} ref={forwardedRef} />;
 });
 
 HoverCard.displayName = 'HoverCard';
 
-export { HoverCard };
+const styles: PrimitiveStyles = {
+  hoverCard: {
+    ...cssReset(DEFAULT_TAG),
+  },
+};
+
+export { styles, HoverCard };
 export type { HoverCardProps };
