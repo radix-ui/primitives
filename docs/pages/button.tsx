2b299be198ab91b191ab13d5fefa04bc13262e0e
Colm Tuite <colmtuite@Colms-iMac.local>
2020-08-01 00:16:27 +0100

    Tweaking the button

--- docs/pages/button.tsx
+++ docs/pages/button.tsx
@@ -96,14 +96,7 @@ export default function ButtonPage() {
               mb: 5,
             }}
           >
-            <Heading
-              as="h4"
-              size={1}
-              sx={{
-                lineHeight: 2,
-                mb: 1,
-              }}
-            >
+            <Heading as="h3" size={3}>
               Installation
             </Heading>
             <Text size={3} sx={{ lineHeight: 2 }}>
@@ -122,13 +115,7 @@ export default function ButtonPage() {
               mb: 5,
             }}
           >
-            <Heading
-              as="h4"
-              size={1}
-              sx={{
-                lineHeight: 2,
-              }}
-            >
+            <Heading as="h3" size={3}>
               Keyboard interaction
             </Heading>
           </Box>
@@ -160,11 +147,19 @@ export default function ButtonPage() {
             </Tbody>
           </Table>
 
+          <Heading as="h3" size={3} mt={7} mb={3}>
+            Examples
+          </Heading>
+
           <Heading as="h4" size={1} sx={{ mt: 7, mb: 5, lineHeight: 2 }}>
             Simple button
           </Heading>
 
-          <CodeBlock live>{`<Button>Button</Button>`}</CodeBlock>
+          <CodeBlock live>
+            {`
+<Button>Button</Button>
+`}
+          </CodeBlock>
 
           <Box
             sx={{
@@ -183,58 +178,37 @@ export default function ButtonPage() {
             </Heading>
           </Box>
 
-          <Box
-            sx={{
-              border: '1px solid gainsboro',
-              borderRadius: '2',
-            }}
-          >
-            <Box
-              sx={{
-                p: 3,
-              }}
-            >
-              <Button>
-                <Box
-                  sx={{
-                    mr: 1,
-                  }}
-                  style={{
-                    lineHeight: 1,
-                  }}
-                >
-                  <svg
-                    width="15"
-                    height="15"
-                    viewBox="0 0 15 15"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    style={{ verticalAlign: 'middle' }}
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M5.64683 3.14628C5.84209 2.95102 6.15867 2.95102 6.35394 3.14628L10.3539 7.14628C10.4477 7.24005 10.5004 7.36722 10.5004 7.49983C10.5004 7.63244 10.4477 7.75962 10.3539 7.85339L6.35393 11.8534C6.15867 12.0486 5.84209 12.0486 5.64683 11.8534C5.45157 11.6581 5.45157 11.3415 5.64683 11.1463L9.29328 7.49983L5.64683 3.85339C5.45157 3.65812 5.45157 3.34154 5.64683 3.14628Z"
-                      fill="#282B2E"
-                    />
-                  </svg>
-                </Box>
-                Button
-              </Button>
-            </Box>
-            <Box
-              sx={{
-                borderTop: '1px solid gainsboro',
-                p: 3,
-              }}
-            >
-              <pre style={{ margin: '0' }}>
-                <code style={{ fontSize: '13px', fontFamily: 'Söhne Mono' }}>
-                  &lt;Button&gt;Button&lt;/Button&gt;
-                </code>
-              </pre>
-            </Box>
-          </Box>
+          <CodeBlock live>
+            {`
+<Button>
+  <Box
+    sx={{
+      mr: 1,
+    }}
+    style={{
+      lineHeight: 1,
+    }}
+  >
+    <svg
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ verticalAlign: 'middle' }}
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M5.64683 3.14628C5.84209 2.95102 6.15867 2.95102 6.35394 3.14628L10.3539 7.14628C10.4477 7.24005 10.5004 7.36722 10.5004 7.49983C10.5004 7.63244 10.4477 7.75962 10.3539 7.85339L6.35393 11.8534C6.15867 12.0486 5.84209 12.0486 5.64683 11.8534C5.45157 11.6581 5.45157 11.3415 5.64683 11.1463L9.29328 7.49983L5.64683 3.85339C5.45157 3.65812 5.45157 3.34154 5.64683 3.14628Z"
+        fill="#282B2E"
+      />
+    </svg>
+  </Box>
+  Button
+</Button>
+`}
+          </CodeBlock>
 
           <Box
             sx={{
@@ -633,13 +607,7 @@ export default function ButtonPage() {
                 mb: 5,
               }}
             >
-              <Heading
-                as="h4"
-                size={1}
-                sx={{
-                  lineHeight: 2,
-                }}
-              >
+              <Heading as="h3" size={3}>
                 Related components
               </Heading>
               <ul>
