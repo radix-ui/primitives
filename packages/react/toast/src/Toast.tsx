e7d01b12613289c3628601a1bca230ea948ab7b6
Jenna Smith <jenna@growldesign.co.uk>
2020-07-31 10:41:11 +0100

    Add interop attributes and default styles to `Toast`

--- packages/react/toast/src/Toast.tsx
+++ packages/react/toast/src/Toast.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
+import { cssReset, interopDataAttrObj } from '@interop-ui/utils';
+import { forwardRef, PrimitiveStyles } from '@interop-ui/react-utils';
 
-type ToastDOMProps = React.ComponentProps<'div'>;
+const DEFAULT_TAG = 'div';
+
+type ToastDOMProps = React.ComponentProps<typeof DEFAULT_TAG>;
 type ToastOwnProps = {};
 type ToastProps = ToastDOMProps & ToastOwnProps;
 
-const Toast = React.forwardRef<HTMLDivElement, ToastProps>(function Toast(props, forwardedRef) {
-  return <div ref={forwardedRef} />;
+const Toast = forwardRef<typeof DEFAULT_TAG, ToastProps>(function Toast(props, forwardedRef) {
+  const { as: Comp = DEFAULT_TAG, ...toastProps } = props;
+  return <Comp {...toastProps} {...interopDataAttrObj('Toast')} ref={forwardedRef} />;
 });
 
 Toast.displayName = 'Toast';
 
-export { Toast };
+const styles: PrimitiveStyles = {
+  toast: {
+    ...cssReset(DEFAULT_TAG),
+  },
+};
+
+export { styles, Toast };
 export type { ToastProps };
