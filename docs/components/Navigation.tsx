e5ef5e1f6d01cb6a9c6b3df5e450b968ac215ced
Colm Tuite <colmtuite@Colms-iMac.local>
2020-08-01 03:33:42 +0100

    Clean up navigation

--- docs/components/Navigation.tsx
+++ docs/components/Navigation.tsx
@@ -73,59 +73,10 @@ export const Navigation = () => {
         </Flex>
         <List>
           <PageLink href="/introduction">Introduction</PageLink>
-          <PageLink href="/getting-started">Getting started</PageLink>
+          <PageLink href="/getting-started">Installation</PageLink>
+          <PageLink href="/getting-started">Styling</PageLink>
+          <PageLink href="/getting-started">Animation</PageLink>
           <PageLink href="/roadmap">Roadmap</PageLink>
-        </List>
-        <Divider />
-        <Flex
-          sx={{
-            pt: 3,
-            pb: 1,
-            px: 4,
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Text
-            as="h3"
-            size={3}
-            weight="medium"
-            sx={{
-              lineHeight: 1,
-            }}
-          >
-            Docs
-          </Text>
-          <Box
-            sx={{
-              mr: -2,
-            }}
-          >
-            <svg
-              width="15"
-              height="15"
-              viewBox="0 0 15 15"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              style={{ lineHeight: 1 }}
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M8 3C8 2.72386 7.77614 2.5 7.5 2.5C7.22386 2.5 7 2.72386 7 3V7H3C2.72386 7 2.5 7.22386 2.5 7.5C2.5 7.77614 2.72386 8 3 8H7V12C7 12.2761 7.22386 12.5 7.5 12.5C7.77614 12.5 8 12.2761 8 12V8H12C12.2761 8 12.5 7.77614 12.5 7.5C12.5 7.22386 12.2761 7 12 7H8V3Z"
-                fill="#282B2E"
-              />
-            </svg>
-          </Box>
-        </Flex>
-        <List>
-          <PageLink href="/styling-with-interop">Styling with Interop</PageLink>
-          <PageLink href="/styling-with-styled-components">Styling with Styled Components</PageLink>
-          <PageLink href="/styling-with-emotion">Styling with Emotion</PageLink>
-          <PageLink href="/styling-with-treat">Styling with Treat</PageLink>
-          <PageLink href="/styling-with-stitches">Styling with Stitches</PageLink>
-          <PageLink href="/styling-with-css">Styling with CSS</PageLink>
-          <PageLink href="/animation">Animation</PageLink>
         </List>
         <Divider />
         <Flex
@@ -187,17 +138,8 @@ export const Navigation = () => {
           <PageLink href="/box" isNested>
             Box
           </PageLink>
-          <PageLink href="/container" isNested>
-            Container
-          </PageLink>
           <PageLink href="/flex" isNested>
             Flex
-          </PageLink>
-          <PageLink href="/grid" isNested>
-            Grid
-          </PageLink>
-          <PageLink href="/section" isNested>
-            Section
           </PageLink>
         </List>
         <Text
@@ -215,29 +157,20 @@ export const Navigation = () => {
           Input
         </Text>
         <List>
-          <PageLink href="/autocomplete" isNested>
-            Autocomplete<Badge sx={{ ml: 1 }}>WIP</Badge>
-          </PageLink>
           <PageLink href="/combobox" isNested>
             ComboBox
           </PageLink>
           <PageLink href="/checkbox" isNested>
             Checkbox
           </PageLink>
-          <PageLink href="/datepicker" isNested>
-            DatePicker<Badge sx={{ ml: 1 }}>WIP</Badge>
-          </PageLink>
           <PageLink href="/input" isNested>
             Input
-          </PageLink>
-          <PageLink href="/multi-select" isNested>
-            MultiSelect<Badge sx={{ ml: 1 }}>WIP</Badge>
           </PageLink>
           <PageLink href="/radio" isNested>
             Radio
           </PageLink>
           <PageLink href="/slider" isNested>
-            Slider<Badge sx={{ ml: 1 }}>WIP</Badge>
+            Slider
           </PageLink>
           <PageLink href="/select" isNested>
             Select
@@ -275,9 +208,6 @@ export const Navigation = () => {
           </PageLink>
           <PageLink href="/dialog" isNested>
             Dialog
-          </PageLink>
-          <PageLink href="/meter" isNested>
-            Meter<Badge sx={{ ml: 1 }}>WIP</Badge>
           </PageLink>
           <PageLink href="/progres" isNested>
             Progress
@@ -330,16 +260,18 @@ export const Navigation = () => {
             AspectRatio
           </PageLink>
           <PageLink href="/hover-zone" isNested>
-            HoverZone<Badge sx={{ ml: 1 }}>WIP</Badge>
+            HoverZone
           </PageLink>
           <PageLink href="/portal" isNested>
             Portal
           </PageLink>
-          <PageLink href="/toolbar" isNested>
-            Toolbar<Badge sx={{ ml: 1 }}>WIP</Badge>
-          </PageLink>
           <PageLink href="/visually-hidden" isNested>
-            VisuallyHidden<Badge sx={{ ml: 1 }}>WIP</Badge>
+            <Flex sx={{ alignItems: 'center' }}>
+              VisuallyHidden
+              <Badge variant="blue" sx={{ ml: 1 }}>
+                New
+              </Badge>
+            </Flex>
           </PageLink>
         </List>
         <Text
@@ -357,9 +289,6 @@ export const Navigation = () => {
           Contextual
         </Text>
         <List>
-          <PageLink href="/hover-card" isNested>
-            HoverCard<Badge sx={{ ml: 1 }}>WIP</Badge>
-          </PageLink>
           <PageLink href="/overlay" isNested>
             Overlay
           </PageLink>
@@ -367,7 +296,7 @@ export const Navigation = () => {
             Popover
           </PageLink>
           <PageLink href="/right-click-menu" isNested>
-            RightClickMenu
+            ContextMenu
           </PageLink>
           <PageLink href="/sheet" isNested>
             Sheet
@@ -405,9 +334,6 @@ export const Navigation = () => {
           </PageLink>
           <PageLink href="/card" isNested>
             Card
-          </PageLink>
-          <PageLink href="/carrousel" isNested>
-            Carrousel<Badge sx={{ ml: 1 }}>WIP</Badge>
           </PageLink>
           <PageLink href="/collapsible" isNested>
             Collapsible
