06df4d0ddc12148f3eefd5cc3bb0dc42b5c8bc4b
Jenna Smith <jenna@growldesign.co.uk>
2020-08-07 11:52:31 +0100

    Integrate style extraction for `Input`

--- packages/react/input/src/Input.tsx
+++ packages/react/input/src/Input.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { cssReset, interopDataAttrObj } from '@interop-ui/utils';
+import { cssReset, interopDataAttrObj, interopSelector } from '@interop-ui/utils';
 import { forwardRef, PrimitiveStyles } from '@interop-ui/react-utils';
 
+const NAME = 'Input';
 const DEFAULT_TAG = 'input';
 
 type InputDOMProps = React.ComponentPropsWithoutRef<typeof DEFAULT_TAG>;
@@ -10,18 +11,19 @@ type InputProps = InputDOMProps & InputOwnProps;
 
 const Input = forwardRef<typeof DEFAULT_TAG, InputProps>(function Input(props, forwardedRef) {
   const { as: Comp = DEFAULT_TAG, ...inputProps } = props;
-  return <Comp {...interopDataAttrObj('Input')} ref={forwardedRef} {...inputProps} />;
+  return <Comp {...interopDataAttrObj(NAME)} ref={forwardedRef} {...inputProps} />;
 });
 
-Input.displayName = 'Input';
+Input.displayName = NAME;
 
 const styles: PrimitiveStyles = {
-  input: {
+  [interopSelector(NAME)]: {
     ...cssReset(DEFAULT_TAG),
     lineHeight: '1',
-  },
-  'input.state.disabled': {
-    pointerEvents: 'none',
+
+    '&:disabled': {
+      pointerEvents: 'none',
+    },
   },
 };
 
