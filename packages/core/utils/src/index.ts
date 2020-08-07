fd52c13f8f0671d755cf97f3d5530e8575eb3fd8
Chance Strickland <hi@chancedigital.io>
2020-08-07 11:48:36 -0700

    import __DEV__ from utils

--- packages/core/utils/src/index.ts
+++ packages/core/utils/src/index.ts
@@ -9,3 +9,6 @@ export * from './observeElementRect';
 export * from './typeUtils';
 
 export * from './types';
+
+// TODO: Consider removing this if we can work with the babel plugin as intended
+export const __DEV__ = process.env.NODE_ENV === 'development';
