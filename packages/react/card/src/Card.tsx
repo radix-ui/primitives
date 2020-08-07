1c5dedcdb517d97e139c8f0b3f825203c0b8badb
Jenna Smith <jenna@growldesign.co.uk>
2020-08-07 17:08:15 +0100

    Integrate style extraction for `Card` (#58)

--- packages/react/card/src/Card.tsx
+++ packages/react/card/src/Card.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { cssReset, interopDataAttrObj } from '@interop-ui/utils';
+import { cssReset, interopDataAttrObj, interopSelector } from '@interop-ui/utils';
 import { forwardRef, PrimitiveStyles } from '@interop-ui/react-utils';
 
+const NAME = 'Card';
 const DEFAULT_TAG = 'div';
 
 type CardDOMProps = React.ComponentPropsWithoutRef<typeof DEFAULT_TAG>;
@@ -10,13 +11,13 @@ type CardProps = CardDOMProps & CardOwnProps;
 
 const Card = forwardRef<typeof DEFAULT_TAG, CardProps>(function Card(props, forwardedRef) {
   const { as: Comp = DEFAULT_TAG, ...cardProps } = props;
-  return <Comp {...interopDataAttrObj('Card')} ref={forwardedRef} {...cardProps} />;
+  return <Comp {...interopDataAttrObj(NAME)} ref={forwardedRef} {...cardProps} />;
 });
 
-Card.displayName = 'Card';
+Card.displayName = NAME;
 
 const styles: PrimitiveStyles = {
-  card: {
+  [interopSelector(NAME)]: {
     ...cssReset(DEFAULT_TAG),
   },
 };
