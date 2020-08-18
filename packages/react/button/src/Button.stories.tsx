29b80eb0073c5b9a58c7a601a9e7c187b3515818
Jenna Smith <jenna@growldesign.co.uk>
2020-08-18 15:28:18 +0100

    Reset stories for `Button`

--- packages/react/button/src/Button.stories.tsx
+++ packages/react/button/src/Button.stories.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react';
-import { Button } from './Button';
+import { Button as ButtonPrimitive, styles } from './Button';
 
 export default { title: 'Button' };
 
-export const Basic = () => {
-  return <Button as="div">Hello Button</Button>;
-};
+export const Basic = () => <Button>Button</Button>;
+
+export const InlineStyle = () => (
+  <Button style={{ backgroundColor: 'gainsboro', padding: '5px 10px', borderRadius: '4px' }}>
+    Button
+  </Button>
+);
+
+const Button = (props: React.ComponentProps<typeof ButtonPrimitive>) => (
+  <ButtonPrimitive {...props} style={{ ...styles.root, ...props.style }} />
+);
