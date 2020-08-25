179637218b9921cff566fed19922d97ad0e3112d
Jenna Smith <jenna@growldesign.co.uk>
2020-08-25 18:34:43 +0100

    Reset stories for `AccesibleIcon` (#96)

--- packages/react/accessible-icon/src/AccessibleIcon.stories.tsx
+++ packages/react/accessible-icon/src/AccessibleIcon.stories.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
-import { AccessibleIcon } from './AccessibleIcon';
+import { AccessibleIcon as AccessibleIconPrimitive, styles } from './AccessibleIcon';
 
 export default { title: 'AccessibleIcon' };
 
-export const Basic = () => (
-  <AccessibleIcon label="Close">
+export const Basic = () => <AccessibleIcon label="Close" />;
+export const InlineStyle = () => <AccessibleIcon label="Close" style={{ color: 'gainsboro' }} />;
+
+const AccessibleIcon = (props: React.ComponentProps<typeof AccessibleIconPrimitive>) => (
+  <AccessibleIconPrimitive {...props} style={{ ...styles.root, ...props.style }}>
     <svg viewBox="0 0 32 32" width={24} height={24} fill="none" stroke="currentColor">
       <path d="M2 30 L30 2 M30 30 L2 2" />
     </svg>
-  </AccessibleIcon>
+  </AccessibleIconPrimitive>
 );
