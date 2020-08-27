b0eac9c3e3da58c1a2233e1051464626fa7a8c38
Jenna Smith <jenna@growldesign.co.uk>
2020-08-27 19:38:49 +0100

    Remove closed `AspectRatio` (#128)

--- packages/react/aspect-ratio/src/AspectRatio.tsx
+++ packages/react/aspect-ratio/src/AspectRatio.tsx
@@ -2,38 +2,48 @@ import * as React from 'react';
 import { cssReset } from '@interop-ui/utils';
 import { forwardRef, createStyleObj } from '@interop-ui/react-utils';
 
-const ROOT_NAME = 'AspectRatio.Root';
-const ROOT_DEFAULT_TAG = 'div';
+/* -------------------------------------------------------------------------------------------------
+ * AspectRatio
+ * -----------------------------------------------------------------------------------------------*/
 
-type AspectRatioDOMProps = React.ComponentPropsWithoutRef<typeof ROOT_DEFAULT_TAG>;
+const ASPECT_RATIO_NAME = 'AspectRatio';
+const ASPECT_RATIO__DEFAULT_TAG = 'div';
+
+type AspectRatioDOMProps = React.ComponentPropsWithoutRef<typeof ASPECT_RATIO__DEFAULT_TAG>;
 type AspectRatioOwnProps = { ratio?: string };
 type AspectRatioProps = AspectRatioDOMProps & AspectRatioOwnProps;
 
-const AspectRatioRoot = forwardRef<typeof ROOT_DEFAULT_TAG, AspectRatioProps>(
-  function AspectRatioRoot(props, forwardedRef) {
-    const { as: Comp = ROOT_DEFAULT_TAG, ratio = '1:1', style, ...aspectRatioProps } = props;
+const AspectRatio = forwardRef<
+  typeof ASPECT_RATIO__DEFAULT_TAG,
+  AspectRatioProps,
+  AspectRatioStaticProps
+>(function AspectRatio(props, forwardedRef) {
+  const { as: Comp = ASPECT_RATIO__DEFAULT_TAG, ratio = '1:1', style, ...aspectRatioProps } = props;
 
-    const [n1, n2] = ratio.split(':');
-    const paddingBottom = 100 / (Number(n1) / Number(n2));
+  const [n1, n2] = ratio.split(':');
+  const paddingBottom = 100 / (Number(n1) / Number(n2));
 
-    return (
-      <Comp
-        {...aspectRatioProps}
-        {...interopDataAttrObj('root')}
-        ref={forwardedRef}
-        style={{
-          ...style,
-          ['--paddingBottom' as any]: `${paddingBottom}%`,
-        }}
-      />
-    );
-  }
-);
+  return (
+    <Comp
+      {...aspectRatioProps}
+      {...interopDataAttrObj('root')}
+      ref={forwardedRef}
+      style={{
+        ...style,
+        ['--paddingBottom' as any]: `${paddingBottom}%`,
+      }}
+    />
+  );
+});
+
+/* -------------------------------------------------------------------------------------------------
+ * AspectRatioInner
+ * -----------------------------------------------------------------------------------------------*/
 
 const INNER_NAME = 'AspectRatio.Inner';
 const INNER_DEFAULT_TAG = 'div';
 
-type AspectRatioInnerDOMProps = React.ComponentPropsWithoutRef<typeof ROOT_DEFAULT_TAG>;
+type AspectRatioInnerDOMProps = React.ComponentPropsWithoutRef<typeof ASPECT_RATIO__DEFAULT_TAG>;
 type AspectRatioInnerOwnProps = {};
 type AspectRatioInnerProps = AspectRatioInnerDOMProps & AspectRatioInnerOwnProps;
 
@@ -45,35 +55,18 @@ const AspectRatioInner = forwardRef<typeof INNER_DEFAULT_TAG, AspectRatioInnerPr
   }
 );
 
-const ASPECT_RATIO_NAME = 'AspectRatio';
-
-const AspectRatio = forwardRef<typeof ROOT_DEFAULT_TAG, AspectRatioProps, AspectRatioStaticProps>(
-  function AspectRatio(props, forwardedRef) {
-    const { children, ...aspectRatioProps } = props;
-
-    return (
-      <AspectRatioRoot {...aspectRatioProps} ref={forwardedRef}>
-        <AspectRatioInner>{children}</AspectRatioInner>
-      </AspectRatioRoot>
-    );
-  }
-);
-
-AspectRatio.Root = AspectRatioRoot;
 AspectRatio.Inner = AspectRatioInner;
 
 AspectRatio.displayName = ASPECT_RATIO_NAME;
-AspectRatio.Root.displayName = ROOT_NAME;
 AspectRatio.Inner.displayName = INNER_NAME;
 
 interface AspectRatioStaticProps {
-  Root: typeof AspectRatioRoot;
   Inner: typeof AspectRatioInner;
 }
 
 const [styles, interopDataAttrObj] = createStyleObj(ASPECT_RATIO_NAME, {
   root: {
-    ...cssReset(ROOT_DEFAULT_TAG),
+    ...cssReset(ASPECT_RATIO__DEFAULT_TAG),
     paddingBottom: 'var(--paddingBottom)',
     position: 'relative',
     width: '100%',
