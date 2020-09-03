04a2d043f5d9791c3c91163ab2e77055b4972a6d
Benoît Grélard <benoit.grelard@gmail.com>
2020-09-03 10:55:08 +0100

    Re-add stories and tests to Portal package

---
+++ packages/react/portal/src/Portal.stories.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import { Portal } from './Portal';
+import { DebugContextProvider } from '@interop-ui/react-debug-context';
+
+export default { title: 'Portal' };
+
+export const Base = () => (
+  <div
+    style={{
+      maxWidth: 300,
+      maxHeight: 200,
+      overflow: 'auto',
+      border: '1px solid',
+    }}
+  >
+    <h1>This content is rendered in the main DOM tree</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos porro, est ex quia itaque facere
+      fugit necessitatibus aut enim. Nisi rerum quae, repellat in perspiciatis explicabo laboriosam
+      necessitatibus eius pariatur.
+    </p>
+
+    <Portal>
+      <h1>This content is rendered in a portal (another DOM tree)</h1>
+      <p>
+        Because of the portal, it can appear in a different DOM tree from the main one (by default a
+        new element inside the body), even though it is part of the same React tree.
+      </p>
+    </Portal>
+  </div>
+);
+
+export const CustomContainer = () => {
+  const portalContainerRef = React.useRef<HTMLDivElement>(null);
+
+  return (
+    <>
+      <div style={{ maxWidth: 300, padding: 10, margin: 10, border: '1px solid' }}>
+        <h1>Container A</h1>
+        <Portal containerRef={portalContainerRef}>
+          <p>
+            This content is rendered in a portal inside Container A but appears inside Container B
+            because we have used Container B as a container element for the Portal.
+          </p>
+        </Portal>
+      </div>
+
+      <div
+        ref={portalContainerRef}
+        style={{ maxWidth: 300, padding: 10, margin: 10, border: '1px solid' }}
+      >
+        <h1>Container B</h1>
+      </div>
+    </>
+  );
+};
+
+export const CustomContainerViaDebugContextProvider = () => {
+  const portalContainerRef = React.useRef<HTMLDivElement>(null);
+
+  return (
+    <>
+      <div style={{ maxWidth: 300, padding: 10, margin: 10, border: '1px solid' }}>
+        <h1>Container A</h1>
+        <DebugContextProvider portalContainerRef={portalContainerRef}>
+          <Portal>
+            <p>
+              This content is rendered in a portal inside Container A but appears inside Container B
+              because we have used Container B as a container element for the Portal (via
+              `DebugContextProvider`).
+            </p>
+          </Portal>
+        </DebugContextProvider>
+      </div>
+
+      <div
+        ref={portalContainerRef}
+        style={{ maxWidth: 300, padding: 10, margin: 10, border: '1px solid' }}
+      >
+        <h1>Container B</h1>
+      </div>
+    </>
+  );
+};
