a1325827f4812a1c43a3ff4fa11d985facb3cdd8
Chance Strickland <hi@chancedigital.io>
2020-09-14 11:07:56 -0700

    fix tracking map issues

--- packages/react/announcer/src/Announcer.tsx
+++ packages/react/announcer/src/Announcer.tsx
@@ -4,6 +4,7 @@ import { cssReset, interopDataAttr } from '@interop-ui/utils';
 import {
   forwardRef,
   createStyleObj,
+  useComposedRefs,
   useIsomorphicLayoutEffect as useLayoutEffect,
 } from '@interop-ui/react-utils';
 
@@ -17,77 +18,7 @@ const ROLES: { [key in RegionType]: RegionRole } = {
   off: 'none',
 };
 
-function useAnnouncer({
-  'aria-atomic': ariaAtomic,
-  'aria-relevant': ariaRelevant,
-  type = 'polite',
-  role = ROLES[type],
-  ownerDocument = document,
-  children,
-  id,
-}: AnnouncerHookProps) {
-  const [region, setRegion] = React.useState<HTMLElement>();
-  const relevant = ariaRelevant
-    ? Array.isArray(ariaRelevant)
-      ? ariaRelevant.join(' ')
-      : ariaRelevant
-    : undefined;
-
-  const getLiveRegionElement = React.useCallback(() => {
-    let element = ownerDocument.querySelector(buildSelector({ type, role, relevant, id }));
-    if (!element) {
-      element = buildLiveRegionElement(ownerDocument, {
-        type,
-        relevant,
-        role,
-        atomic: ariaAtomic || false,
-        id,
-      });
-    }
-    return element;
-  }, [ariaAtomic, ownerDocument, relevant, role, type, id]);
-
-  useLayoutEffect(() => {
-    setRegion(getLiveRegionElement() as HTMLElement);
-  }, [getLiveRegionElement]);
-
-  // In some screen reader/browser combinations, alerts coming from an inactive browser tab may be
-  // announced, which is a confusing experience for a user interacting with a completely different
-  // page. When the page visibility changes we'll update the `role` and `aria-live` attributes of
-  // our region element to prevent that.
-  // https://inclusive-components.design/notifications/#restrictingmessagestocontexts
-  React.useEffect(() => {
-    const liveRegionElement = getLiveRegionElement();
-
-    // I'm pretty sure this is fine. We don't need this listener for more than once per live region
-    // type. YOLO...
-    const yoloGlobalVar = `__INTEROP_LIVE_REGION_LISTENER_ATTACHED_${
-      type.toUpperCase() + id ? `_${id}` : ''
-    }`;
-    if (!(window as any)[yoloGlobalVar]) {
-      ownerDocument.addEventListener('visibilitychange', updateAttributesOnVisibilityChange);
-      Object.assign(window, { [yoloGlobalVar]: true });
-      return function () {
-        Object.assign(window, { [yoloGlobalVar]: false });
-        ownerDocument.removeEventListener('visibilitychange', updateAttributesOnVisibilityChange);
-      };
-    }
-
-    return;
-
-    function updateAttributesOnVisibilityChange() {
-      liveRegionElement.setAttribute('role', ownerDocument.hidden ? 'none' : role);
-      liveRegionElement.setAttribute('aria-live', ownerDocument.hidden ? 'off' : type);
-    }
-  }, [getLiveRegionElement, ownerDocument, role, type, id]);
-
-  if (region) {
-    // Portal into live region for screen reader announcements
-    ReactDOM.createPortal(<div>{children}</div>, region);
-  }
-
-  return region;
-}
+const listenerMap = new Map<Element, number>();
 
 /* -------------------------------------------------------------------------------------------------
  * Announcer
@@ -97,62 +28,7 @@ const NAME = 'Announcer';
 const DEFAULT_TAG = 'div';
 
 type AnnouncerDOMProps = React.ComponentPropsWithoutRef<typeof DEFAULT_TAG>;
-type AnnouncerOwnProps = Omit<AnnouncerHookProps, 'ownerDocument'> & {
-  regionIdentifier?: string;
-};
-type AnnouncerProps = AnnouncerDOMProps & AnnouncerOwnProps;
-
-const Announcer = forwardRef<typeof DEFAULT_TAG, AnnouncerProps>(function Announcer(
-  props,
-  forwardedRef
-) {
-  const {
-    as: Comp = DEFAULT_TAG,
-    type = 'polite',
-    'aria-relevant': ariaRelevant,
-    role,
-    children,
-    regionIdentifier,
-    ...regionProps
-  } = props;
-
-  const ariaAtomic = ['true', true].includes(regionProps['aria-atomic'] as any);
-  useAnnouncer({
-    'aria-atomic': ariaAtomic,
-    'aria-relevant': ariaRelevant,
-    children,
-    id: regionIdentifier,
-    role,
-    type,
-  });
-
-  return (
-    <Comp {...regionProps} {...interopDataAttrObj('root')} ref={forwardedRef}>
-      {children}
-    </Comp>
-  );
-});
-
-Announcer.displayName = NAME;
-
-const [styles, interopDataAttrObj] = createStyleObj(NAME, {
-  root: {
-    ...cssReset(DEFAULT_TAG),
-  },
-});
-
-export { Announcer, styles, useAnnouncer };
-export type { AnnouncerProps, AnnouncerHookProps };
-
-type AnnouncerOptions = {
-  type: string;
-  relevant?: string;
-  role: string;
-  atomic?: boolean;
-  id?: string;
-};
-
-type AnnouncerHookProps = {
+type AnnouncerOwnProps = {
   /**
    * Mirrors the `aria-atomic` DOM attribute for live regions. It is an optional attribute that
    * indicates whether assistive technologies will present all, or only parts of, the changed region
@@ -186,19 +62,14 @@ type AnnouncerHookProps = {
   /**
    * An optional unique identifier for the live region.
    *
-   * By default, `Announcer` components and the `useAnnouncer` hook create, at most, two unique
-   * `aria-live` regions in the document (one for all `polite` notifications, one for all
-   * `assertive` notifications). In some cases you may wish to append additional `aria-live` regions
-   * for distinct purposes (for example, simple status updates may need to be separated from a stack
-   * of toast-style notifications). By passing an id, you indicate that any content rendered by
-   * components with the same identifier should be mirrored in a separate `aria-live` region.
+   * By default, `Announcer` components create, at most, two unique `aria-live` regions in the
+   * document (one for all `polite` notifications, one for all `assertive` notifications). In some
+   * cases you may wish to append additional `aria-live` regions for distinct purposes (for example,
+   * simple status updates may need to be separated from a stack of toast-style notifications). By
+   * passing an id, you indicate that any content rendered by components with the same identifier
+   * should be mirrored in a separate `aria-live` region.
    */
-  id?: string;
-  /**
-   * Optional prop to declare the Document to which the live region is appended. Useful for contexts
-   * outside of the browser window. Defaults to the global `document`.
-   */
-  ownerDocument?: Document;
+  regionIdentifier?: string;
   /**
    * Mirrors the `role` DOM attribute. This is optional and may be useful as an override in some
    * cases. By default, the role is determined by the `type` prop.
@@ -214,6 +85,127 @@ type AnnouncerHookProps = {
    * @see MDN https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Live_Regions
    */
   type?: RegionType;
+};
+type AnnouncerProps = AnnouncerDOMProps & AnnouncerOwnProps;
+type AnnouncerDOMElement = HTMLElementTagNameMap[typeof DEFAULT_TAG];
+
+const Announcer = forwardRef<typeof DEFAULT_TAG, AnnouncerProps>(function Announcer(
+  props,
+  forwardedRef
+) {
+  const {
+    as: Comp = DEFAULT_TAG,
+    'aria-relevant': ariaRelevant,
+    children,
+    type = 'polite',
+    role = ROLES[type],
+    regionIdentifier,
+    ...regionProps
+  } = props;
+
+  const ariaAtomic = ['true', true].includes(regionProps['aria-atomic'] as any);
+  const [ownerDocument, setOwnerDocument] = React.useState(document);
+  const setOwnerDocumentFromRef = React.useCallback((node: AnnouncerDOMElement) => {
+    if (node) {
+      setOwnerDocument(node.ownerDocument);
+    }
+  }, []);
+  const ownRef = React.useRef<AnnouncerDOMElement | null>(null);
+  const ref = useComposedRefs(forwardedRef, ownRef, setOwnerDocumentFromRef);
+
+  const [region, setRegion] = React.useState<HTMLElement>();
+  const relevant = ariaRelevant
+    ? Array.isArray(ariaRelevant)
+      ? ariaRelevant.join(' ')
+      : ariaRelevant
+    : undefined;
+
+  const getLiveRegionElement = React.useCallback(() => {
+    let element = ownerDocument.querySelector(
+      buildSelector({ type, role, relevant, id: regionIdentifier })
+    );
+    if (!element) {
+      element = buildLiveRegionElement(ownerDocument, {
+        type,
+        relevant,
+        role,
+        atomic: ariaAtomic || false,
+        id: regionIdentifier,
+      });
+    }
+    return element;
+  }, [ariaAtomic, ownerDocument, relevant, role, type, regionIdentifier]);
+
+  useLayoutEffect(() => {
+    setRegion(getLiveRegionElement() as HTMLElement);
+  }, [getLiveRegionElement]);
+
+  // In some screen reader/browser combinations, alerts coming from an inactive browser tab may be
+  // announced, which is a confusing experience for a user interacting with a completely different
+  // page. When the page visibility changes we'll update the `role` and `aria-live` attributes of
+  // our region element to prevent that.
+  // https://inclusive-components.design/notifications/#restrictingmessagestocontexts
+  React.useEffect(() => {
+    function updateAttributesOnVisibilityChange() {
+      regionElement.setAttribute('role', ownerDocument.hidden ? 'none' : role);
+      regionElement.setAttribute('aria-live', ownerDocument.hidden ? 'off' : type);
+    }
+
+    // Ok, so this might look a little weird and confusing, but here's what's going on:
+    //   - We need to hide `aria-live` regions via a global event listener, as noted in the comment
+    //     above.
+    //   - We only need one listener per region. Keep in mind that each `Announcer` does not
+    //     necessarily generate a unique live region element.
+    //   - We track whether or not a listener has already been attached for a given region in a map
+    //     so we can skip these effects after `Announcer` is used again with a shared live region.
+    const regionElement = getLiveRegionElement();
+
+    if (!listenerMap.get(regionElement)) {
+      ownerDocument.addEventListener('visibilitychange', updateAttributesOnVisibilityChange);
+      listenerMap.set(regionElement, 1);
+    } else {
+      const announcerCount = listenerMap.get(regionElement)!;
+      listenerMap.set(regionElement, announcerCount + 1);
+    }
+
+    return function () {
+      const announcerCount = listenerMap.get(regionElement)!;
+      listenerMap.set(regionElement, announcerCount - 1);
+      if (announcerCount === 1) {
+        ownerDocument.removeEventListener('visibilitychange', updateAttributesOnVisibilityChange);
+      }
+    };
+  }, [getLiveRegionElement, ownerDocument, role, type]);
+
+  if (region) {
+    // Portal into live region for screen reader announcements
+    ReactDOM.createPortal(<div>{children}</div>, region);
+  }
+
+  return (
+    <Comp {...regionProps} {...interopDataAttrObj('root')} ref={ref}>
+      {children}
+    </Comp>
+  );
+});
+
+Announcer.displayName = NAME;
+
+const [styles, interopDataAttrObj] = createStyleObj(NAME, {
+  root: {
+    ...cssReset(DEFAULT_TAG),
+  },
+});
+
+export { Announcer, styles };
+export type { AnnouncerProps };
+
+type AnnouncerOptions = {
+  type: string;
+  relevant?: string;
+  role: string;
+  atomic?: boolean;
+  id?: string;
 };
 
 function buildLiveRegionElement(
