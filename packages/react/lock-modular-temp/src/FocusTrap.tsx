8d74045dccc3a838dc3cd01e677ed2393e0e0d58
Benoît Grélard <benoit.grelard@gmail.com>
2020-09-18 16:28:58 +0100

    FocusTrap

---
+++ packages/react/lock-modular-temp/src/FocusTrap.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import * as ReactIs from 'react-is';
+import { useComposedRefs } from '@interop-ui/react-utils';
+import { createFocusTrap } from './createFocusTrap';
+
+function useFocusTrap(ref: React.RefObject<HTMLElement>) {
+  React.useEffect(() => {
+    const container = ref.current;
+    let destroyFocusTrap = () => {};
+    if (container) {
+      destroyFocusTrap = createFocusTrap(container);
+    }
+    return destroyFocusTrap;
+  }, [ref]);
+}
+
+type FocusTrapProps = { children: React.ReactNode };
+
+function FocusTrap({ children }: FocusTrapProps) {
+  const child = React.Children.only(children);
+  if (!React.isValidElement(child) || ReactIs.isFragment(child)) {
+    throw new Error('Focus Trap needs to have a single valid React child.');
+  }
+  return <FocusTrapImpl>{child}</FocusTrapImpl>;
+}
+
+type FocusTrapImplProps = { children: React.ReactElement };
+
+function FocusTrapImpl({ children: child }: FocusTrapImplProps) {
+  const containerRef = React.useRef<HTMLElement>(null);
+  const ref = useComposedRefs((child as any).ref, containerRef);
+
+  useFocusTrap(containerRef);
+
+  return React.cloneElement(child, { ref });
+}
+
+export { useFocusTrap, FocusTrap };
