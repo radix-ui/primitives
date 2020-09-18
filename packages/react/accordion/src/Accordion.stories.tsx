f5be9ffcf6d1065011fc7ed4cf735de735b5c9bc
Chance Strickland <chance@modulz.app>
2020-09-18 11:59:17 -0700

    Accordion key fix (#181)
    * prevent scroll on accordion nav

--- packages/react/accordion/src/Accordion.stories.tsx
+++ packages/react/accordion/src/Accordion.stories.tsx
@@ -84,6 +84,50 @@ export const InlineStyle = () => (
   </Accordion>
 );
 
+export const OutsideViewport = () => (
+  <>
+    <p>Scroll down to see tabs</p>
+    <div style={{ height: '150vh' }} />
+    <p>
+      When accordion buttons are focused and the user is navigating via keyboard, the page should
+      not scroll unless the next tab is entering the viewport.
+    </p>
+    <Accordion>
+      <AccordionItem value="one">
+        <AccordionButton>One</AccordionButton>
+        <AccordionPanel>
+          Per erat orci nostra luctus sociosqu mus risus penatibus, duis elit vulputate viverra
+          integer ullamcorper congue curabitur sociis, nisi malesuada scelerisque quam suscipit
+          habitant sed.
+        </AccordionPanel>
+      </AccordionItem>
+      <AccordionItem value="two">
+        <AccordionButton>Two</AccordionButton>
+        <AccordionPanel>
+          Cursus sed mattis commodo fermentum conubia ipsum pulvinar sagittis, diam eget bibendum
+          porta nascetur ac dictum, leo tellus dis integer platea ultrices mi.
+        </AccordionPanel>
+      </AccordionItem>
+      <AccordionItem value="three" disabled>
+        <AccordionButton>Three (disabled)</AccordionButton>
+        <AccordionPanel>
+          Sociis hac sapien turpis conubia sagittis justo dui, inceptos penatibus feugiat himenaeos
+          euismod magna, nec tempor pulvinar eu etiam mattis.
+        </AccordionPanel>
+      </AccordionItem>
+      <AccordionItem value="four">
+        <AccordionButton>Four</AccordionButton>
+        <AccordionPanel>
+          Odio placerat <a href="#">quisque</a> sapien sagittis non sociis ligula penatibus
+          dignissim vitae, enim vulputate nullam semper potenti etiam volutpat libero.
+          <button>Cool</button>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+    <div style={{ height: '150vh' }} />
+  </>
+);
+
 const Accordion = (props: React.ComponentProps<typeof AccordionPrimitive>) => (
   <AccordionPrimitive {...props} style={{ ...styles.root, ...props.style }} />
 );
