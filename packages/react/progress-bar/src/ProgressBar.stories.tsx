4cc9e13a1bfdb545c9cfce955c23d53ce4c3ded0
Chance Strickland <chance@modulz.app>
2020-09-23 15:10:49 -0700

    New component: ProgressBar (#183)

---
+++ packages/react/progress-bar/src/ProgressBar.stories.tsx
@@ -0,0 +1,140 @@
+import * as React from 'react';
+import { ProgressBar, styles } from './ProgressBar';
+import { createStyled } from '@stitches/react';
+
+export default { title: 'ProgressBar' };
+
+const { styled } = createStyled({
+  tokens: {
+    colors: {
+      $white: '#fff',
+      $gray100: '#ccc',
+      $gray300: '#aaa',
+      $black: '#111',
+      $red: 'crimson',
+      $green: 'green',
+    },
+  },
+});
+
+export const Basic = () => {
+  const [value, percentage, setValue] = useProgressValueState(0);
+  const toggleIndeterminate = useIndeterminateToggle(value, setValue);
+  return (
+    <div>
+      <ProgressBar style={styles.root} value={value}>
+        <ProgressBar.Indicator
+          style={{ ...styles.indicator, width: percentage ?? `${percentage}%` }}
+        />
+      </ProgressBar>
+      <hr />
+      <button onClick={toggleIndeterminate}>Toggle Indeterminate</button>
+      <ProgressRange value={value} setValue={setValue} />
+    </div>
+  );
+};
+
+export const StitchesStyle = () => {
+  const max = 150;
+  const [value, percentage, setValue] = useProgressValueState(0, max);
+  const toggleIndeterminate = useIndeterminateToggle(value, setValue);
+  return (
+    <div>
+      <ProgressBar as={Root} value={value} max={max}>
+        <ProgressBar.Indicator
+          as={Indicator}
+          style={{ width: percentage != null ? `${percentage}%` : undefined }}
+        />
+      </ProgressBar>
+      <hr />
+      <button onClick={toggleIndeterminate}>Toggle Indeterminate</button>
+      <ProgressRange value={value} setValue={setValue} max={max} />
+    </div>
+  );
+};
+
+function ProgressRange({ value, setValue, max = 100 }: any) {
+  const previousValueRef = usePreviousValueRef(value);
+  return (
+    <input
+      type="range"
+      disabled={value == null}
+      value={value == null ? previousValueRef.current : value}
+      max={max}
+      min={0}
+      onChange={(e) => {
+        const val = Number(e.target.value);
+        if (!isNaN(val)) {
+          setValue(val);
+        }
+      }}
+    />
+  );
+}
+
+/* -------------------------------------------------------------------------------------------------
+ * Reset components
+ * -----------------------------------------------------------------------------------------------*/
+
+const HEIGHT = 20;
+const WIDTH = 400;
+const BORDER = 5;
+
+const Root = styled('div', {
+  ...(styles.root as any),
+  height: `${HEIGHT}px`,
+  width: `${WIDTH}px`,
+  maxWidth: '100%',
+  border: `${BORDER}px solid $black`,
+  backgroundColor: '$gray100',
+});
+
+const Indicator = styled('div', {
+  ...(styles.indicator as any),
+  width: '100%',
+  height: '100%',
+  backgroundColor: '$red',
+  transition: 'background 150ms ease-out',
+  '&[data-state="indeterminate"]': {
+    backgroundColor: '$gray300',
+  },
+  '&[data-state="complete"]': {
+    backgroundColor: '$green',
+  },
+});
+
+type ProgressValue = number | null;
+function useProgressValueState(initialState: ProgressValue | (() => ProgressValue), max = 100) {
+  const [value, setValue] = React.useState<number | null>(initialState);
+  const precentage = value != null ? Math.round((value / max) * 100) : null;
+  return [value, precentage, setValue] as const;
+}
+
+function useIndeterminateToggle(
+  value: ProgressValue,
+  setValue: React.Dispatch<React.SetStateAction<ProgressValue>>
+) {
+  const previousValueRef = usePreviousValueRef(value);
+  const toggleIndeterminate = React.useCallback(
+    function setIndeterminate() {
+      setValue((val) => {
+        if (val == null) {
+          return previousValueRef.current;
+        }
+        return null;
+      });
+    },
+    [previousValueRef, setValue]
+  );
+  return toggleIndeterminate;
+}
+
+function usePreviousValueRef(value: ProgressValue) {
+  const previousValueRef = React.useRef<number>(value || 0);
+  React.useEffect(() => {
+    if (value != null) {
+      previousValueRef.current = value;
+    }
+  });
+  return previousValueRef;
+}
---
+++ packages/react/progress-bar/src/ProgressBar.tsx
@@ -0,0 +1,196 @@
+import * as React from 'react';
+import { cssReset } from '@interop-ui/utils';
+import { createContext, createStyleObj, forwardRef } from '@interop-ui/react-utils';
+
+/* -------------------------------------------------------------------------------------------------
+ * ProgressBar
+ * -----------------------------------------------------------------------------------------------*/
+
+const PROGRESS_NAME = 'ProgressBar';
+const PROGRESS_DEFAULT_TAG = 'div';
+const DEFAULT_MAX = 100;
+
+type ProgressBarState = 'indeterminate' | 'complete' | 'loading';
+type ProgressBarDOMProps = React.ComponentPropsWithoutRef<typeof PROGRESS_DEFAULT_TAG>;
+type ProgressBarOwnProps = {
+  value?: number | null | undefined;
+  max?: number;
+  getValueLabel?(value: number, max: number): string;
+};
+type ProgressBarProps = ProgressBarOwnProps & ProgressBarDOMProps;
+
+type ProgressBarContextValue = {
+  value: number | null;
+  max: number;
+};
+
+const [ProgressBarContext, useProgressBarContext] = createContext<ProgressBarContextValue>(
+  PROGRESS_NAME + 'Context',
+  PROGRESS_NAME
+);
+
+const ProgressBar = forwardRef<
+  typeof PROGRESS_DEFAULT_TAG,
+  ProgressBarProps,
+  ProgressBarStaticProps
+>(function ProgressBar(props, forwardedRef) {
+  const {
+    as: Comp = PROGRESS_DEFAULT_TAG,
+    children,
+    value: valueProp,
+    max: maxProp,
+    getValueLabel = defaultGetValueLabel,
+    ...progressProps
+  } = props;
+
+  const max = isValidMaxNumber(maxProp) ? maxProp : DEFAULT_MAX;
+  const value = isValidValueNumber(valueProp, max) ? valueProp : null;
+
+  const ctx: ProgressBarContextValue = React.useMemo(() => ({ value, max }), [value, max]);
+
+  const valueLabel = isNumber(value) ? getValueLabel(value, max) : undefined;
+
+  return (
+    <Comp
+      aria-valuemax={max}
+      aria-valuemin={0}
+      aria-valuenow={isNumber(value) ? value : undefined}
+      aria-valuetext={valueLabel}
+      role="progressbar"
+      {...progressProps}
+      {...interopDataAttrObj('root')}
+      data-state={getProgressBarState(value, max)}
+      data-value={value ?? undefined}
+      data-max={max}
+      ref={forwardedRef}
+    >
+      <ProgressBarContext.Provider value={ctx}>{children}</ProgressBarContext.Provider>
+    </Comp>
+  );
+});
+
+/* -------------------------------------------------------------------------------------------------
+ * ProgressBarIndicator
+ * -----------------------------------------------------------------------------------------------*/
+
+const INDICATOR_NAME = 'ProgressBar.Indicator';
+const INDICATOR_DEFAULT_TAG = 'div';
+
+type ProgressBarIndicatorDOMProps = React.ComponentPropsWithoutRef<typeof INDICATOR_DEFAULT_TAG>;
+type ProgressBarIndicatorOwnProps = {};
+type ProgressBarIndicatorProps = ProgressBarIndicatorDOMProps & ProgressBarIndicatorOwnProps;
+
+const ProgressBarIndicator = forwardRef<typeof INDICATOR_DEFAULT_TAG, ProgressBarIndicatorProps>(
+  function ProgressBarIndicator(props, forwardedRef) {
+    const { value, max } = useProgressBarContext(INDICATOR_NAME);
+    const { as: Comp = INDICATOR_DEFAULT_TAG, ...indicatorProps } = props;
+    return (
+      <Comp
+        {...indicatorProps}
+        {...interopDataAttrObj('indicator')}
+        data-state={getProgressBarState(value, max)}
+        data-value={value || undefined}
+        data-max={max}
+        ref={forwardedRef}
+      />
+    );
+  }
+);
+
+/* ---------------------------------------------------------------------------------------------- */
+
+function useProgressBarState() {
+  const { value, max } = React.useContext(ProgressBarContext);
+  return getProgressBarState(value, max);
+}
+
+ProgressBar.displayName = PROGRESS_NAME;
+ProgressBarIndicator.displayName = INDICATOR_NAME;
+
+ProgressBar.Indicator = ProgressBarIndicator;
+
+interface ProgressBarStaticProps {
+  Indicator: typeof ProgressBarIndicator;
+}
+
+const [styles, interopDataAttrObj] = createStyleObj(PROGRESS_NAME, {
+  root: {
+    ...cssReset(PROGRESS_DEFAULT_TAG),
+    position: 'relative',
+  },
+  indicator: {
+    ...cssReset(INDICATOR_DEFAULT_TAG),
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+  },
+});
+
+ProgressBar.propTypes = {
+  max(props, propName, componentName, location, propFullName) {
+    const propValue = props[propName];
+    const strVal = String(propValue);
+    if (propValue && !isValidMaxNumber(propValue)) {
+      return new Error(getInvalidMaxError(strVal, componentName));
+    }
+    return null;
+  },
+  value(props, propName, componentName) {
+    const valueProp = props[propName];
+    const strVal = String(valueProp);
+    const max = isValidMaxNumber(props.max) ? props.max : DEFAULT_MAX;
+    if (valueProp != null && !isValidValueNumber(valueProp, max)) {
+      return new Error(getInvalidValueError(strVal, componentName));
+    }
+    return null;
+  },
+};
+
+export type { ProgressBarProps, ProgressBarIndicatorProps, ProgressBarState };
+export { ProgressBar, styles, useProgressBarState };
+
+function defaultGetValueLabel(value: number, max: number) {
+  return `${Math.round((value / max) * 100)}%`;
+}
+
+function getProgressBarState(value: number | undefined | null, maxValue: number): ProgressBarState {
+  return value == null ? 'indeterminate' : value === maxValue ? 'complete' : 'loading';
+}
+
+function isNumber(value: any): value is number {
+  return typeof value === 'number';
+}
+
+function isValidMaxNumber(max: any): max is number {
+  // prettier-ignore
+  return (
+    isNumber(max) &&
+    !isNaN(max) &&
+    max > 0
+  );
+}
+
+function isValidValueNumber(value: any, max: number): value is number {
+  // prettier-ignore
+  return (
+    isNumber(value) &&
+    !isNaN(value) &&
+    value <= max &&
+    value >= 0
+  );
+}
+
+// Split this out for clearer readability of the error message.
+function getInvalidMaxError(propValue: string, componentName: string) {
+  return `Invalid prop \`max\` of value \`${propValue}\` supplied to \`${componentName}\`. Only numbers greater than 0 are valid max values. Defaulting to \`${DEFAULT_MAX}\`.`;
+}
+
+function getInvalidValueError(propValue: string, componentName: string) {
+  return `Invalid prop \`value\` of value \`${propValue}\` supplied to \`${componentName}\`. The \`value\` prop must be:
+  - a positive number
+  - less than the value passed to \`max\` (or ${DEFAULT_MAX} if no \`max\` prop is set)
+  - \`null\` if the progress bar is indeterminate.
+
+Defaulting to \`null\`.`;
+}
