f8816727328ca66bba8d25fbd351e7c214adeefa
Pedro Duarte <contact@peduarte.com>
2020-09-25 17:44:49 +0200

    Add source plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   stories: ['../packages/**/*.stories.tsx'],
+  addons: ['@storybook/addon-storysource'],
 
   // we need to add aliases to webpack so it knows how to follow
   // to the source of the packages rather than the built version (dist)
