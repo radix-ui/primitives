b09d3048ee9764d0d0c26f655c16c2507841a00c
Jenna Smith <jenna@growldesign.co.uk>
2020-10-22 20:20:58 +0100

    Update stories

--- packages/react/slider/src/Slider.stories.tsx
+++ packages/react/slider/src/Slider.stories.tsx
@@ -13,45 +13,100 @@ export const Basic = () => (
 );
 
 export const Horizontal = () => {
-  const [isFocus, setIsFocus] = React.useState(false);
+  const [isFocusRange, setIsFocusRange] = React.useState([false, false]);
+  const [isFocusSingle, setIsFocusSingle] = React.useState(false);
 
   return (
-    <Slider as={Root} defaultValue={10} style={{ height: 15 }}>
-      <Slider.Track as={Track} style={{ height: 4 }}>
-        <Slider.Range as={Range} style={{ height: '100%' }} />
-      </Slider.Track>
-      <Slider.Thumb
-        as={Thumb}
-        style={{ top: '50%' }}
-        focused={isFocus}
-        onFocus={() => setIsFocus(true)}
-        onBlur={() => setIsFocus(false)}
-      />
-    </Slider>
+    <>
+      <Slider as={Root} defaultValue={[10, 30]} style={{ height: 15 }}>
+        <Slider.Track as={Track} style={{ height: 4 }}>
+          <Slider.Range as={Range} style={{ height: '100%' }} />
+        </Slider.Track>
+        <Slider.Thumb
+          as={Thumb}
+          style={{ top: '50%' }}
+          focused={isFocusRange[0]}
+          onFocus={() => setIsFocusRange([true, false])}
+          onBlur={() => setIsFocusRange([false, false])}
+        />
+        <Slider.Thumb
+          as={Thumb}
+          style={{ top: '50%' }}
+          focused={isFocusRange[1]}
+          onFocus={() => setIsFocusRange([false, true])}
+          onBlur={() => setIsFocusRange([false, false])}
+        />
+      </Slider>
+
+      <br />
+
+      <Slider as={Root} defaultValue={10} style={{ height: 15 }}>
+        <Slider.Track as={Track} style={{ height: 4 }}>
+          <Slider.Range as={Range} style={{ height: '100%' }} />
+        </Slider.Track>
+        <Slider.Thumb
+          as={Thumb}
+          style={{ top: '50%' }}
+          focused={isFocusSingle}
+          onFocus={() => setIsFocusSingle(true)}
+          onBlur={() => setIsFocusSingle(false)}
+        />
+      </Slider>
+    </>
   );
 };
 
 export const Vertical = () => {
-  const [isFocus, setIsFocus] = React.useState(false);
+  const [isFocusRange, setIsFocusRange] = React.useState([false, false]);
+  const [isFocusSingle, setIsFocusSingle] = React.useState(false);
 
   return (
-    <Slider
-      as={Root}
-      defaultValue={10}
-      orientation="vertical"
-      style={{ width: 15, flexDirection: 'column' }}
-    >
-      <Slider.Track as={Track} style={{ height: '300px', width: 4 }}>
-        <Slider.Range as={Range} style={{ width: '100%' }} />
-      </Slider.Track>
-      <Slider.Thumb
-        as={Thumb}
-        style={{ left: '50%' }}
-        focused={isFocus}
-        onFocus={() => setIsFocus(true)}
-        onBlur={() => setIsFocus(false)}
-      />
-    </Slider>
+    <>
+      <Slider
+        as={Root}
+        defaultValue={[10, 30]}
+        orientation="vertical"
+        style={{ width: 15, flexDirection: 'column' }}
+      >
+        <Slider.Track as={Track} style={{ height: '300px', width: 4 }}>
+          <Slider.Range as={Range} style={{ width: '100%' }} />
+        </Slider.Track>
+        <Slider.Thumb
+          as={Thumb}
+          style={{ left: '50%' }}
+          focused={isFocusRange[0]}
+          onFocus={() => setIsFocusRange([true, false])}
+          onBlur={() => setIsFocusRange([false, false])}
+        />
+        <Slider.Thumb
+          as={Thumb}
+          style={{ left: '50%' }}
+          focused={isFocusRange[1]}
+          onFocus={() => setIsFocusRange([false, true])}
+          onBlur={() => setIsFocusRange([false, false])}
+        />
+      </Slider>
+
+      <br />
+
+      <Slider
+        as={Root}
+        defaultValue={10}
+        orientation="vertical"
+        style={{ width: 15, flexDirection: 'column' }}
+      >
+        <Slider.Track as={Track} style={{ height: '300px', width: 4 }}>
+          <Slider.Range as={Range} style={{ width: '100%' }} />
+        </Slider.Track>
+        <Slider.Thumb
+          as={Thumb}
+          style={{ left: '50%' }}
+          focused={isFocusSingle}
+          onFocus={() => setIsFocusSingle(true)}
+          onBlur={() => setIsFocusSingle(false)}
+        />
+      </Slider>
+    </>
   );
 };
 
