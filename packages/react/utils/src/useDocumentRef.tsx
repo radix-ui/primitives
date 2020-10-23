b2b0dd0c15e2ccfd84bfd73357cd437fe379f09f
Chance Strickland <hi@chancedigital.io>
2020-10-23 09:35:12 -0700

    useDocumentRef: Check that document is defined to prevent throwing in SSR

--- packages/react/utils/src/useDocumentRef.tsx
+++ packages/react/utils/src/useDocumentRef.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import { useLayoutEffect } from './useLayoutEffect';
 
+/**
+ * Get a reference to the global document object relative to a specific node ref
+ * @param forwardedRef
+ */
 export function useDocumentRef<T extends Element>(
   forwardedRef: React.RefObject<T | null>
 ): React.MutableRefObject<Document> {
-  const ownerDocumentRef = React.useRef(document);
+  const ownerDocumentRef = React.useRef(typeof document !== 'undefined' ? document : undefined);
   useLayoutEffect(() => {
     if (forwardedRef.current instanceof Element) {
-      ownerDocumentRef.current = forwardedRef.current.ownerDocument;
+      ownerDocumentRef.current = forwardedRef.current.ownerDocument || document;
     }
   });
-  return ownerDocumentRef;
+  return ownerDocumentRef as React.MutableRefObject<Document>;
 }
