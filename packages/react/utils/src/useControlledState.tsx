3a661d9f2429fc266c6ababdd45671548c886bd3
Jenna Smith <jenna@growldesign.co.uk>
2020-10-28 16:18:23 +0000

    Add `React.useCallback`

--- packages/react/utils/src/useControlledState.tsx
+++ packages/react/utils/src/useControlledState.tsx
@@ -17,16 +17,20 @@ export function useControlledState<T>({
   const [uncontrolledProp, setUncontrolledProp] = useUncontrolledState({ defaultProp, onChange });
   const isControlled = prop !== undefined;
   const value = isControlled ? prop : uncontrolledProp;
+  const handleChange = useCallbackRef(onChange);
 
-  const setValue: React.Dispatch<React.SetStateAction<T | undefined>> = (nextValue) => {
-    if (isControlled) {
-      const setter = nextValue as SetStateFn<T>;
-      const value = typeof nextValue === 'function' ? setter(prop) : nextValue;
-      if (value !== prop) onChange(value as T);
-    } else {
-      setUncontrolledProp(nextValue);
-    }
-  };
+  const setValue: React.Dispatch<React.SetStateAction<T | undefined>> = React.useCallback(
+    (nextValue) => {
+      if (isControlled) {
+        const setter = nextValue as SetStateFn<T>;
+        const value = typeof nextValue === 'function' ? setter(prop) : nextValue;
+        if (value !== prop) handleChange(value as T);
+      } else {
+        setUncontrolledProp(nextValue);
+      }
+    },
+    [isControlled, prop, setUncontrolledProp, handleChange]
+  );
 
   return [value, setValue] as const;
 }
