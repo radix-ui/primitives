22a5f7d79044514422b59bf50b37cc3b17e500f8
Benoît Grélard <benoit.grelard@gmail.com>
2020-11-13 21:53:51 +0000

    Rework AspectRatio

--- packages/react/aspect-ratio/src/AspectRatio.stories.tsx
+++ packages/react/aspect-ratio/src/AspectRatio.stories.tsx
@@ -1,27 +1,48 @@
 import * as React from 'react';
-import { AspectRatio, styles } from './AspectRatio';
+import { AspectRatio } from './AspectRatio';
 import { styled } from '../../../../stitches.config';
 
 export default { title: 'Components/AspectRatio' };
 
-export const Basic = () => (
+export const Styled = () => (
   <div style={{ width: 500 }}>
-    <AspectRatio as={BasicStyledRoot}>
-      <div style={{ backgroundColor: '#eee', height: '100%' }}>
-        A grey box inside the AspectRatio
-      </div>
+    <AspectRatio as={StyledRoot}>
+      <h1>Default ratio (1/1)</h1>
     </AspectRatio>
   </div>
 );
 
-export const Styled = () => (
-  <div style={{ width: 500 }}>
-    <AspectRatio as={StyledRoot} ratio={16 / 9} />
-  </div>
-);
+export const CustomRatios = () => {
+  const image = (
+    <img
+      src="https://images.unsplash.com/photo-1605030753481-bb38b08c384a?&auto=format&fit=crop&w=400&q=80"
+      alt="A house in a forest"
+      style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+    />
+  );
 
-const BasicStyledRoot = styled('div', styles.root);
+  return (
+    <div style={{ display: 'flex', gap: 20 }}>
+      <div style={{ width: 200 }}>
+        <AspectRatio ratio={1 / 2}>{image}</AspectRatio>
+      </div>
+      <div style={{ width: 200 }}>
+        <AspectRatio>{image}</AspectRatio>
+      </div>
+      <div style={{ width: 200 }}>
+        <AspectRatio ratio={16 / 9}>{image}</AspectRatio>
+      </div>
+      <div style={{ width: 200 }}>
+        <AspectRatio ratio={2 / 1}>{image}</AspectRatio>
+      </div>
+    </div>
+  );
+};
 
-const StyledRoot = styled(BasicStyledRoot, {
+const StyledRoot = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
   backgroundColor: '$red',
+  color: 'white',
 });
--- packages/react/aspect-ratio/src/AspectRatio.tsx
+++ packages/react/aspect-ratio/src/AspectRatio.tsx
@@ -1,73 +1,60 @@
 import * as React from 'react';
-import { getPartDataAttr } from '@interop-ui/utils';
-import { forwardRef, createStyleObj } from '@interop-ui/react-utils';
+import { getPartDataAttrObj } from '@interop-ui/utils';
+import { forwardRef } from '@interop-ui/react-utils';
 
 /* -------------------------------------------------------------------------------------------------
  * AspectRatio
  * -----------------------------------------------------------------------------------------------*/
 
 const ASPECT_RATIO_NAME = 'AspectRatio';
-const ASPECT_RATIO__DEFAULT_TAG = 'div';
+const ASPECT_RATIO_DEFAULT_TAG = 'div';
 
-const INNER_DATA_ATTR = getPartDataAttr('AspectRatioInner');
-const INNER_DEFAULT_TAG = 'div';
-
-type AspectRatioDOMProps = React.ComponentPropsWithoutRef<typeof ASPECT_RATIO__DEFAULT_TAG>;
+type AspectRatioDOMProps = React.ComponentPropsWithoutRef<typeof ASPECT_RATIO_DEFAULT_TAG>;
 type AspectRatioOwnProps = { ratio?: number };
 type AspectRatioProps = AspectRatioDOMProps & AspectRatioOwnProps;
 
-const AspectRatio = forwardRef<typeof ASPECT_RATIO__DEFAULT_TAG, AspectRatioProps>(
+const AspectRatio = forwardRef<typeof ASPECT_RATIO_DEFAULT_TAG, AspectRatioProps>(
   function AspectRatio(props, forwardedRef) {
     const {
-      as: Comp = ASPECT_RATIO__DEFAULT_TAG,
+      as: Comp = ASPECT_RATIO_DEFAULT_TAG,
       ratio = 1 / 1,
       style,
       children,
       ...aspectRatioProps
     } = props;
 
-    const innerDataAttrObj = { [INNER_DATA_ATTR]: '' };
-    const Inner = INNER_DEFAULT_TAG;
-
     return (
-      <Comp
-        {...aspectRatioProps}
-        {...getPartDataAttrObj('root')}
-        ref={forwardedRef}
+      <div
         style={{
-          ...style,
-          // Using `any` here recommended by the React.CSSProperties interface definition:
-          // https://github.com/frenic/csstype#what-should-i-do-when-i-get-type-errors
-          ['--interop-aspect-ratio' as any]: ratio,
+          // ensures inner element is contained
+          position: 'relative',
+          // ensures padding bottom trick maths works
+          width: '100%',
+          paddingBottom: `calc(100% / ${ratio})`,
         }}
       >
-        <Inner {...innerDataAttrObj}>{children}</Inner>
-      </Comp>
+        <Comp
+          {...aspectRatioProps}
+          {...getPartDataAttrObj(ASPECT_RATIO_NAME)}
+          ref={forwardedRef}
+          style={{
+            ...style,
+            // ensures children expand in ratio
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          }}
+        >
+          {children}
+        </Comp>
+      </div>
     );
   }
 );
 
 AspectRatio.displayName = ASPECT_RATIO_NAME;
 
-const [styles, getPartDataAttrObj] = createStyleObj(ASPECT_RATIO_NAME, {
-  root: {
-    // ensures inner element is contained
-    position: 'relative',
-    // ensures padding bottom trick maths works
-    width: '100%',
-    paddingBottom: `calc(100% / var(--interop-aspect-ratio))`,
-
-    // ensures children expand in ratio
-    [`& > [${INNER_DATA_ATTR}]`]: {
-      position: 'absolute',
-      left: 0,
-      top: 0,
-      width: '100%',
-      height: '100%',
-      overflow: 'hidden',
-    },
-  },
-});
-
-export { AspectRatio, styles };
+export { AspectRatio };
 export type { AspectRatioProps };
