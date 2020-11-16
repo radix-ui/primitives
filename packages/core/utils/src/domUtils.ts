e174fb89d887ea4983ed1bbc7b0a9541534dd139
Benoît Grélard <benoit.grelard@gmail.com>
2020-11-16 00:16:27 +0000

    Remove static CSS generation

--- packages/core/utils/src/domUtils.ts
+++ packages/core/utils/src/domUtils.ts
@@ -1,17 +1,15 @@
-function camelToKebab(input: string) {
-  return input.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
-}
-
+/** Possible inputs: "Component", "SomeComponent", "Component.Part", "SomeComponent.Part", "SomeComponent.SomePart" */
 function getPartDataAttr(componentPart: string) {
-  return `data-interop-${camelToKebab(componentPart.replace('.', ''))}`;
+  const part = componentPart
+    .replace('.', '')
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .toLowerCase();
+  return `data-interop-${part}`;
 }
 
+/** Possible inputs: "Component", "SomeComponent", "Component.Part", "SomeComponent.Part", "SomeComponent.SomePart" */
 function getPartDataAttrObj(componentPart: string) {
   return { [getPartDataAttr(componentPart)]: '' };
-}
-
-function getPartDataAttrSelector(componentPart: string) {
-  return `[${getPartDataAttr(componentPart)}]`;
 }
 
 function canUseDOM() {
@@ -22,4 +20,4 @@ function makeId(...args: (string | number | null | undefined)[]) {
   return args.filter((val) => val != null).join('-');
 }
 
-export { getPartDataAttr, getPartDataAttrObj, getPartDataAttrSelector, canUseDOM, makeId };
+export { getPartDataAttr, getPartDataAttrObj, canUseDOM, makeId };
