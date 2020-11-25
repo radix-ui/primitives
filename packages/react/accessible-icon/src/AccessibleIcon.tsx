ed6faa670a020eebc8ef05f38c7aa278c827bd2c
Jenna Smith <jenna@growldesign.co.uk>
2020-11-25 16:00:02 +0000

    Add `fowardRefWithAs` to AccessibleIcon (#250)

--- packages/react/accessible-icon/src/AccessibleIcon.tsx
+++ packages/react/accessible-icon/src/AccessibleIcon.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { getPartDataAttrObj } from '@interop-ui/utils';
-import { forwardRef } from '@interop-ui/react-utils';
+import { forwardRefWithAs } from '@interop-ui/react-polymorphic';
 import { VisuallyHidden } from '@interop-ui/react-visually-hidden';
 
 const NAME = 'AccessibleIcon';
@@ -10,24 +10,23 @@ type AccessibleIconDOMProps = React.ComponentPropsWithoutRef<typeof DEFAULT_TAG>
 type AccessibleIconOwnProps = { label: string };
 type AccessibleIconProps = AccessibleIconDOMProps & AccessibleIconOwnProps;
 
-const AccessibleIcon = forwardRef<typeof DEFAULT_TAG, AccessibleIconProps>(function AccessibleIcon(
-  props,
-  forwardedRef
-) {
-  const { as: Comp = DEFAULT_TAG, children, label, ...iconProps } = props;
-  const child = React.Children.only(children);
+const AccessibleIcon = forwardRefWithAs<HTMLSpanElement, AccessibleIconProps>(
+  (props, forwardedRef) => {
+    const { as: Comp = DEFAULT_TAG, children, label, ...iconProps } = props;
+    const child = React.Children.only(children);
 
-  return (
-    <Comp {...getPartDataAttrObj(NAME)} ref={forwardedRef} {...iconProps}>
-      {React.cloneElement(child as React.ReactElement, {
-        // accessibility
-        'aria-hidden': true,
-        focusable: 'false', // See: https://allyjs.io/tutorials/focusing-in-svg.html#making-svg-elements-focusable
-      })}
-      <VisuallyHidden>{label}</VisuallyHidden>
-    </Comp>
-  );
-});
+    return (
+      <Comp {...getPartDataAttrObj(NAME)} ref={forwardedRef} {...iconProps}>
+        {React.cloneElement(child as React.ReactElement, {
+          // accessibility
+          'aria-hidden': true,
+          focusable: 'false', // See: https://allyjs.io/tutorials/focusing-in-svg.html#making-svg-elements-focusable
+        })}
+        <VisuallyHidden>{label}</VisuallyHidden>
+      </Comp>
+    );
+  }
+);
 
 AccessibleIcon.displayName = NAME;
 
