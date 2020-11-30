593f57b615b99c146910585645eb8040e91024b1
Jenna Smith <jenna@growldesign.co.uk>
2020-11-30 21:44:28 +0000

    Add `forwardRefWithAs` to `ToggleButton`

--- packages/react/toggle-button/src/ToggleButton.tsx
+++ packages/react/toggle-button/src/ToggleButton.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import { forwardRef, useControlledState, composeEventHandlers } from '@interop-ui/react-utils';
+import { useControlledState, composeEventHandlers } from '@interop-ui/react-utils';
 import { getPartDataAttrObj } from '@interop-ui/utils';
+import { forwardRefWithAs } from '@interop-ui/react-polymorphic';
 
 const NAME = 'ToggleButton';
 const DEFAULT_TAG = 'button';
 
-type ToggleButtonDOMProps = React.ComponentPropsWithoutRef<typeof DEFAULT_TAG>;
 type ToggleButtonOwnProps = {
   /** Whether the button is toggled or not, if controlled */
   toggled?: boolean;
@@ -17,49 +17,46 @@ type ToggleButtonOwnProps = {
   /** A function called when the button is toggled */
   onToggle?(toggled: boolean): void;
 };
-type ToggleButtonProps = ToggleButtonDOMProps & ToggleButtonOwnProps;
 
-const ToggleButton = forwardRef<typeof DEFAULT_TAG, ToggleButtonProps>(function ToggleButton(
-  props,
-  forwardedRef
-) {
-  const {
-    as: Comp = DEFAULT_TAG,
-    toggled: toggledProp,
-    defaultToggled = false,
-    onClick,
-    onToggle,
-    children,
-    ...buttonProps
-  } = props;
+const ToggleButton = forwardRefWithAs<typeof DEFAULT_TAG, ToggleButtonOwnProps>(
+  (props, forwardedRef) => {
+    const {
+      as: Comp = DEFAULT_TAG,
+      toggled: toggledProp,
+      defaultToggled = false,
+      onClick,
+      onToggle,
+      children,
+      ...buttonProps
+    } = props;
 
-  const [toggled = false, setToggled] = useControlledState({
-    prop: toggledProp,
-    onChange: onToggle,
-    defaultProp: defaultToggled,
-  });
+    const [toggled = false, setToggled] = useControlledState({
+      prop: toggledProp,
+      onChange: onToggle,
+      defaultProp: defaultToggled,
+    });
 
-  return (
-    <Comp
-      {...getPartDataAttrObj(NAME)}
-      type="button"
-      aria-pressed={toggled}
-      data-state={toggled ? 'on' : 'off'}
-      data-disabled={props.disabled ? '' : undefined}
-      ref={forwardedRef}
-      onClick={composeEventHandlers(onClick, () => {
-        if (!props.disabled) {
-          setToggled(!toggled);
-        }
-      })}
-      {...buttonProps}
-    >
-      {children}
-    </Comp>
-  );
-});
+    return (
+      <Comp
+        {...getPartDataAttrObj(NAME)}
+        type="button"
+        aria-pressed={toggled}
+        data-state={toggled ? 'on' : 'off'}
+        data-disabled={props.disabled ? '' : undefined}
+        ref={forwardedRef}
+        onClick={composeEventHandlers(onClick, () => {
+          if (!props.disabled) {
+            setToggled(!toggled);
+          }
+        })}
+        {...buttonProps}
+      >
+        {children}
+      </Comp>
+    );
+  }
+);
 
 ToggleButton.displayName = NAME;
 
 export { ToggleButton };
-export type { ToggleButtonProps };
