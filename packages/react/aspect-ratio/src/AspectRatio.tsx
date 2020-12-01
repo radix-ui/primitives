f2e73034207d64b1d78c09b00e22cd5269b850af
Jenna Smith <jenna@growldesign.co.uk>
2020-12-01 15:32:06 +0000

    Add `forwardRefWithAs` to `AspectRatio` (#260)
    * Add `forwardRefWithAs` to `AspectRatio`
    
    * Changeset

--- packages/react/aspect-ratio/src/AspectRatio.tsx
+++ packages/react/aspect-ratio/src/AspectRatio.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { getPartDataAttrObj } from '@interop-ui/utils';
-import { forwardRef } from '@interop-ui/react-utils';
+import { forwardRefWithAs } from '@interop-ui/react-polymorphic';
 
 /* -------------------------------------------------------------------------------------------------
  * AspectRatio
@@ -9,47 +9,43 @@ import { forwardRef } from '@interop-ui/react-utils';
 const NAME = 'AspectRatio';
 const DEFAULT_TAG = 'div';
 
-type AspectRatioDOMProps = React.ComponentPropsWithoutRef<typeof DEFAULT_TAG>;
 type AspectRatioOwnProps = { ratio?: number };
-type AspectRatioProps = AspectRatioDOMProps & AspectRatioOwnProps;
 
-const AspectRatio = forwardRef<typeof DEFAULT_TAG, AspectRatioProps>(function AspectRatio(
-  props,
-  forwardedRef
-) {
-  const { as: Comp = DEFAULT_TAG, ratio = 1 / 1, style, children, ...aspectRatioProps } = props;
+const AspectRatio = forwardRefWithAs<typeof DEFAULT_TAG, AspectRatioOwnProps>(
+  (props, forwardedRef) => {
+    const { as: Comp = DEFAULT_TAG, ratio = 1 / 1, style, children, ...aspectRatioProps } = props;
 
-  return (
-    <div
-      style={{
-        // ensures inner element is contained
-        position: 'relative',
-        // ensures padding bottom trick maths works
-        width: '100%',
-        paddingBottom: `${100 / ratio}%`,
-      }}
-    >
-      <Comp
-        {...aspectRatioProps}
-        {...getPartDataAttrObj(NAME)}
-        ref={forwardedRef}
+    return (
+      <div
         style={{
-          ...style,
-          // ensures children expand in ratio
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
+          // ensures inner element is contained
+          position: 'relative',
+          // ensures padding bottom trick maths works
+          width: '100%',
+          paddingBottom: `${100 / ratio}%`,
         }}
       >
-        {children}
-      </Comp>
-    </div>
-  );
-});
+        <Comp
+          {...aspectRatioProps}
+          {...getPartDataAttrObj(NAME)}
+          ref={forwardedRef}
+          style={{
+            ...style,
+            // ensures children expand in ratio
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          }}
+        >
+          {children}
+        </Comp>
+      </div>
+    );
+  }
+);
 
 AspectRatio.displayName = NAME;
 
 export { AspectRatio };
-export type { AspectRatioProps };
