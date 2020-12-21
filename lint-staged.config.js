e3140381a7193247f0ab89f4ee069c7dd32d7397
Chance Strickland <hi@chancedigital.io>
2020-12-21 08:53:27 -0800

    test lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+const lint = 'yarn lint --quiet --fix --cache';
+
+// https://github.com/okonet/lint-staged/issues/825#issuecomment-620018284
+module.exports = {
+  '*.{js}': [lint],
+  '*.{ts,tsx}': [() => 'tsc --skipLibCheck --noEmit', lint],
+};
