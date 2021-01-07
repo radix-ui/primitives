50964078a4400e0ece0b27100ce6c75e463b6bfc
Chance Strickland <chance@modulz.app>
2021-01-07 11:20:36 -0800

    [react-utils]: Fix return value from useCallbackRef (#378)
    * return value from callback and improve typing
    
    * version check

--- packages/react/utils/src/useCallbackRef.tsx
+++ packages/react/utils/src/useCallbackRef.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 
 /**
- * A custom hook that converts a callback to a ref
- * to avoid triggering re-renders when passed as a prop
- * or avoid re-executing effects when passed as a dependency
+ * A custom hook that converts a callback to a ref to avoid triggering re-renders when passed as a
+ * prop or avoid re-executing effects when passed as a dependency
  */
-export function useCallbackRef<T extends (...args: any[]) => any>(callback?: T): T {
+export function useCallbackRef<T extends (...args: any[]) => any>(callback: T | undefined): T {
   const callbackRef = React.useRef(callback);
 
   React.useEffect(() => {
@@ -14,7 +13,7 @@ export function useCallbackRef<T extends (...args: any[]) => any>(callback?: T):
 
   return React.useCallback(
     ((...args) => {
-      callbackRef.current?.(...args);
+      return callbackRef.current?.(...args);
     }) as T,
     []
   );
