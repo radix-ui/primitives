15b61adc3c973e6017b2e0b2133fa5edbab1d025
Jenna Smith <jenna@growldesign.co.uk>
2021-01-15 23:14:22 +0000

    Add `selector` prop to `Arrow`

--- packages/react/arrow/src/Arrow.tsx
+++ packages/react/arrow/src/Arrow.tsx
@@ -1,21 +1,37 @@
 import * as React from 'react';
 import { forwardRefWithAs } from '@radix-ui/react-polymorphic';
-import { getPartDataAttrObj } from '@radix-ui/utils';
+import { getSelector, getSelectorObj } from '@radix-ui/utils';
 
 const NAME = 'Arrow';
 const DEFAULT_TAG = 'svg';
+
+type ArrowOwnProps = {
+  /**
+   * A string to use as the component selector for CSS purposes. It will be added as
+   * a data attribute. Pass `null` to remove selector.
+   *
+   * @defaultValue radix-arrow
+   */
+  selector?: string | null;
+};
 
 const Arrow = forwardRefWithAs<typeof ArrowImpl>((props, forwardedRef) => {
   const { as: Comp = ArrowImpl, ...arrowProps } = props;
   return <Comp {...arrowProps} ref={forwardedRef} />;
 });
 
-const ArrowImpl = forwardRefWithAs<typeof DEFAULT_TAG>((props, forwardedRef) => {
-  const { as: Comp = DEFAULT_TAG, width = 10, height = 5, ...arrowProps } = props;
+const ArrowImpl = forwardRefWithAs<typeof DEFAULT_TAG, ArrowOwnProps>((props, forwardedRef) => {
+  const {
+    as: Comp = DEFAULT_TAG,
+    selector = getSelector(NAME),
+    width = 10,
+    height = 5,
+    ...arrowProps
+  } = props;
   return (
     <Comp
-      {...getPartDataAttrObj(NAME)}
       {...arrowProps}
+      {...getSelectorObj(selector)}
       ref={forwardedRef}
       viewBox="0 0 30 10"
       width={width}
