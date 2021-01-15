7816af9423d1847f67972958d2af278f5608ae50
Jenna Smith <jenna@growldesign.co.uk>
2021-01-15 23:17:02 +0000

    Add `selector` prop to `VisuallyHidden`

--- packages/react/visually-hidden/src/VisuallyHidden.tsx
+++ packages/react/visually-hidden/src/VisuallyHidden.tsx
@@ -1,32 +1,50 @@
 import * as React from 'react';
-import { getPartDataAttrObj } from '@radix-ui/utils';
+import { getSelector, getSelectorObj } from '@radix-ui/utils';
 import { forwardRefWithAs } from '@radix-ui/react-polymorphic';
 
 const NAME = 'VisuallyHidden';
 const DEFAULT_TAG = 'span';
 
-const VisuallyHidden = forwardRefWithAs<typeof DEFAULT_TAG>(
-  ({ as: Comp = DEFAULT_TAG, style, ...visuallyHiddenProps }, forwardedRef) => (
-    <Comp
-      {...visuallyHiddenProps}
-      {...getPartDataAttrObj(NAME)}
-      ref={forwardedRef}
-      style={{
-        ...style,
-        // See: https://github.com/twbs/bootstrap/blob/master/scss/mixins/_screen-reader.scss
-        position: 'absolute',
-        border: 0,
-        width: 1,
-        height: 1,
-        padding: 0,
-        margin: -1,
-        overflow: 'hidden',
-        clip: 'rect(0, 0, 0, 0)',
-        whiteSpace: 'nowrap',
-        wordWrap: 'normal',
-      }}
-    />
-  )
+type VisuallyHiddenOwnProps = {
+  /**
+   * A string to use as the component selector for CSS purposes. It will be added as
+   * a data attribute. Pass `null` to remove selector.
+   *
+   * @defaultValue radix-visually-hidden
+   */
+  selector?: string | null;
+};
+
+const VisuallyHidden = forwardRefWithAs<typeof DEFAULT_TAG, VisuallyHiddenOwnProps>(
+  (props, forwardedRef) => {
+    const {
+      as: Comp = DEFAULT_TAG,
+      selector = getSelector(NAME),
+      style,
+      ...visuallyHiddenProps
+    } = props;
+    return (
+      <Comp
+        {...visuallyHiddenProps}
+        {...getSelectorObj(selector)}
+        ref={forwardedRef}
+        style={{
+          ...style,
+          // See: https://github.com/twbs/bootstrap/blob/master/scss/mixins/_screen-reader.scss
+          position: 'absolute',
+          border: 0,
+          width: 1,
+          height: 1,
+          padding: 0,
+          margin: -1,
+          overflow: 'hidden',
+          clip: 'rect(0, 0, 0, 0)',
+          whiteSpace: 'nowrap',
+          wordWrap: 'normal',
+        }}
+      />
+    );
+  }
 );
 
 VisuallyHidden.displayName = NAME;
