45e9c1cdf138a49416847f8c5f540817c09016c3
Jenna Smith <jenna@growldesign.co.uk>
2021-01-18 18:57:49 +0000

    Update `VisuallyHidden` to use `Primitive`

--- packages/react/visually-hidden/src/VisuallyHidden.tsx
+++ packages/react/visually-hidden/src/VisuallyHidden.tsx
@@ -1,50 +1,36 @@
 import * as React from 'react';
-import { getSelector, getSelectorObj } from '@radix-ui/utils';
+import { getSelector } from '@radix-ui/utils';
 import { forwardRefWithAs } from '@radix-ui/react-polymorphic';
+import { Primitive } from '@radix-ui/react-primitive';
+
+import type { OwnProps } from '@radix-ui/react-polymorphic';
 
 const NAME = 'VisuallyHidden';
 const DEFAULT_TAG = 'span';
 
-type VisuallyHiddenOwnProps = {
-  /**
-   * A string to use as the component selector for CSS purposes. It will be added as
-   * a data attribute. Pass `null` to remove selector.
-   *
-   * @defaultValue radix-visually-hidden
-   */
-  selector?: string | null;
-};
-
-const VisuallyHidden = forwardRefWithAs<typeof DEFAULT_TAG, VisuallyHiddenOwnProps>(
-  (props, forwardedRef) => {
-    const {
-      as: Comp = DEFAULT_TAG,
-      selector = getSelector(NAME),
-      style,
-      ...visuallyHiddenProps
-    } = props;
-    return (
-      <Comp
-        {...visuallyHiddenProps}
-        {...getSelectorObj(selector)}
-        ref={forwardedRef}
-        style={{
-          ...style,
-          // See: https://github.com/twbs/bootstrap/blob/master/scss/mixins/_screen-reader.scss
-          position: 'absolute',
-          border: 0,
-          width: 1,
-          height: 1,
-          padding: 0,
-          margin: -1,
-          overflow: 'hidden',
-          clip: 'rect(0, 0, 0, 0)',
-          whiteSpace: 'nowrap',
-          wordWrap: 'normal',
-        }}
-      />
-    );
-  }
+const VisuallyHidden = forwardRefWithAs<typeof DEFAULT_TAG, OwnProps<typeof Primitive>>(
+  (props, forwardedRef) => (
+    <Primitive
+      as={DEFAULT_TAG}
+      selector={getSelector(NAME)}
+      {...props}
+      ref={forwardedRef}
+      style={{
+        ...props.style,
+        // See: https://github.com/twbs/bootstrap/blob/master/scss/mixins/_screen-reader.scss
+        position: 'absolute',
+        border: 0,
+        width: 1,
+        height: 1,
+        padding: 0,
+        margin: -1,
+        overflow: 'hidden',
+        clip: 'rect(0, 0, 0, 0)',
+        whiteSpace: 'nowrap',
+        wordWrap: 'normal',
+      }}
+    />
+  )
 );
 
 VisuallyHidden.displayName = NAME;
