f20e79a392aac1e9677e7b81ea76b1bd2a4e32c9
Chance Strickland <hi@chancedigital.io>
2021-01-19 08:58:58 -0800

    simplify stories

--- packages/react/toggle-group/src/ToggleGroup.stories.tsx
+++ packages/react/toggle-group/src/ToggleGroup.stories.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
-import { ToggleGroupItem, MultiSelectToggleGroup, ToggleGroup } from './ToggleGroup';
+import {
+  ToggleGroupItem,
+  MultiSelectToggleGroup,
+  MultiSelectToggleGroupItem,
+  ToggleGroup,
+} from './ToggleGroup';
 import { styled } from '../../../../stitches.config';
 import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
 
-export default { title: 'Components/ToggleGroup' };
+export default {
+  title: 'Components/ToggleGroup',
+};
 
 export const Styled = () => {
   return (
@@ -14,14 +21,7 @@ export const Styled = () => {
       <ToggleGroupItem value="2" as={StyledItem}>
         Option 2
       </ToggleGroupItem>
-      <ToggleGroupItem
-        value="3"
-        as={StyledItem}
-        onToggledChange={() => {
-          // Make sure onToggledChange fires even in grouped buttons
-          console.log('Button 3 has changed!');
-        }}
-      >
+      <ToggleGroupItem value="3" as={StyledItem}>
         Option 3
       </ToggleGroupItem>
     </ToggleGroup>
@@ -29,24 +29,22 @@ export const Styled = () => {
 };
 
 export const Controlled = () => {
-  const [value, setValue] = React.useState('1');
+  const [value, setValue] = React.useState<string | null>(null);
+  function handleChange(value: string | null) {
+    setValue((prevValue) => {
+      return value == null ? prevValue : value;
+    });
+  }
 
   return (
-    <ToggleGroup aria-label="Options" value={value} onValueChange={setValue}>
+    <ToggleGroup aria-label="Options" value={value} onValueChange={handleChange}>
       <ToggleGroupItem value="1" as={StyledItem}>
         Option 1
       </ToggleGroupItem>
       <ToggleGroupItem value="2" as={StyledItem}>
         Option 2
       </ToggleGroupItem>
-      <ToggleGroupItem
-        value="3"
-        as={StyledItem}
-        onToggledChange={() => {
-          // Make sure onToggledChange fires even in grouped buttons
-          console.log('Button 3 has changed!');
-        }}
-      >
+      <ToggleGroupItem value="3" as={StyledItem}>
         Option 3
       </ToggleGroupItem>
     </ToggleGroup>
@@ -66,14 +64,7 @@ export const WithRovingFocus = () => {
         <ToggleGroupItem value="2" as={StyledItem}>
           Option 2
         </ToggleGroupItem>
-        <ToggleGroupItem
-          value="3"
-          as={StyledItem}
-          onToggledChange={() => {
-            // Make sure onToggledChange fires even in grouped buttons
-            console.log('Button 3 has changed!');
-          }}
-        >
+        <ToggleGroupItem value="3" as={StyledItem}>
           Option 3
         </ToggleGroupItem>
       </ToggleGroup>
@@ -81,28 +72,70 @@ export const WithRovingFocus = () => {
   );
 };
 
+export const MultiSelect = () => {
+  return (
+    <div>
+      <MultiSelectToggleGroup aria-label="Options" defaultValue={['1']}>
+        <MultiSelectToggleGroupItem value="1" as={StyledItem}>
+          Option 1
+        </MultiSelectToggleGroupItem>
+        <MultiSelectToggleGroupItem value="2" as={StyledItem}>
+          Option 2
+        </MultiSelectToggleGroupItem>
+        <MultiSelectToggleGroupItem value="3" as={StyledItem}>
+          Option 3
+        </MultiSelectToggleGroupItem>
+      </MultiSelectToggleGroup>
+    </div>
+  );
+};
+
 export const ControlledMultiSelect = () => {
   const [value, setValue] = React.useState<string[]>([]);
+  function handleChange(value: string[]) {
+    setValue((prevValue) => {
+      return value.length < 1 ? prevValue : value;
+    });
+  }
 
   return (
     <div>
-      <MultiSelectToggleGroup aria-label="Options" value={value} onValueChange={setValue}>
-        <ToggleGroupItem value="1" as={StyledItem}>
+      <MultiSelectToggleGroup aria-label="Options" value={value} onValueChange={handleChange}>
+        <MultiSelectToggleGroupItem value="1" as={StyledItem}>
           Option 1
-        </ToggleGroupItem>
-        <ToggleGroupItem value="2" as={StyledItem}>
+        </MultiSelectToggleGroupItem>
+        <MultiSelectToggleGroupItem value="2" as={StyledItem}>
           Option 2
-        </ToggleGroupItem>
-        <ToggleGroupItem
-          value="3"
-          as={StyledItem}
-          onToggledChange={() => {
-            // Make sure onToggledChange fires even in grouped buttons
-            console.log('Button 3 has changed!');
-          }}
-        >
+        </MultiSelectToggleGroupItem>
+        <MultiSelectToggleGroupItem value="3" as={StyledItem}>
           Option 3
-        </ToggleGroupItem>
+        </MultiSelectToggleGroupItem>
+      </MultiSelectToggleGroup>
+    </div>
+  );
+};
+
+export const MultiSelectWithRovingFocus = () => {
+  return (
+    <div>
+      <VisuallyHidden id="desc">
+        To navigate the buttons within the group, use the arrow keys
+      </VisuallyHidden>
+      <MultiSelectToggleGroup
+        aria-label="Options"
+        aria-describedby="desc"
+        defaultValue={['1']}
+        rovingFocus
+      >
+        <MultiSelectToggleGroupItem value="1" as={StyledItem}>
+          Option 1
+        </MultiSelectToggleGroupItem>
+        <MultiSelectToggleGroupItem value="2" as={StyledItem}>
+          Option 2
+        </MultiSelectToggleGroupItem>
+        <MultiSelectToggleGroupItem value="3" as={StyledItem}>
+          Option 3
+        </MultiSelectToggleGroupItem>
       </MultiSelectToggleGroup>
     </div>
   );
