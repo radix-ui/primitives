fad3e5df8400c6a8eca60f15d95f5f1389d41b37
Jenna Smith <jenna@growldesign.co.uk>
2021-01-20 15:32:48 +0000

    Update `VisuallyHidden` with polymorphic type

--- packages/react/visually-hidden/src/VisuallyHidden.tsx
+++ packages/react/visually-hidden/src/VisuallyHidden.tsx
@@ -1,37 +1,40 @@
 import * as React from 'react';
 import { getSelector } from '@radix-ui/utils';
-import { forwardRefWithAs } from '@radix-ui/react-polymorphic';
 import { Primitive } from '@radix-ui/react-primitive';
 
-import type { OwnProps } from '@radix-ui/react-polymorphic';
+import type * as Polymorphic from '@radix-ui/react-polymorphic';
 
 const NAME = 'VisuallyHidden';
 const DEFAULT_TAG = 'span';
 
-const VisuallyHidden = forwardRefWithAs<typeof DEFAULT_TAG, OwnProps<typeof Primitive>>(
-  (props, forwardedRef) => (
-    <Primitive
-      as={DEFAULT_TAG}
-      selector={getSelector(NAME)}
-      {...props}
-      ref={forwardedRef}
-      style={{
-        ...props.style,
-        // See: https://github.com/twbs/bootstrap/blob/master/scss/mixins/_screen-reader.scss
-        position: 'absolute',
-        border: 0,
-        width: 1,
-        height: 1,
-        padding: 0,
-        margin: -1,
-        overflow: 'hidden',
-        clip: 'rect(0, 0, 0, 0)',
-        whiteSpace: 'nowrap',
-        wordWrap: 'normal',
-      }}
-    />
-  )
-);
+type VisuallyHiddenOwnProps = Polymorphic.OwnProps<typeof Primitive>;
+type VisuallyHiddenPrimitive = Polymorphic.ForwardRefComponent<
+  typeof DEFAULT_TAG,
+  VisuallyHiddenOwnProps
+>;
+
+const VisuallyHidden = React.forwardRef((props, forwardedRef) => (
+  <Primitive
+    as={DEFAULT_TAG}
+    selector={getSelector(NAME)}
+    {...props}
+    ref={forwardedRef}
+    style={{
+      ...props.style,
+      // See: https://github.com/twbs/bootstrap/blob/master/scss/mixins/_screen-reader.scss
+      position: 'absolute',
+      border: 0,
+      width: 1,
+      height: 1,
+      padding: 0,
+      margin: -1,
+      overflow: 'hidden',
+      clip: 'rect(0, 0, 0, 0)',
+      whiteSpace: 'nowrap',
+      wordWrap: 'normal',
+    }}
+  />
+)) as VisuallyHiddenPrimitive;
 
 VisuallyHidden.displayName = NAME;
 
