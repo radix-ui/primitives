a5c358b70d87b0ef4a8ce2ed6da8b9200331204c
Jenna Smith <jenna@growldesign.co.uk>
2021-01-20 15:34:21 +0000

    Update `Label` with polymorphic type

--- packages/react/label/src/Label.tsx
+++ packages/react/label/src/Label.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { getSelector } from '@radix-ui/utils';
 import { useId, useComposedRefs } from '@radix-ui/react-utils';
-import { forwardRefWithAs } from '@radix-ui/react-polymorphic';
 import { Primitive } from '@radix-ui/react-primitive';
 
-import type { MergeOwnProps } from '@radix-ui/react-polymorphic';
+import type * as Polymorphic from '@radix-ui/react-polymorphic';
+import type { Merge } from '@radix-ui/utils';
 
 /* -------------------------------------------------------------------------------------------------
  * Label
@@ -13,79 +13,76 @@ import type { MergeOwnProps } from '@radix-ui/react-polymorphic';
 const NAME = 'Label';
 const DEFAULT_TAG = 'span';
 
-type LabelOwnProps = {
-  htmlFor?: string;
-};
+type LabelOwnProps = Merge<Polymorphic.OwnProps<typeof Primitive>, { htmlFor?: string }>;
+type LabelPrimitive = Polymorphic.ForwardRefComponent<typeof DEFAULT_TAG, LabelOwnProps>;
 
 type LabelContextValue = { id: string; ref: React.RefObject<HTMLSpanElement> };
 const LabelContext = React.createContext<LabelContextValue | undefined>(undefined);
 
-const Label = forwardRefWithAs<typeof DEFAULT_TAG, MergeOwnProps<typeof Primitive, LabelOwnProps>>(
-  (props, forwardedRef) => {
-    const { htmlFor, id: idProp, children, ...labelProps } = props;
-    const labelRef = React.useRef<HTMLSpanElement>(null);
-    const ref = useComposedRefs(forwardedRef, labelRef);
-    const generatedId = `label-${useId()}`;
-    const id = idProp || generatedId;
+const Label = React.forwardRef((props, forwardedRef) => {
+  const { htmlFor, id: idProp, children, ...labelProps } = props;
+  const labelRef = React.useRef<HTMLSpanElement>(null);
+  const ref = useComposedRefs(forwardedRef, labelRef);
+  const generatedId = `label-${useId()}`;
+  const id = idProp || generatedId;
 
-    React.useEffect(() => {
+  React.useEffect(() => {
+    const label = labelRef.current;
+
+    if (label) {
+      const handleMouseDown = (event: MouseEvent) => {
+        if (event.detail > 1) event.preventDefault();
+      };
+
+      // prevent text selection when double clicking label
+      label.addEventListener('mousedown', handleMouseDown);
+      return () => label.removeEventListener('mousedown', handleMouseDown);
+    }
+  }, [labelRef]);
+
+  React.useEffect(() => {
+    if (htmlFor) {
+      const element = document.getElementById(htmlFor);
       const label = labelRef.current;
 
-      if (label) {
-        const handleMouseDown = (event: MouseEvent) => {
-          if (event.detail > 1) event.preventDefault();
+      if (label && element) {
+        const removeLabelClickEventListener = addLabelClickEventListener(label, element);
+        const getAriaLabel = () => element.getAttribute('aria-labelledby');
+        const ariaLabelledBy = [getAriaLabel(), id].filter(Boolean).join(' ');
+        element.setAttribute('aria-labelledby', ariaLabelledBy);
+
+        return () => {
+          removeLabelClickEventListener();
+          /**
+           * We get the latest attribute value because at the time that this cleanup fires,
+           * the values from the closure may have changed.
+           */
+          const ariaLabelledBy = getAriaLabel()?.replace(id, '');
+          if (ariaLabelledBy === '') {
+            element.removeAttribute('aria-labelledby');
+          } else if (ariaLabelledBy) {
+            element.setAttribute('aria-labelledby', ariaLabelledBy);
+          }
         };
-
-        // prevent text selection when double clicking label
-        label.addEventListener('mousedown', handleMouseDown);
-        return () => label.removeEventListener('mousedown', handleMouseDown);
       }
-    }, [labelRef]);
+    }
+  }, [id, htmlFor]);
 
-    React.useEffect(() => {
-      if (htmlFor) {
-        const element = document.getElementById(htmlFor);
-        const label = labelRef.current;
-
-        if (label && element) {
-          const removeLabelClickEventListener = addLabelClickEventListener(label, element);
-          const getAriaLabel = () => element.getAttribute('aria-labelledby');
-          const ariaLabelledBy = [getAriaLabel(), id].filter(Boolean).join(' ');
-          element.setAttribute('aria-labelledby', ariaLabelledBy);
-
-          return () => {
-            removeLabelClickEventListener();
-            /**
-             * We get the latest attribute value because at the time that this cleanup fires,
-             * the values from the closure may have changed.
-             */
-            const ariaLabelledBy = getAriaLabel()?.replace(id, '');
-            if (ariaLabelledBy === '') {
-              element.removeAttribute('aria-labelledby');
-            } else if (ariaLabelledBy) {
-              element.setAttribute('aria-labelledby', ariaLabelledBy);
-            }
-          };
-        }
-      }
-    }, [id, htmlFor]);
-
-    return (
-      <Primitive
-        as={DEFAULT_TAG}
-        selector={getSelector(NAME)}
-        {...labelProps}
-        ref={ref}
-        id={id}
-        role="label"
-      >
-        <LabelContext.Provider value={React.useMemo(() => ({ id, ref: labelRef }), [id])}>
-          {children}
-        </LabelContext.Provider>
-      </Primitive>
-    );
-  }
-);
+  return (
+    <Primitive
+      as={DEFAULT_TAG}
+      selector={getSelector(NAME)}
+      {...labelProps}
+      ref={ref}
+      id={id}
+      role="label"
+    >
+      <LabelContext.Provider value={React.useMemo(() => ({ id, ref: labelRef }), [id])}>
+        {children}
+      </LabelContext.Provider>
+    </Primitive>
+  );
+}) as LabelPrimitive;
 
 Label.displayName = 'Label';
 
