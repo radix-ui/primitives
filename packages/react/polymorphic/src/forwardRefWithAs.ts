23895675e1d37e7447a19a531b16a65a8047aa85
Jenna Smith <jenna@modulz.app>
2021-02-03 12:00:27 +0000

    [Polymorphic] Fix type autocompletion for polymorphic components (#421)
    * Fix type autocompletion in Polymorphic types
    
    * Versions

--- packages/react/polymorphic/src/forwardRefWithAs.ts
+++ packages/react/polymorphic/src/forwardRefWithAs.ts
@@ -18,6 +18,8 @@ type OwnProps<E> = E extends ForwardRefComponent<any, infer P> ? P : {};
  */
 type IntrinsicElement<E> = E extends ForwardRefComponent<infer I, any> ? I : never;
 
+type NarrowIntrinsic<E> = E extends keyof JSX.IntrinsicElements ? E : never;
+
 /* -------------------------------------------------------------------------------------------------
  * ForwardRefComponent
  * -----------------------------------------------------------------------------------------------*/
@@ -33,31 +35,26 @@ interface ForwardRefComponent<
     MergeProps<IntrinsicElementString, OwnProps & { as?: IntrinsicElementString }>
   > {
   /**
-   * When passing an `as` prop as a component, use this overload.
-   * Merges orignal own props (without DOM props) and the inferred props
-   * from `as` element with the own props taking precendence.
-   *
-   * We don't use `React.ComponentType` here as we get type errors
-   * when consumers try to do inline `as` components.
-   */
-  <
-    As extends React.ElementType<any>,
-    // Inferring with props so inline `as` components get implicit `any` errors
-    // e.g. `as={(props) => {}}` <-- `props` errors
-    _AsWithProps = As extends React.ElementType<infer P> ? React.ElementType<P> : never
-  >(
-    props: MergeProps<_AsWithProps, OwnProps & { as: _AsWithProps }>
-  ): React.ReactElement | null;
-
-  /**
    * When passing an `as` prop as a string, use this overload.
-   * Merges orignal own props (without DOM props) and the inferred props
+   * Merges original own props (without DOM props) and the inferred props
    * from `as` element with the own props taking precendence.
    *
    * We explicitly define a `JSX.IntrinsicElements` overload so that
    * events are typed for consumers.
    */
-  <As extends keyof JSX.IntrinsicElements>(
+  <As extends keyof JSX.IntrinsicElements = NarrowIntrinsic<IntrinsicElementString>>(
+    props: MergeProps<As, OwnProps & { as: As }>
+  ): React.ReactElement | null;
+
+  /**
+   * When passing an `as` prop as a component, use this overload.
+   * Merges original own props (without DOM props) and the inferred props
+   * from `as` element with the own props taking precendence.
+   *
+   * We don't use `React.ComponentType` here as we get type errors
+   * when consumers try to do inline `as` components.
+   */
+  <As extends React.ElementType>(
     props: MergeProps<As, OwnProps & { as: As }>
   ): React.ReactElement | null;
 }
