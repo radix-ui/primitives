3124d17adbe2f11e927ca7c3fb1a7a5a605f925a
Benoît Grélard <benoit.grelard@gmail.com>
2021-02-05 12:55:25 +0000

    [Slot] Add chromatic story (#434)

--- packages/react/slot/src/Slot.stories.tsx
+++ packages/react/slot/src/Slot.stories.tsx
@@ -15,6 +15,21 @@ export const WithSlottable = () => (
   </AsCompWithSlottable>
 );
 
+export const Chromatic = () => (
+  <>
+    <h1>Without slottable</h1>
+    <AsCompWithoutSlottable as={Slot}>
+      <b data-slot-element>hello</b>
+    </AsCompWithoutSlottable>
+
+    <h1>With slottable</h1>
+    <AsCompWithSlottable as={Slot}>
+      <b data-slot-element>hello</b>
+    </AsCompWithSlottable>
+  </>
+);
+Chromatic.parameters = { chromatic: { disable: false } };
+
 type AsCompProps = React.ComponentProps<'div'> & { as: React.ElementType };
 
 const AsCompWithoutSlottable = ({ as: Comp = 'div', ...props }: AsCompProps) => <Comp {...props} />;
