48f8ad0b5eb9bed7c8a7e294760f559c9f85ae85
Ar Nazeh <Ar.nazeh@gmail.com>
2021-02-10 15:52:59 +0300

    New component: Toolbar (#412)

---
+++ packages/react/toolbar/src/Toolbar.stories.tsx
@@ -0,0 +1,188 @@
+import * as React from 'react';
+import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarItem } from './Toolbar';
+import { css } from '../../../../stitches.config';
+import { ToggleButton } from '@radix-ui/react-toggle-button';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuArrow,
+} from '@radix-ui/react-dropdown-menu';
+
+import { styledComponents } from '../../menu/src/Menu.stories';
+
+const { StyledRoot: StyledDropDownMenu, StyledItem: StyledDropdownItem } = styledComponents;
+
+export default { title: 'Components/Toolbar' };
+
+export const Styled = () => {
+  return (
+    <>
+      <ToolbarExample title="Horizontal"></ToolbarExample>
+      <ToolbarExample title="Vertical" orientation="vertical"></ToolbarExample>
+    </>
+  );
+};
+
+const ToolbarExample = ({ title, orientation }: any) => {
+  const anchorRef = React.useRef<HTMLAnchorElement>(null);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const uniqueInputID = 'input-' + (Math.random() * 10).toFixed(0);
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <Toolbar
+        className={toolbarClass}
+        orientation={orientation}
+        loop={true}
+        aria-label={`${title} toolbar`}
+      >
+        <ToolbarItem className={toolbarItemClass} as={ToggleButton}>
+          Toggle
+        </ToolbarItem>
+        <ToolbarSeparator className={toolbarSeparatorClass}></ToolbarSeparator>
+        <ToolbarGroup className={toolbarGroupClass}>
+          <div className="toolbar-label">A group</div>
+          <label className={toolbarItemClass} htmlFor={uniqueInputID}>
+            <span>Input: </span>
+            <ToolbarItem
+              ref={inputRef}
+              id={uniqueInputID}
+              defaultValue="42"
+              as={'input'}
+              style={{ maxWidth: 50 }}
+            ></ToolbarItem>
+          </label>
+          <ToolbarItem
+            className={toolbarItemClass}
+            as={'button'}
+            onClick={(event) => window.alert('The answer is: ' + inputRef.current?.value)}
+          >
+            Submit
+          </ToolbarItem>
+        </ToolbarGroup>
+        <ToolbarSeparator className={toolbarSeparatorClass}></ToolbarSeparator>
+        <DropdownMenu>
+          <ToolbarItem className={toolbarItemClass} as={DropdownMenuTrigger}>
+            Menu
+          </ToolbarItem>
+          <DropdownMenuContent as={StyledDropDownMenu}>
+            <DropdownMenuItem as={StyledDropdownItem} onSelect={() => window.alert('undo')}>
+              Undo
+            </DropdownMenuItem>
+            <DropdownMenuItem as={StyledDropdownItem} onSelect={() => window.alert('redo')}>
+              Redo
+            </DropdownMenuItem>
+            <DropdownMenuArrow />
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <ToolbarItem className={toolbarItemClass} as={'button'} disabled>
+          Disabled
+        </ToolbarItem>
+        <ToolbarItem
+          as={'a'}
+          className={linkClass}
+          href="https://www.w3.org/TR/2019/WD-wai-aria-practices-1.2-20191218/examples/toolbar/toolbar.html"
+          target="_blank"
+          ref={anchorRef}
+          onKeyDown={(event) => {
+            if (event.key === ' ') {
+              anchorRef.current?.click();
+            }
+          }}
+        >
+          Examle
+        </ToolbarItem>
+      </Toolbar>
+    </>
+  );
+};
+
+const RECOMMENDED_CSS__TOOLBAR = {
+  // ensures things are layed out correctly by default
+  display: 'flex',
+  '&[data-orientation="vertical"]': {
+    flexDirection: 'column',
+  },
+};
+
+const toolbarClass = css({
+  ...RECOMMENDED_CSS__TOOLBAR,
+  display: 'inline-flex',
+  gap: 5,
+  boxSizing: 'border-box',
+  minWidth: 130,
+  backgroundColor: '$white',
+  border: '1px solid $gray100',
+  borderRadius: 6,
+  padding: 5,
+  boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.1)',
+  fontFamily: 'apple-system, BlinkMacSystemFont, helvetica, arial, sans-serif',
+  fontSize: 13,
+
+  '&:focus-within': {
+    borderColor: '$black',
+  },
+});
+
+const toolbarGroupClass = css({
+  ...RECOMMENDED_CSS__TOOLBAR,
+  gap: 5,
+  '.toolbar-label': {
+    padding: 0,
+    color: '$gray100',
+    textAlign: 'center',
+  },
+
+  '&[data-orientation="horizontal"]': {
+    '.toolbar-label': {
+      alignSelf: 'center',
+    },
+  },
+});
+
+const toolbarSeparatorClass = css({
+  width: 1,
+  margin: 5,
+  backgroundColor: '$gray100',
+
+  '&[data-orientation="vertical"]': {
+    height: 1,
+    width: 'auto',
+  },
+});
+
+const toolbarItemClass = css({
+  border: '1px solid $black',
+  borderRadius: 6,
+  backgroundColor: 'transparent',
+  padding: '5px 10px',
+  fontFamily: 'apple-system, BlinkMacSystemFont, helvetica, arial, sans-serif',
+  fontSize: 13,
+
+  display: 'flex',
+  alignItems: 'center',
+
+  '&:focus, &:focus-within': {
+    outline: 'none',
+    boxShadow: '0 0 0 2px rgba(0, 0, 0, 0.5)',
+  },
+
+  '&[data-state="on"]': {
+    boxShadow: '0 0 0 1px rgba(0, 0, 0, 0.7)',
+  },
+
+  '&[data-disabled]': {
+    opacity: 0.5,
+    userSelect: 'none',
+  },
+});
+
+const linkClass = css(toolbarItemClass, {
+  display: 'inline-flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: 'black',
+});
---
+++ packages/react/toolbar/src/Toolbar.tsx
@@ -0,0 +1,205 @@
+import * as React from 'react';
+import { RovingFocusGroup, useRovingFocus } from '@radix-ui/react-roving-focus';
+import { Primitive } from '@radix-ui/react-primitive';
+import { getSelector } from '@radix-ui/utils';
+import { createContext, composeEventHandlers } from '@radix-ui/react-utils';
+import type * as Polymorphic from '@radix-ui/react-polymorphic';
+import type { Merge } from '@radix-ui/utils';
+import { Separator as SeparatorPrimitive } from '@radix-ui/react-separator';
+
+/* -------------------------------------------------------------------------------------------------
+ * Toolbar
+ * -----------------------------------------------------------------------------------------------*/
+
+const TOOLBAR_NAME = 'Toolbar';
+const TOOLBAR_CONTEXT_NAME = TOOLBAR_NAME + 'Context';
+
+type Orientation = React.AriaAttributes['aria-orientation'];
+
+type RovingFocusGroupProps = React.ComponentProps<typeof RovingFocusGroup>;
+
+type ToolbarOwnProps = Merge<
+  Polymorphic.OwnProps<typeof Primitive>,
+  {
+    /**
+     * The orientation of the toolbar.
+     * Mainly so arrow navigation is done accordingly (left & right vs. up & down)
+     * (default: horizontal)
+     */
+    orientation?: RovingFocusGroupProps['orientation'];
+    /**
+     * The direction of navigation between toolbar items.
+     * (default: ltr)
+     */
+    dir?: RovingFocusGroupProps['dir'];
+    /**
+     * Whether keyboard navigation should loop focus
+     * (default: true)
+     */
+    loop?: RovingFocusGroupProps['loop'];
+  }
+>;
+
+type ToolbarPrimitive = Polymorphic.ForwardRefComponent<
+  Polymorphic.IntrinsicElement<typeof Primitive>,
+  ToolbarOwnProps
+>;
+
+type ToolbarContextValue = {
+  name: typeof TOOLBAR_CONTEXT_NAME;
+  orientation: Orientation;
+};
+
+const [ToolbarContext, useToolbarContext] = createContext<ToolbarContextValue>(
+  TOOLBAR_CONTEXT_NAME,
+  TOOLBAR_NAME
+);
+
+const Toolbar = React.forwardRef((props, forwardedRef) => {
+  const {
+    'aria-label': ariaLabel,
+    'aria-labelledby': ariaLabelledBy,
+    'aria-controls': ariaControls,
+
+    orientation = 'horizontal',
+    dir = 'ltr',
+    loop = true,
+
+    ...toolbarProps
+  } = props;
+
+  const context: ToolbarContextValue = React.useMemo(
+    () => ({ name: TOOLBAR_CONTEXT_NAME, orientation }),
+    [orientation]
+  );
+
+  return (
+    <ToolbarContext.Provider value={context}>
+      <RovingFocusGroup orientation={orientation} dir={dir} loop={loop}>
+        <Primitive
+          role="toolbar"
+          aria-labelledby={ariaLabel ? undefined : ariaLabelledBy}
+          aria-label={ariaLabel || undefined}
+          aria-controls={ariaControls}
+          aria-orientation={orientation}
+          data-orientation={orientation}
+          {...toolbarProps}
+          selector={getSelector(TOOLBAR_NAME)}
+          ref={forwardedRef}
+        ></Primitive>
+      </RovingFocusGroup>
+    </ToolbarContext.Provider>
+  );
+}) as ToolbarPrimitive;
+
+/* -------------------------------------------------------------------------------------------------
+ * ToolbarGroup
+ * -----------------------------------------------------------------------------------------------*/
+
+const GROUP_NAME = 'ToolbarGroup';
+
+type ToolbarGroupOwnProps = Polymorphic.OwnProps<typeof Primitive>;
+type ToolbarGroupPrimitive = Polymorphic.ForwardRefComponent<
+  Polymorphic.IntrinsicElement<typeof Primitive>,
+  ToolbarGroupOwnProps
+>;
+
+const ToolbarGroup = React.forwardRef((props, forwardedRef) => {
+  const { orientation } = useToolbarContext(GROUP_NAME);
+
+  return (
+    <Primitive
+      role="group"
+      data-orientation={orientation}
+      {...props}
+      selector={getSelector(GROUP_NAME)}
+      ref={forwardedRef}
+    />
+  );
+}) as ToolbarGroupPrimitive;
+
+ToolbarGroup.displayName = GROUP_NAME;
+
+/* -------------------------------------------------------------------------------------------------
+ * ToolbarSeparator
+ * -----------------------------------------------------------------------------------------------*/
+
+const SEPARATOR_NAME = 'ToolbarSeparator';
+
+type ToolbarSeparatorOwnProps = Polymorphic.OwnProps<typeof Primitive>;
+type ToolbarSeparatorPrimitive = Polymorphic.ForwardRefComponent<
+  Polymorphic.IntrinsicElement<typeof SeparatorPrimitive>,
+  ToolbarSeparatorOwnProps
+>;
+
+const ToolbarSeparator = React.forwardRef((props, forwardedRef) => {
+  const { orientation } = useToolbarContext(GROUP_NAME);
+
+  return (
+    <SeparatorPrimitive
+      aria-orientation={orientation}
+      data-orientation={orientation}
+      {...props}
+      selector={getSelector(SEPARATOR_NAME)}
+      ref={forwardedRef}
+    ></SeparatorPrimitive>
+  );
+}) as ToolbarSeparatorPrimitive;
+
+ToolbarSeparator.displayName = SEPARATOR_NAME;
+
+/* -------------------------------------------------------------------------------------------------
+ * ToolbarItem
+ * -----------------------------------------------------------------------------------------------*/
+
+const ITEM_NAME = 'ToolbarItem';
+
+type ToolbarItemOwnProps = Merge<Polymorphic.OwnProps<typeof Primitive>, { disabled?: boolean }>;
+type ToolbarItemPrimitive = Polymorphic.ForwardRefComponent<
+  Polymorphic.IntrinsicElement<typeof Primitive>,
+  ToolbarItemOwnProps
+>;
+
+const ToolbarItem = React.forwardRef((props, forwardedRef) => {
+  const { disabled, ...itemProps } = props;
+
+  const rovingFocusProps = useRovingFocus({
+    disabled,
+    active: false,
+  });
+
+  return (
+    <Primitive
+      role="toolbaritem"
+      aria-disabled={disabled}
+      data-disabled={disabled ? '' : undefined}
+      {...itemProps}
+      selector={getSelector(ITEM_NAME)}
+      ref={forwardedRef}
+      {...rovingFocusProps}
+      // RovingFocus eventHandlers should not block item eventHandlers
+      onFocus={composeEventHandlers(itemProps.onFocus, rovingFocusProps.onFocus)}
+      onKeyDown={composeEventHandlers(itemProps.onKeyDown, rovingFocusProps.onKeyDown)}
+      onMouseDown={composeEventHandlers(itemProps.onMouseDown, rovingFocusProps.onMouseDown)}
+    />
+  );
+}) as ToolbarItemPrimitive;
+
+ToolbarItem.displayName = ITEM_NAME;
+
+const Root = Toolbar;
+const Group = ToolbarGroup;
+const Separator = ToolbarSeparator;
+const Item = ToolbarItem;
+
+export {
+  Toolbar,
+  ToolbarGroup,
+  ToolbarSeparator,
+  ToolbarItem,
+  //
+  Root,
+  Group,
+  Item,
+  Separator,
+};
