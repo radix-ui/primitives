8379a54842a55688b76fc4dd873b03bf54273bc5
Benoît Grélard <benoit.grelard@gmail.com>
2021-02-23 14:52:53 +0000

    Move ssr package to id package

---
+++ packages/react/id/src/id.tsx
@@ -0,0 +1,53 @@
+// This implementation is heavily inspired by react-aria's implementation
+// See: https://github.com/adobe/react-spectrum/blob/main/packages/%40react-aria/ssr/src/SSRProvider.tsx
+
+import * as React from 'react';
+
+type IdContextValue = {
+  prefix: number;
+  current: number;
+};
+
+const defaultIdContext: IdContextValue = {
+  prefix: Math.round(Math.random() * 10000000000),
+  current: 0,
+};
+
+const IdContext = React.createContext<IdContextValue>(defaultIdContext);
+
+const IdProvider: React.FC = (props) => {
+  const currentContext = React.useContext(IdContext);
+  const isRootIdProvider = currentContext === defaultIdContext;
+  const context: IdContextValue = React.useMemo(
+    () => ({
+      prefix: isRootIdProvider ? 0 : ++currentContext.prefix,
+      current: 0,
+    }),
+    [isRootIdProvider, currentContext]
+  );
+
+  return <IdContext.Provider value={context} {...props} />;
+};
+
+function useId(deterministicId?: string): string {
+  const context = React.useContext(IdContext);
+  const isDOMEnv = canUseDOM();
+
+  if (!isDOMEnv && context === defaultIdContext) {
+    console.warn(
+      'When server rendering, you must wrap your application in an <IdProvider> to ensure consistent ids are generated between the client and server.'
+    );
+  }
+
+  return React.useMemo(
+    () => deterministicId || `radix-id-${context.prefix}-${++context.current}`,
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [deterministicId]
+  );
+}
+
+function canUseDOM() {
+  return Boolean(typeof window !== 'undefined' && window.document && window.document.createElement);
+}
+
+export { IdProvider, useId };
--- ssr-testing/pages/index.tsx
+++ ssr-testing/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
       <Head>
@@ -13,7 +13,7 @@ export default function Home() {
         <h1>Components</h1>
         <ul>
           <li>
-            <Link href="/ssr">SSR</Link>
+            <Link href="/id">Id</Link>
           </li>
         </ul>
       </main>
