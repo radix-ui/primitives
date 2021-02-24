1094d36ac63d53fa6aee866a8543de597ad06a17
Jenna Smith <jenna@modulz.app>
2021-02-24 21:36:48 +0000

    Update `createContext` to expose a Provider that accepts props (#477)
    * Update context in `Accordion`
    
    * Versions
    
    * Only re-memoize when provider prop values change

--- packages/react/utils/src/createContext.tsx
+++ packages/react/utils/src/createContext.tsx
@@ -12,3 +12,29 @@ export function createContext<ContextValueType>(displayName: string, rootCompone
   }
   return [Context, useContext] as const;
 }
+
+export function createContextObj<ContextValueType extends object>(rootComponentName: string) {
+  const Context = React.createContext<ContextValueType>(null as any);
+
+  function Provider(props: ContextValueType & { children: React.ReactNode }) {
+    const { children, ...providerProps } = props;
+    // Only re-memoize when prop values change
+    const value = React.useMemo(
+      () => providerProps,
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      Object.values(providerProps)
+    ) as ContextValueType;
+    return <Context.Provider value={value}>{children}</Context.Provider>;
+  }
+
+  function useContext(componentName: string) {
+    const context = React.useContext(Context);
+    if (context === null) {
+      throw new Error(`\`${componentName}\` must be used within \`${rootComponentName}\``);
+    }
+    return context;
+  }
+
+  Provider.displayName = rootComponentName + 'Provider';
+  return [Provider, useContext] as const;
+}
