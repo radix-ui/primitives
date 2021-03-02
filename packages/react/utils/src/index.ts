8f64aa4223432d74c8f1a6b43f9d79c5778c26ec
Benoît Grélard <benoit.grelard@gmail.com>
2021-03-02 19:54:51 +0000

    [React utils refactor] new react-use-previous package (#523)

--- packages/react/utils/src/index.ts
+++ packages/react/utils/src/index.ts
@@ -4,6 +4,5 @@ export * from './useCallbackRef';
 export * from './useComposedRefs';
 export * from './useControlledState';
 export * from './useDocumentRef';
-export * from './usePrevious';
 export * from './useRect';
 export * from './useSize';
