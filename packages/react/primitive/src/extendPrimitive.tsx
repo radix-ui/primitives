6a30a343d38f8f83aeef128aba6ba389087ea190
Jenna Smith <jenna@growldesign.co.uk>
2021-03-05 15:19:13 +0000

    Support default `as` prop

--- packages/react/primitive/src/extendPrimitive.tsx
+++ packages/react/primitive/src/extendPrimitive.tsx
@@ -2,19 +2,24 @@ import * as React from 'react';
 
 import type * as Polymorphic from '@radix-ui/react-polymorphic';
 
-function extendPrimitive<As extends Polymorphic.ForwardRefComponent<any, any>>(
-  Comp: As extends Polymorphic.ForwardRefComponent<infer I, infer P>
-    ? Polymorphic.ForwardRefComponent<I, P>
-    : As,
-  config: { displayName?: string; defaultProps?: Partial<React.ComponentProps<typeof Comp>> }
-) {
+type ExtendedPrimitive<C, As> = Polymorphic.ForwardRefComponent<As, Polymorphic.OwnProps<C>>;
+type DefaultProps<C, As> = { as?: As } & Omit<
+  Partial<React.ComponentProps<ExtendedPrimitive<C, As>>>,
+  'as'
+>;
+
+function extendPrimitive<
+  C extends Polymorphic.ForwardRefComponent<any, any>,
+  DefaultAs extends React.ElementType = Polymorphic.IntrinsicElement<C>
+>(Comp: C, config: { displayName?: string; defaultProps?: DefaultProps<C, DefaultAs> }) {
   const Extended = React.forwardRef((props, forwardedRef) => {
     const As = Comp as any;
     const propsWithDefaults = { ...config.defaultProps, ...props };
     return <As {...propsWithDefaults} ref={forwardedRef} />;
-  }) as As;
+  });
+
   Extended.displayName = config.displayName || 'Extended' + Comp.displayName;
-  return Extended;
+  return Extended as ExtendedPrimitive<C, DefaultAs>;
 }
 
 export { extendPrimitive };
