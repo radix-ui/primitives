6227827ac74d09604d8feb88176dda7f93f0e9f2
Jenna Smith <jenna@growldesign.co.uk>
2021-04-29 20:21:21 +0100

    Prevent `useDebounceCallback` from triggering after unmount

--- packages/react/scroll-area/src/ScrollArea.tsx
+++ packages/react/scroll-area/src/ScrollArea.tsx
@@ -899,6 +899,7 @@ const addUnlinkedScrollListener = (node: HTMLElement, handler = () => {}) => {
 function useDebounceCallback(callback: () => void, delay: number) {
   const handleCallback = useCallbackRef(callback);
   const debounceTimerRef = React.useRef(0);
+  React.useEffect(() => () => window.clearTimeout(debounceTimerRef.current), []);
   return React.useCallback(() => {
     window.clearTimeout(debounceTimerRef.current);
     debounceTimerRef.current = window.setTimeout(handleCallback, delay);
