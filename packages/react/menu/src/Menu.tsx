16d3ec09d7ed6797bb62123c815d383407a0ff55
Jenna Smith <jenna@growldesign.co.uk>
2021-06-10 13:58:38 +0100

    Add `pointerdown` to keyboard user logic

--- packages/react/menu/src/Menu.tsx
+++ packages/react/menu/src/Menu.tsx
@@ -79,12 +79,14 @@ const Menu: React.FC<MenuOwnProps> = (props) => {
 
   React.useEffect(() => {
     const handleKeyDown = () => (isUsingKeyboardRef.current = true);
-    const handlePointerMove = () => (isUsingKeyboardRef.current = false);
+    const handlePointer = () => (isUsingKeyboardRef.current = false);
     document.addEventListener('keydown', handleKeyDown, { capture: true });
-    document.addEventListener('pointermove', handlePointerMove, { capture: true });
+    document.addEventListener('pointerdown', handlePointer, { capture: true });
+    document.addEventListener('pointermove', handlePointer, { capture: true });
     return () => {
       document.removeEventListener('keydown', handleKeyDown, { capture: true });
-      document.removeEventListener('pointermove', handlePointerMove, { capture: true });
+      document.removeEventListener('pointerdown', handlePointer, { capture: true });
+      document.removeEventListener('pointermove', handlePointer, { capture: true });
     };
   }, []);
 
