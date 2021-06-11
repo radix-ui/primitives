bc3758acb1f075343f7b7c5bcea47f85a4b6c366
Andy Hook <hello@andy-hook.co.uk>
2021-06-11 10:00:28 +0100

    Remove export of private func

--- packages/react/menu/src/Menu.tsx
+++ packages/react/menu/src/Menu.tsx
@@ -1079,7 +1079,7 @@ type GraceArea = { area: Polygon; side: Side };
 
 // Determine if a point is inside of a polygon.
 // Based on https://github.com/substack/point-in-polygon
-export function isPointInPolygon(point: Point, polygon: Polygon) {
+function isPointInPolygon(point: Point, polygon: Polygon) {
   const { x, y } = point;
   let inside = false;
   for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
