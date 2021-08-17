a76efedbbdac8dca7d6c75c602c666ebb11458c4
Jenna Smith <jenna@growldesign.co.uk>
2021-08-17 13:28:08 +0100

    Feedback

--- packages/react/menu/src/Menu.tsx
+++ packages/react/menu/src/Menu.tsx
@@ -739,16 +739,18 @@ const MenuSubTrigger = React.forwardRef((props, forwardedRef) => {
         data-state={getOpenState(context.open)}
         {...props}
         ref={composeRefs(forwardedRef, context.onTriggerChange)}
+        // This is redundant for mouse users but we cannot determine pointer type from
+        // click event and we cannot use pointerup event (see git history for reasons why)
         onClick={(event) => {
           props.onClick?.(event);
-          if (event.defaultPrevented) return;
-          if (!props.disabled && !context.open) context.onOpenChange(true);
+          if (props.disabled || event.defaultPrevented) return;
           /**
            * We manually focus because iOS Safari doesn't always focus on click (e.g. buttons)
            * and we rely heavily on `onFocusOutside` for submenus to close when switching
            * between separate submenus.
            */
           event.currentTarget.focus();
+          if (!context.open) context.onOpenChange(true);
         }}
         onPointerMove={composeEventHandlers(
           props.onPointerMove,
