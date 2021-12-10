2fd57251c00a63d4c06a8d14b4314a80db0bad82
Jenna Smith <jenna@growldesign.co.uk>
2021-12-10 10:15:07 +0000

    Fix `React.useId` check bundler issues

--- packages/react/id/src/id.tsx
+++ packages/react/id/src/id.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { useLayoutEffect } from '@radix-ui/react-use-layout-effect';
 
-const useReactId = (React as any).useId || (() => undefined);
+// We `toString()` to prevent bundlers from trying to `import { useId } from 'react';`
+const useReactId = (React as any)['useId'.toString()] || (() => undefined);
 let count = 0;
 
 function useId(deterministicId?: string): string {
