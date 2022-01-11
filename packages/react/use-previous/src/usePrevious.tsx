8bff6e13781b96f65b3f9db53aebc8a4311c0705
Andy Hook <hello@andy-hook.co.uk>
2022-01-11 16:34:17 +0000

    Tidy redundant comment

--- packages/react/use-previous/src/usePrevious.tsx
+++ packages/react/use-previous/src/usePrevious.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 
 function usePrevious<T>(value: T) {
-  // The ref object is a generic container whose current property is mutable ...
-  // ... and can hold any value, similar to an instance property on a class
   const ref = React.useRef({ value, previous: value });
 
   // We compare values before making an update to ensure that
