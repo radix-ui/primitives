36e0a1f2a90b57504ddb691c39deca8ac9cbf287
lita <luisorbaiceta@gmail.com>
2022-02-05 18:14:21 +0100

    Feat(tests): Describe component behaviour

--- packages/react/menubar/src/Menubar.tsx
+++ packages/react/menubar/src/Menubar.tsx
@@ -1,5 +1,24 @@
 import * as React from 'react';
+// @ts-ignore
+import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
 
-const Menubar: React.FC = () => <div>Menubar Dummy</div>;
+const MenubarRoot: React.FC = (props) => (
+  <nav>
+    <div role="menubar" {...props} />
+  </nav>
+);
+const MenubarMenu: React.FC = (props) => <div {...props} />;
+const MenubarTrigger: React.FC = (props) => <div {...props} />;
+const MenubarContent: React.FC = (props) => <div {...props} />;
+const MenubarItem: React.FC = (props) => <MenubarItemImpl {...props} />;
+const MenubarItemImpl: React.FC = (props) => <div {...props} />;
+const MenubarArrow: React.FC = (props) => <div {...props} />;
 
-export { Menubar as Root };
+export {
+  MenubarRoot as Root,
+  MenubarMenu as Menu,
+  MenubarItem as Item,
+  MenubarContent as Content,
+  MenubarTrigger as Trigger,
+  MenubarArrow as Arrow,
+};
