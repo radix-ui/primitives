2ec857c0d89015c68bff613167a0c74641927003
Benoît Grélard <benoit.grelard@gmail.com>
2022-02-23 19:49:20 +0000

    [Select] SSR (#1184)
    * [Select] SSR
    
    * Fix SSR
    
    * Fix forwardRef error from slot
    
    * Versions
    
    * Add comment
    
    Co-authored-by: Jenna Smith <jenna@growldesign.co.uk>

--- packages/react/select/src/Select.tsx
+++ packages/react/select/src/Select.tsx
@@ -297,21 +297,26 @@ interface SelectContentProps extends SelectContentImplProps {}
 const SelectContent = React.forwardRef<SelectContentElement, SelectContentProps>(
   (props: ScopedProps<SelectContentProps>, forwardedRef) => {
     const context = useSelectContext(CONTENT_NAME, props.__scopeSelect);
-    const fragmentRef = React.useRef(new DocumentFragment());
+    const [fragment, setFragment] = React.useState<DocumentFragment>();
+
+    // setting the fragment in `useLayoutEffect` as `DocumentFragment` doesn't exist on the server
+    useLayoutEffect(() => {
+      setFragment(new DocumentFragment());
+    }, []);
 
     return context.open ? (
       <SelectContentImpl {...props} ref={forwardedRef} />
-    ) : (
+    ) : fragment ? (
       ReactDOM.createPortal(
-        <Collection.Slot scope={props.__scopeSelect}>
-          {/* @ts-ignore: This is to avoid the "SelectViewport must be inside "SelectContent" error */}
-          <SelectContentContextProvider scope={props.__scopeSelect}>
+        // @ts-ignore: This is to avoid the "SelectViewport must be inside "SelectContent" error
+        <SelectContentContextProvider scope={props.__scopeSelect}>
+          <Collection.Slot scope={props.__scopeSelect}>
             <div>{props.children}</div>
-          </SelectContentContextProvider>
-        </Collection.Slot>,
-        fragmentRef.current as any
+          </Collection.Slot>
+        </SelectContentContextProvider>,
+        fragment as any
       )
-    );
+    ) : null;
   }
 );
 
