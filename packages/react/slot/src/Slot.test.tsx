89d10fc99af205411a0fcf2eab0ab10bf1115223
Benoît Grélard <benoit.grelard@gmail.com>
2022-05-06 11:57:52 +0100

    Add tests

--- packages/react/slot/src/Slot.test.tsx
+++ packages/react/slot/src/Slot.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import { Slot } from '@radix-ui/react-slot';
+import { Slot, Slottable } from '@radix-ui/react-slot';
 
 describe('given a slotted Trigger', () => {
   describe('with onClick on itself', () => {
@@ -108,6 +108,75 @@ describe('given a slotted Trigger', () => {
   });
 });
 
+describe('given a Button with Slottable', () => {
+  describe('without asChild', () => {
+    beforeEach(() => {
+      render(
+        <Button
+          iconLeft={<span data-testid="left">left</span>}
+          iconRight={<span data-testid="right">right</span>}
+        >
+          Click <em>me</em>!
+        </Button>
+      );
+    });
+
+    it('should render a button with icon on the left/right', async () => {
+      const button = screen.getByText(/click/i);
+      const iconLeft = screen.getByTestId('left');
+      const iconRight = screen.getByTestId('right');
+      expect(button.nodeName).toMatch(/button/i);
+      expect(button.childNodes.length).toBe(5);
+      expect(button.childNodes[0]).toBe(iconLeft);
+      expect(button.childNodes[button.childNodes.length - 1]).toBe(iconRight);
+    });
+  });
+
+  describe('with asChild', () => {
+    beforeEach(() => {
+      render(
+        <Button
+          iconLeft={<span data-testid="left">left</span>}
+          iconRight={<span data-testid="right">right</span>}
+          asChild
+        >
+          <a href="https://radix-ui.com">
+            Click <em>me</em>!
+          </a>
+        </Button>
+      );
+    });
+
+    it('should render a link with icon on the left/right', async () => {
+      const link = screen.getByText(/click/i);
+      const iconLeft = screen.getByTestId('left');
+      const iconRight = screen.getByTestId('right');
+      expect(link.nodeName).toMatch(/a/i);
+      expect(link.childNodes.length).toBe(5);
+      expect(link.childNodes[0]).toBe(iconLeft);
+      expect(link.childNodes[link.childNodes.length - 1]).toBe(iconRight);
+    });
+  });
+});
+
 type TriggerProps = React.ComponentProps<'button'> & { as: React.ElementType };
 
 const Trigger = ({ as: Comp = 'button', ...props }: TriggerProps) => <Comp {...props} />;
+
+const Button = React.forwardRef<
+  React.ElementRef<'button'>,
+  React.ComponentProps<'button'> & {
+    asChild?: boolean;
+    iconLeft?: React.ReactNode;
+    iconRight?: React.ReactNode;
+  }
+>(({ children, asChild = false, iconLeft, iconRight, ...props }, forwardedRef) => {
+  const Comp = asChild ? Slot : 'button';
+  return (
+    <Comp {...props} ref={forwardedRef}>
+      {iconLeft}
+      <Slottable>{children}</Slottable>
+      {iconRight}
+    </Comp>
+  );
+});
