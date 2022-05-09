c6deb468a698e04bde554cd90d571abdfbf48aed
Andy Hook <hello@andyhook.dev>
2022-05-09 13:43:54 +0100

    Annotate

--- packages/react/dismissable-layer/src/DismissableLayer.tsx
+++ packages/react/dismissable-layer/src/DismissableLayer.tsx
@@ -282,7 +282,7 @@ function handleAndDispatchDiscreteCustomEvent<E extends CustomEvent, OriginalEve
   handler: ((event: E) => void) | undefined,
   detail: { originalEvent: OriginalEvent } & (E extends CustomEvent<infer D> ? D : never)
 ) {
-  const target = detail.originalEvent.target as HTMLElement;
+  const target = detail.originalEvent.target;
   const event = new CustomEvent(name, { bubbles: false, cancelable: true, detail });
   if (handler) target.addEventListener(name, handler as EventListener, { once: true });
   dispatchDiscreteCustomEvent(target, event);
