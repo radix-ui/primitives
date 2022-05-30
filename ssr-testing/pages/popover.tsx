66f24000b06326b53806dd6134abcd143eca5f29
Andy Hook <hello@andyhook.dev>
2022-05-30 19:04:18 +0100

    [Popover] Add to SSR testing page

--- ssr-testing/pages/popover.tsx
+++ ssr-testing/pages/popover.tsx
@@ -1,20 +1,16 @@
 import * as React from 'react';
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverClose,
-  PopoverArrow,
-} from '@radix-ui/react-popover';
+import * as Popover from '@radix-ui/react-popover';
 
 export default function PopoverPage() {
   return (
-    <Popover>
-      <PopoverTrigger>open</PopoverTrigger>
-      <PopoverContent sideOffset={5}>
-        <PopoverClose>close</PopoverClose>
-        <PopoverArrow width={20} height={10} />
-      </PopoverContent>
-    </Popover>
+    <Popover.Root>
+      <Popover.Trigger>open</Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content sideOffset={5}>
+          <Popover.Close>close</Popover.Close>
+          <Popover.Arrow width={20} height={10} />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 }
