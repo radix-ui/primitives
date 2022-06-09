46b1319ed2a7e491d50f61d78a77f39d7f1ade9e
Benoît Grélard <benoit.grelard@gmail.com>
2022-06-09 16:12:37 +0100

    Update SSR example

--- ssr-testing/pages/select.tsx
+++ ssr-testing/pages/select.tsx
@@ -9,32 +9,7 @@ export default function SelectPage() {
           <Select.Value />
           <Select.Icon>▼</Select.Icon>
         </Select.Trigger>
-        <Select.Content>
-          <Select.ScrollUpButton>▲</Select.ScrollUpButton>
-          <Select.Viewport>
-            <Select.Item value="1">
-              <Select.ItemText>Item 1</Select.ItemText>
-              <Select.ItemIndicator>✔</Select.ItemIndicator>
-            </Select.Item>
-            <Select.Item value="2">
-              <Select.ItemText>Item 2</Select.ItemText>
-              <Select.ItemIndicator>✔</Select.ItemIndicator>
-            </Select.Item>
-            <Select.Item value="3">
-              <Select.ItemText>Item 3</Select.ItemText>
-              <Select.ItemIndicator>✔</Select.ItemIndicator>
-            </Select.Item>
-          </Select.Viewport>
-          <Select.ScrollDownButton>▼</Select.ScrollDownButton>
-        </Select.Content>
-      </Select.Root>
-
-      <form>
-        <Select.Root>
-          <Select.Trigger>
-            <Select.Value placeholder="Pick an option" />
-            <Select.Icon>▼</Select.Icon>
-          </Select.Trigger>
+        <Select.Portal>
           <Select.Content>
             <Select.ScrollUpButton>▲</Select.ScrollUpButton>
             <Select.Viewport>
@@ -53,6 +28,35 @@ export default function SelectPage() {
             </Select.Viewport>
             <Select.ScrollDownButton>▼</Select.ScrollDownButton>
           </Select.Content>
+        </Select.Portal>
+      </Select.Root>
+
+      <form>
+        <Select.Root>
+          <Select.Trigger>
+            <Select.Value placeholder="Pick an option" />
+            <Select.Icon>▼</Select.Icon>
+          </Select.Trigger>
+          <Select.Portal>
+            <Select.Content>
+              <Select.ScrollUpButton>▲</Select.ScrollUpButton>
+              <Select.Viewport>
+                <Select.Item value="1">
+                  <Select.ItemText>Item 1</Select.ItemText>
+                  <Select.ItemIndicator>✔</Select.ItemIndicator>
+                </Select.Item>
+                <Select.Item value="2">
+                  <Select.ItemText>Item 2</Select.ItemText>
+                  <Select.ItemIndicator>✔</Select.ItemIndicator>
+                </Select.Item>
+                <Select.Item value="3">
+                  <Select.ItemText>Item 3</Select.ItemText>
+                  <Select.ItemIndicator>✔</Select.ItemIndicator>
+                </Select.Item>
+              </Select.Viewport>
+              <Select.ScrollDownButton>▼</Select.ScrollDownButton>
+            </Select.Content>
+          </Select.Portal>
         </Select.Root>
       </form>
     </>
