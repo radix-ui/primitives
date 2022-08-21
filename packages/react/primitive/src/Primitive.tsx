57e5551647e8c7c9eeb2dbe02dd2388d82277dc7
William Crutchfield <william.r.crutchfield@gmail.com>
2022-08-21 00:13:18 -0400

    Update Primitive.tsx
    Referenced pull request has been merged

--- packages/react/primitive/src/Primitive.tsx
+++ packages/react/primitive/src/Primitive.tsx
@@ -18,9 +18,6 @@ const NODES = [
   'ul',
 ] as const;
 
-// Temporary while we await merge of this fix:
-// https://github.com/DefinitelyTyped/DefinitelyTyped/pull/55396
-// prettier-ignore
 type PropsWithoutRef<P> = P extends any ? ('ref' extends keyof P ? Pick<P, Exclude<keyof P, 'ref'>> : P) : P;
 type ComponentPropsWithoutRef<T extends React.ElementType> = PropsWithoutRef<
   React.ComponentProps<T>
