8bb61932303dd528c82e68c6c86720f06a4c6f18
Artsem Rudakou <artsem.rudakou@itechart-group.com>
2022-09-21 11:23:40 +0300

    [DropdownMenu] refactor: remove check for open menu

--- packages/react/dropdown-menu/src/DropdownMenu.tsx
+++ packages/react/dropdown-menu/src/DropdownMenu.tsx
@@ -126,14 +126,11 @@ const DropdownMenuTrigger = React.forwardRef<DropdownMenuTriggerElement, Dropdow
           })}
           onKeyDown={composeEventHandlers(props.onKeyDown, (event) => {
             if (disabled) return;
-            if (['Enter', ' '].includes(event.key)) {
-              context.onOpenToggle();
-              // prevent passing keydown event to the focused MenuItem element in Firefox
-              if (!context.open) event.preventDefault();
-            }
+            if (['Enter', ' '].includes(event.key)) context.onOpenToggle();
             if (event.key === 'ArrowDown') context.onOpenChange(true);
-            // prevent keypresses from scrolling window
-            if ([' ', 'ArrowDown'].includes(event.key)) event.preventDefault();
+            // prevent keydown from scrolling window / first focused item to execute
+            // that keydown (inadvertently closing the menu)
+            if (['Enter', ' ', 'ArrowDown'].includes(event.key)) event.preventDefault();
           })}
         />
       </MenuPrimitive.Anchor>
