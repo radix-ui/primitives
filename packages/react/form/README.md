def342bb8c7f57a40b8861368f0103dabdf53342
Benoît Grélard <benoit.grelard@gmail.com>
2023-03-07 14:04:45 +0100

    New `Form` primitive (#1998)
    * Add new package
    
    * Add cypress tests
    
    * Use context scoping
    
    * Remove server messages
    
    * Add support for server errors back
    
    * Re-add focus management for server errors
    
    * Handle clearing server errors
    
    * Refactor reset from effect to react handler
    
    * Pass formData to custom matchers
    
    * Add data attributes to field element
    
    * Tweak type
    
    * Lift validation handling to the root
    
    * Add `name` support to `ValidityState`
    
    * Create 2b7058fa.yml
    
    * PR feedback

---
+++ packages/react/form/README.md
@@ -0,0 +1,13 @@
+# `react-form`
+
+## Installation
+
+```sh
+$ yarn add @radix-ui/react-form
+# or
+$ npm install @radix-ui/react-form
+```
+
+## Usage
+
+View docs [here](https://radix-ui.com/primitives/docs/components/form).
---
+++ packages/react/form/src/index.ts
@@ -0,0 +1,29 @@
+export {
+  createFormScope,
+  //
+  Form,
+  FormField,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  FormValidityState,
+  FormSubmit,
+  //
+  Root,
+  Field,
+  Label,
+  Control,
+  Message,
+  ValidityState,
+  Submit,
+} from './Form';
+
+export type {
+  FormProps,
+  FormFieldProps,
+  FormLabelProps,
+  FormControlProps,
+  FormMessageProps,
+  FormValidityStateProps,
+  FormSubmitProps,
+} from './Form';
