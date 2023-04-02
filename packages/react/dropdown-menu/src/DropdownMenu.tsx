131545ce9025cad69ec2b8526ee24030182dd757
ricci2511 <ricardo.christmann@protonmail.com>
2023-04-02 22:36:09 +0200

    [DropdownMenu] Allow click events that are not only triggered by pointers

--- packages/react/dropdown-menu/src/DropdownMenu.tsx
+++ packages/react/dropdown-menu/src/DropdownMenu.tsx
@@ -114,7 +114,7 @@ const DropdownMenuTrigger = React.forwardRef<DropdownMenuTriggerElement, Dropdow
           disabled={disabled}
           {...triggerProps}
           ref={composeRefs(forwardedRef, context.triggerRef)}
-          onPointerDown={composeEventHandlers(props.onPointerDown, (event) => {
+          onMouseDown={composeEventHandlers(props.onMouseDown, (event) => {
             // only call handler if it's the left button (mousedown gets triggered by all mouse buttons)
             // but not when the control key is pressed (avoiding MacOS right click)
             if (!disabled && event.button === 0 && event.ctrlKey === false) {
