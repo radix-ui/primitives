fddbefe0343a4e58b023254d82994f4a75b8df76
Jenna Smith <175330+jjenzz@users.noreply.github.com>
2023-04-27 15:59:27 +0100

    Fix `Slot` when used in an RSC

--- packages/react/slot/src/Slot.tsx
+++ packages/react/slot/src/Slot.tsx
@@ -63,7 +63,7 @@ const SlotClone = React.forwardRef<any, SlotCloneProps>((props, forwardedRef) =>
   if (React.isValidElement(children)) {
     return React.cloneElement(children, {
       ...mergeProps(slotProps, children.props),
-      ref: composeRefs(forwardedRef, (children as any).ref),
+      ref: forwardedRef ? composeRefs(forwardedRef, (children as any).ref) : (children as any).ref,
     });
   }
 
