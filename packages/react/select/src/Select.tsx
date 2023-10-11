92aeab183c6d542c2196a7e9215ea4838c366c4f
Job <Manduro@users.noreply.github.com>
2023-10-11 16:21:23 +0200

    [Select] Support form attribute
    This lets you associate the underlying native `<select>` element to a `<form>` anywhere in the document instead of requiring an ancestor `<form>` element.

--- packages/react/select/src/Select.tsx
+++ packages/react/select/src/Select.tsx
@@ -92,6 +92,7 @@ interface SelectProps {
   autoComplete?: string;
   disabled?: boolean;
   required?: boolean;
+  form?: string;
 }
 
 const Select: React.FC<SelectProps> = (props: ScopedProps<SelectProps>) => {
@@ -109,6 +110,7 @@ const Select: React.FC<SelectProps> = (props: ScopedProps<SelectProps>) => {
     autoComplete,
     disabled,
     required,
+    form,
   } = props;
   const popperScope = usePopperScope(__scopeSelect);
   const [trigger, setTrigger] = React.useState<SelectTriggerElement | null>(null);
@@ -128,7 +130,7 @@ const Select: React.FC<SelectProps> = (props: ScopedProps<SelectProps>) => {
   const triggerPointerDownPosRef = React.useRef<{ x: number; y: number } | null>(null);
 
   // We set this to true by default so that events bubble to forms without JS (SSR)
-  const isFormControl = trigger ? Boolean(trigger.closest('form')) : true;
+  const isFormControl = trigger ? (Boolean(trigger.closest('form')) || form) : true;
   const [nativeOptionsSet, setNativeOptionsSet] = React.useState(new Set<NativeOption>());
 
   // The native `select` only associates the correct default value if the corresponding
@@ -190,6 +192,7 @@ const Select: React.FC<SelectProps> = (props: ScopedProps<SelectProps>) => {
             // enable form autofill
             onChange={(event) => setValue(event.target.value)}
             disabled={disabled}
+            form={form}
           >
             {value === undefined ? <option value="" /> : null}
             {Array.from(nativeOptionsSet)}
