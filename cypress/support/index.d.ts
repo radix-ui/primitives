c2c20fea5073c16d98a22d3dd53ec59bec947448
Anatol Zakrividoroga <53095479+anatolzak@users.noreply.github.com>
2024-01-08 21:39:45 +0100

    Add visitUtilityStory declaration to Cypress Chainable interface

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,5 +1,6 @@
 declare namespace Cypress {
   interface Chainable<Subject> {
     visitStory(storyName: string, options?: Partial<Cypress.VisitOptions>): Chainable<any>;
+    visitUtilityStory(storyName: string, options?: Partial<Cypress.VisitOptions>): Chainable<any>;
   }
 }
