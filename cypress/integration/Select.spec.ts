1fe8c4addfd2014e0b7725331e0dd28d4d9331b5
Anatol Zakrividoroga <53095479+anatolzak@users.noreply.github.com>
2024-01-08 11:13:26 +0100

    [Select] added test cases for typeahead backspace functionality

--- cypress/integration/Select.spec.ts
+++ cypress/integration/Select.spec.ts
@@ -30,4 +30,42 @@ describe('Select', () => {
       cy.findByText('…').should('exist');
     });
   });
+
+  describe('given a keyboard user', () => {
+    it('should delete only the last character in typeahead on select content when backspace pressed', () => {
+      cy.findByLabelText(/choose a size/).click();
+      cy.focused().type('sm');
+      cy.findByRole('option', { name: /small/i }).should('have.attr', 'data-highlighted');
+      cy.focused().type('{backspace}m');
+      cy.findByRole('option', { name: /small/i }).should('have.attr', 'data-highlighted');
+    });
+
+    it('should update typeahead correctly when backspace pressed on select content', () => {
+      cy.findByLabelText(/choose a size/).click();
+      cy.focused().type('s');
+      cy.findByRole('option', { name: /small/i }).should('have.attr', 'data-highlighted');
+      cy.focused().type('{backspace}m');
+      cy.findByRole('option', { name: /medium/i }).should('have.attr', 'data-highlighted');
+      cy.focused().type('{backspace}l');
+      cy.findByRole('option', { name: /large/i }).should('have.attr', 'data-highlighted');
+    });
+
+    it('should delete only the last character in typeahead on select trigger when backspace pressed', () => {
+      cy.findByLabelText(/choose a size/).focus();
+      cy.focused().type('sm');
+      cy.findByLabelText(/choose a size/).should('contain.text', 'Small');
+      cy.focused().type('{backspace}m');
+      cy.findByLabelText(/choose a size/).should('contain.text', 'Small');
+    });
+
+    it('should update typeahead correctly when backspace pressed on select trigger', () => {
+      cy.findByLabelText(/choose a size/).focus();
+      cy.focused().type('s');
+      cy.findByLabelText(/choose a size/).should('contain.text', 'Small');
+      cy.focused().type('{backspace}m');
+      cy.findByLabelText(/choose a size/).should('contain.text', 'Medium');
+      cy.focused().type('{backspace}l');
+      cy.findByLabelText(/choose a size/).should('contain.text', 'Large');
+    });
+  });
 });
