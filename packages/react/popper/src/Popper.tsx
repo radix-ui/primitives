radix-ui/primitives

commit e5ba0d9a09445abb65872f2e6105bb77a704428b

fix(Popper): use `visibility` to hide instead of `opacity` (#2744)
* fix(Popper): use `visibility` to hide instead of `opacity`

When using `hideWhenDetached`, the opacity is changed to 0 instead of the
visibility. This lets users click items that they cannot see. This is
generally a bad user experience.

This patch updates the `Popper` to set the `visibility` to `hidden` when
`hideWhenDetached` is used. This ensures the user cannot interact with
the hidden element.

This aligns with what is in the `@floating-ui/react-dom` documentation.

Ref: https://floating-ui.com/docs/hide#usage

Closes: https://github.com/radix-ui/primitives/issues/2743

* Update packages/react/popper/src/Popper.tsx

* Create 5793010b.yml

---------

Co-authored-by: Benoît Grélard <benoit.grelard@gmail.com>

packages/react/popper/src/Popper.tsx
@@ -268,7 +268,7 @@ const PopperContent = React.forwardRef<PopperContentElement, PopperContentProps>
               // we prevent animations so that users's animation don't kick in too early referring wrong sides
               animation: !isPositioned ? 'none' : undefined,
               // hide the content if using the hide middleware and should be hidden
-              opacity: middlewareData.hide?.referenceHidden ? 0 : undefined,
+              visibility: middlewareData.hide?.referenceHidden ? 'hidden' : undefined,
             }}
           />
         </PopperContentProvider>

.yarn/versions/5793010b.yml
@@ -1,0 +1,13 @@
+releases:
+  "@radix-ui/react-context-menu": patch
+  "@radix-ui/react-dropdown-menu": patch
+  "@radix-ui/react-hover-card": patch
+  "@radix-ui/react-menu": patch
+  "@radix-ui/react-menubar": patch
+  "@radix-ui/react-popover": patch
+  "@radix-ui/react-popper": patch
+  "@radix-ui/react-select": patch
+  "@radix-ui/react-tooltip": patch
+
+declined:
+  - primitives

commit 5bbbb0434f32d50dbec70032d6714f67ba770f14

fix(Popper): disable pointer events when hidden
This patch sets `pointer-events: none` when the `<PopperContent>` is
hidden so that the UI behaves as if it is not there at all. This ensures
that users can interact with elements beneath the `<PopperContent>`
uninterrupted.

Ref: https://github.com/radix-ui/primitives/issues/2743#issuecomment-1971843131

Fixes: e5ba0d9a0944 ("fix(Popper): use `visibility` to hide instead of `opacity` (#2744)")

packages/react/popper/src/Popper.tsx
@@ -268,7 +268,10 @@ const PopperContent = React.forwardRef<PopperContentElement, PopperContentProps>
               // we prevent animations so that users's animation don't kick in too early referring wrong sides
               animation: !isPositioned ? 'none' : undefined,
               // hide the content if using the hide middleware and should be hidden
+              // set visibility to hidden and disable pointer events so the UI behaves
+              // as if the PopperContent isn't there at all
               visibility: middlewareData.hide?.referenceHidden ? 'hidden' : undefined,
+              pointerEvents: middlewareData.hide?.referenceHidden ? 'none' : undefined,
             }}
           />
         </PopperContentProvider>