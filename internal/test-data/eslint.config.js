36e6662a2fca6ffbc86739616fb14a9ecfb73091
Chance Strickland <hi@chance.dev>
2025-01-22 09:58:05 -0800

    consolidate eslint configs

--- internal/test-data/eslint.config.js
+++ internal/test-data/eslint.config.js
@@ -1,12 +1,11 @@
 import * as js from '@chance/eslint';
 import * as typescript from '@chance/eslint/typescript';
-import pluginCypress from 'eslint-plugin-cypress/flat';
 
 /** @type {import("eslint").Linter.Config[]} */
 const config = [
-  js.config,
+  //
+  js.getConfig(),
   typescript.config,
-  pluginCypress.configs.recommended, //
 ];
 
 export default config;
