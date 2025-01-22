f94bb1b0c9bdac54ae65cd244e14d1d41e8896cb
Chance Strickland <hi@chance.dev>
2025-01-21 20:27:49 -0800

    cypress migrations

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  viewportWidth: 1024,
+  viewportHeight: 768,
+  fixturesFolder: false,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:9009',
+  },
+});
