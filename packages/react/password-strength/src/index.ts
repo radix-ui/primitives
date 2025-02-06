3225cb40a2c3e4712c7a9ca18282b7f13ba21314
Chance Strickland <hi@chance.dev>
2025-02-05 20:57:09 -0800

    add initial implementation

--- packages/react/password-strength/src/index.ts
+++ packages/react/password-strength/src/index.ts
@@ -1,2 +1,23 @@
 'use client';
-export {} from './password-strength';
+export type {
+  PasswordStrengthProps,
+  PasswordStrengthProgressProps,
+  PasswordStrengthInputProps,
+  PasswordStrengthAnnounceProps,
+  PasswordStrengthRule,
+} from './password-strength';
+export {
+  PasswordStrength,
+  PasswordStrengthProgress,
+  PasswordStrengthInput,
+  PasswordStrengthBar,
+  PasswordStrengthRules,
+  PasswordStrengthAnnounce,
+  //
+  Root,
+  Progress,
+  Input,
+  Bar,
+  Rules,
+  Announce,
+} from './password-strength';
--- packages/react/password-strength/src/password-strength.tsx
+++ packages/react/password-strength/src/password-strength.tsx
@@ -1,1 +1,340 @@
-export {};
+import * as React from 'react';
+import { useComposedRefs } from '@radix-ui/react-compose-refs';
+import { useControllableState } from '@radix-ui/react-use-controllable-state';
+import { Primitive } from '@radix-ui/react-primitive';
+import { useIsHydrated } from '@radix-ui/react-use-is-hydrated';
+import { composeEventHandlers } from '@radix-ui/primitive';
+
+// This reference is for constructing an in-memory input element whose only
+// function is to reference the validity property for iterating through all
+// validity keys. We're breaking some rules here, so only set this once
+// and only when React is hydrated on the client.
+const INPUT_CACHE = new WeakMap<typeof PasswordStrength, HTMLInputElement | null>();
+
+interface PasswordStrengthContextValue {
+  rules: ValidatedRule[];
+  progress: number;
+  validatedRuleCount: number;
+  value: string | undefined;
+  setValue: React.Dispatch<React.SetStateAction<string | undefined>>;
+}
+
+const PasswordStrengthContext = React.createContext<PasswordStrengthContextValue | null>(null);
+PasswordStrengthContext.displayName = 'PasswordStrengthContext';
+
+function usePasswordStrengthContext() {
+  const context = React.useContext(PasswordStrengthContext);
+  if (!context) {
+    throw Error('usePasswordStrengthContext must be called in PasswordStrength.Root');
+  }
+  return context;
+}
+
+interface PasswordStrengthProps {
+  value?: string;
+  defaultValue?: string;
+  onValueChange?: (value: string) => void;
+  children: React.ReactNode;
+  rules: PasswordStrengthRule[];
+}
+
+const PasswordStrength: React.FC<PasswordStrengthProps> = function PasswordStrength({
+  value: valueProp,
+  defaultValue,
+  onValueChange,
+  children,
+  rules,
+}) {
+  const [value, setValue] = useControllableState({
+    prop: valueProp,
+    defaultProp: defaultValue,
+    onChange: onValueChange,
+  });
+  const { validatedRules, validatedRuleCount, progress } = React.useMemo(() => {
+    let validatedRuleCount = 0;
+    const progress = validatedRuleCount / rules.length;
+    const validatedRules = rules.map<ValidatedRule>((rule) => {
+      const isValid = rule.validate(value ?? '');
+      if (isValid) validatedRuleCount++;
+      return { label: rule.label, isValid };
+    });
+    return { validatedRuleCount, validatedRules, progress };
+  }, [rules, value]);
+
+  const isHydrated = useIsHydrated();
+  if (isHydrated && !INPUT_CACHE.has(PasswordStrength)) {
+    INPUT_CACHE.set(PasswordStrength, document.createElement('input'));
+  }
+
+  return (
+    <PasswordStrengthContext.Provider
+      value={{
+        progress,
+        rules: validatedRules,
+        value,
+        validatedRuleCount,
+        setValue,
+      }}
+    >
+      {children}
+    </PasswordStrengthContext.Provider>
+  );
+};
+
+interface PasswordStrengthProgressContextValue {
+  bars: any[];
+  setBars: React.Dispatch<React.SetStateAction<any[]>>;
+}
+
+const PasswordStrengthProgressContext =
+  React.createContext<PasswordStrengthProgressContextValue | null>(null);
+PasswordStrengthProgressContext.displayName = 'PasswordStrengthProgressContext';
+
+function usePasswordStrengthProgressContext() {
+  const context = React.useContext(PasswordStrengthProgressContext);
+  if (!context) {
+    throw Error('usePasswordStrengthProgressContext must be called in PasswordStrength.Meter');
+  }
+  return context;
+}
+
+interface PasswordStrengthProgressProps
+  extends Omit<React.ComponentPropsWithoutRef<typeof Primitive.div>, 'aria-valuetext'> {
+  'aria-valuetext'?:
+    | string
+    | ((props: { rules: ValidatedRule[]; total: number; validated: number }) => string);
+}
+
+const PasswordStrengthProgress = React.forwardRef<HTMLDivElement, PasswordStrengthProgressProps>(
+  function PasswordStrengthProgress(
+    { children, 'aria-valuetext': ariaValueTextProp, ...props },
+    forwardedRef
+  ) {
+    const { rules, progress, validatedRuleCount } = usePasswordStrengthContext();
+    const [bars, setBars] = React.useState<any[]>([]);
+    const totalRuleCount = rules.length;
+
+    let ariaValueText: string | undefined;
+    if (ariaValueTextProp) {
+      ariaValueText =
+        typeof ariaValueTextProp === 'function'
+          ? ariaValueTextProp({
+              rules,
+              total: totalRuleCount,
+              validated: validatedRuleCount,
+            })
+          : ariaValueTextProp;
+    }
+    if (!ariaValueText) {
+      ariaValueText = `${validatedRuleCount} of ${totalRuleCount} rules satisfied`;
+    }
+
+    return (
+      <PasswordStrengthProgressContext.Provider value={{ bars, setBars }}>
+        <Primitive.div
+          ref={forwardedRef}
+          role="progressbar"
+          aria-label="Password strength"
+          aria-valuemin={0}
+          aria-valuemax={1}
+          aria-valuetext={ariaValueText}
+          aria-valuenow={progress}
+          style={
+            {
+              '--radix-password-strength-progress': progress,
+              '--radix-password-strength-rule-count': totalRuleCount,
+              '--radix-password-strength-step': Math.ceil(progress * totalRuleCount),
+            } as React.CSSProperties
+          }
+          data-step={Math.ceil(progress * totalRuleCount)}
+          {...props}
+        >
+          {children}
+        </Primitive.div>
+      </PasswordStrengthProgressContext.Provider>
+    );
+  }
+);
+
+interface PasswordStrengthInputProps
+  extends Omit<React.ComponentPropsWithoutRef<typeof Primitive.input>, 'value' | 'type'> {
+  type?: 'password' | 'text';
+}
+
+const PasswordStrengthInput = React.forwardRef<HTMLInputElement, PasswordStrengthInputProps>(
+  function PasswordStrengthInput(
+    {
+      onChange,
+      // TODO: warn if value is passed
+      // @ts-expect-error
+      value: _value,
+      type = 'password',
+      ...props
+    },
+    forwardedRef
+  ) {
+    const { progress, value = '', setValue } = usePasswordStrengthContext();
+    const inputRef = React.useRef<HTMLInputElement | null>(null);
+    const ref = useComposedRefs(forwardedRef, inputRef);
+
+    return (
+      <Primitive.input
+        ref={ref}
+        value={value}
+        onChange={composeEventHandlers(onChange, (event) => {
+          setValue(event.target.value);
+        })}
+        type={type}
+        aria-invalid={progress !== 1 || undefined}
+        {...props}
+      />
+    );
+  }
+);
+
+interface PasswordStrengthBarProps extends React.ComponentPropsWithoutRef<typeof Primitive.div> {}
+
+const PasswordStrengthBar = React.forwardRef<HTMLDivElement, PasswordStrengthBarProps>(
+  function PasswordStrengthBar({ style, ...props }, forwardedRef) {
+    const { progress, rules } = usePasswordStrengthContext();
+    const { bars, setBars } = usePasswordStrengthProgressContext();
+    const [instance] = React.useState(() => Object.create(null));
+    const index = bars.findIndex((bar) => bar === instance);
+    const ownScore = index === -1 ? 0 : calculateElementProgress(progress, index, rules.length);
+    React.useEffect(() => {
+      setBars((bars) => [...bars, instance]);
+      return () => {
+        setBars((bars) => bars.filter((bar) => bar !== instance));
+      };
+    }, [instance, setBars]);
+
+    return (
+      <Primitive.div
+        style={
+          {
+            ...style,
+            '--radix-password-strength-bar-progress': ownScore,
+          } as React.CSSProperties
+        }
+        aria-hidden
+        data-active={ownScore > 0}
+        data-index={index}
+        ref={forwardedRef}
+        {...props}
+      />
+    );
+  }
+);
+
+interface PasswordStrengthRulesProps {
+  children: (props: { rules: Array<ValidatedRule> }) => React.ReactNode;
+}
+
+const PasswordStrengthRules: React.FC<PasswordStrengthRulesProps> = function PasswordStrengthRules({
+  children,
+}) {
+  const { rules } = usePasswordStrengthContext();
+  return children({ rules }) as React.ReactElement;
+};
+
+interface PasswordStrengthAnnounceProps
+  extends Omit<React.ComponentPropsWithoutRef<'div'>, 'children'> {
+  message?: (props: { isValid: boolean }) => string;
+}
+
+// TODO: Not sure if we need this, test with different
+// ATs to see if aria-invalid + error state is sufficient.
+const PasswordStrengthAnnounce = React.forwardRef<HTMLDivElement, PasswordStrengthAnnounceProps>(
+  function PasswordStrengthAnnounce(
+    {
+      style,
+      message,
+      // @ts-expect-error
+      children,
+      ...props
+    },
+    forwardedRef
+  ) {
+    const { rules, progress } = usePasswordStrengthContext();
+    const isValid = progress >= 1;
+    const [wasValid, setWasValid] = React.useState(isValid);
+    const validationHasChanged = React.useRef(false);
+    if (!validationHasChanged.current && isValid !== wasValid) {
+      setWasValid(isValid);
+      validationHasChanged.current = true;
+    }
+
+    // TODO: add fallback based on rules
+    let announcement = isValid
+      ? 'Password is valid'
+      : rules.reduce((message, rule) => {
+          if (!rule.isValid) {
+            return message
+              ? `; ${message} ${rule.label}`
+              : `Password does not meet the following requirements: ${rule.label}`;
+          }
+          return message;
+        }, '');
+    if (message) {
+      const results = message({ isValid });
+      if (typeof results === 'string' && results !== '') {
+        announcement = results;
+      }
+    }
+
+    return (
+      <Primitive.div role="region" aria-live="polite" ref={forwardedRef} {...props}>
+        {!validationHasChanged.current
+          ? // Live region should only announce changes to the validation state
+            ''
+          : announcement}
+      </Primitive.div>
+    );
+  }
+);
+
+function calculateElementProgress(t: number, index: number, total: number): number {
+  if (total <= 1) return t;
+  const progress = t * total - index;
+  return Math.min(1, Math.max(0, progress));
+}
+
+interface PasswordStrengthRule {
+  label: string;
+  validate: (value: string) => boolean;
+}
+
+interface ValidatedRule {
+  label: string;
+  isValid: boolean;
+}
+
+const Root = PasswordStrength;
+const Progress = PasswordStrengthProgress;
+const Input = PasswordStrengthInput;
+const Bar = PasswordStrengthBar;
+const Rules = PasswordStrengthRules;
+const Announce = PasswordStrengthAnnounce;
+
+export {
+  PasswordStrength,
+  PasswordStrengthProgress,
+  PasswordStrengthInput,
+  PasswordStrengthBar,
+  PasswordStrengthRules,
+  PasswordStrengthAnnounce,
+  //
+  Root,
+  Progress,
+  Input,
+  Bar,
+  Rules,
+  Announce,
+};
+export type {
+  PasswordStrengthProps,
+  PasswordStrengthProgressProps,
+  PasswordStrengthInputProps,
+  PasswordStrengthAnnounceProps,
+  PasswordStrengthRule,
+};
