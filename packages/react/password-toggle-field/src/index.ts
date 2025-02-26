13c228aa99de4b71bb2c2eef4b57ac67ac992c6b
Chance Strickland <hi@chance.dev>
2025-02-26 09:56:43 -0800

    update toggle field API

--- packages/react/password-toggle-field/src/index.ts
+++ packages/react/password-toggle-field/src/index.ts
@@ -3,13 +3,19 @@ export type {
   PasswordToggleFieldProps,
   PasswordToggleFieldInputProps,
   PasswordToggleFieldToggleProps,
+  PasswordToggleFieldIconProps,
+  PasswordToggleFieldSlotProps,
 } from './password-toggle-field';
 export {
   PasswordToggleField,
   PasswordToggleFieldInput,
   PasswordToggleFieldToggle,
+  PasswordToggleFieldIcon,
+  PasswordToggleFieldSlot,
   //
   Root,
   Input,
   Toggle,
+  Icon,
+  Slot,
 } from './password-toggle-field';
