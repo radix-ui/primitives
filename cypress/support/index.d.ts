f8334ba3e6564a7f28df2fa145cf87f2b47283b8
Chance Strickland <hi@chance.dev>
2025-04-08 19:07:59 -0700

    eslint ignore

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,4 +1,5 @@
 declare namespace Cypress {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chainable<Subject> {
     visitStory(storyName: string, options?: Partial<Cypress.VisitOptions>): Chainable<any>;
   }
