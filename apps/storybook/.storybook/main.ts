ec409a6f1b2246d2787ee3db1c5ac9a90e09f8e9
Chance Strickland <hi@chance.dev>
2025-04-09 14:53:19 -0700

    revert to webpack for perf

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,4 +1,4 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   stories: [
@@ -10,13 +10,30 @@ const config: StorybookConfig = {
     //
     '@storybook/addon-essentials',
     '@storybook/addon-storysource',
+    '@storybook/addon-webpack5-compiler-swc',
     // '@chromatic-com/storybook',
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: '@storybook/react-webpack5',
     options: {
+      builder: {},
+      // enable React strict mode
       strictMode: true,
     },
   },
+
+  swc: (config: any) => ({
+    ...config,
+    jsc: {
+      ...config?.jsc,
+      transform: {
+        ...config?.jsc?.transform,
+        react: {
+          // Do not require importing React into scope to use JSX
+          runtime: 'automatic',
+        },
+      },
+    },
+  }),
 };
 export default config;
--- apps/storybook/.storybook/vitest.setup.ts
+++
@@ -1,9 +0,0 @@
-import { beforeAll } from 'vitest';
-import { setProjectAnnotations } from '@storybook/react';
-import * as projectAnnotations from './preview';
-
-// This is an important step to apply the right configuration when testing your
-// stories. See https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
-const project = setProjectAnnotations([projectAnnotations]);
-
-beforeAll(project.beforeAll);
