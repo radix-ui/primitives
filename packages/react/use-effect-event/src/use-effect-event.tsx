7f701e27e3a5ae174e97a068fb839a1c808d6530
Chance Strickland <hi@chance.dev>
2025-04-12 21:20:26 -0700

    tweak react-use-effect-event

--- packages/react/use-effect-event/src/use-effect-event.tsx
+++ packages/react/use-effect-event/src/use-effect-event.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useLayoutEffect } from '@radix-ui/react-use-layout-effect';
 import * as React from 'react';
 
 type AnyFunction = (...args: any[]) => any;
@@ -6,20 +8,27 @@ type AnyFunction = (...args: any[]) => any;
  * Designed to approximate the behavior on `experimental_useEffectEvent` as best
  * as possible until its stable release, and back-fill it as a shim as needed.
  */
-export function useEffectEvent<T extends AnyFunction>(callback?: T) {
+export function useEffectEvent<T extends AnyFunction>(callback?: T): T {
+  // @ts-expect-error: See https://github.com/webpack/webpack/issues/14814
+  if (typeof React['useEffectEvent '.trim().toString()] === 'function') {
+    // @ts-expect-error
+    return React.useEffectEvent(callback);
+  }
+
   const ref = React.useRef<AnyFunction | undefined>(() => {
     throw new Error('Cannot call an event handler while rendering.');
   });
   // @ts-expect-error: See https://github.com/webpack/webpack/issues/14814
-  if (typeof React['useInsertionEffect'.toString()] === 'function') {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
+  if (typeof React['useInsertionEffect '.trim().toString()] === 'function') {
     React.useInsertionEffect(() => {
       ref.current = callback;
     });
   } else {
-    // Prior to React 18's concurrent rendering model, updating the ref during
-    // render is safe enough for our the purpose of updating event handlers.
-    ref.current = callback;
+    useLayoutEffect(() => {
+      ref.current = callback;
+    });
   }
-  return React.useCallback<AnyFunction>((...args) => ref.current?.(...args), []) as T;
+
+  // https://github.com/facebook/react/issues/19240
+  return React.useMemo(() => ((...args) => ref.current?.(...args)) as T, []);
 }
