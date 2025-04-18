4c39b4383f34fa0f9de0b42fcff4b51d7a0adfda
Chance Strickland <hi@chance.dev>
2025-04-18 13:52:27 -0400

    Do not attempt to import `useEffectEvent` from React
    See https://github.com/radix-ui/primitives/issues/3485

--- packages/react/use-effect-event/src/use-effect-event.tsx
+++ packages/react/use-effect-event/src/use-effect-event.tsx
@@ -9,25 +9,31 @@ type AnyFunction = (...args: any[]) => any;
  * as possible until its stable release, and back-fill it as a shim as needed.
  */
 export function useEffectEvent<T extends AnyFunction>(callback?: T): T {
-  // @ts-expect-error: See https://github.com/webpack/webpack/issues/14814
-  if (typeof React['useEffectEvent '.trim().toString()] === 'function') {
-    // @ts-expect-error
-    return React.useEffectEvent(callback);
-  }
+  // https://github.com/radix-ui/primitives/issues/3485
+  // See https://github.com/webpack/webpack/issues/14814
+  // if (typeof React['useEffectEvent '.trim().toString()] === 'function') {
+  //   // @ts-expect-error
+  //   return React.useEffectEvent(callback);
+  // }
 
   const ref = React.useRef<AnyFunction | undefined>(() => {
     throw new Error('Cannot call an event handler while rendering.');
   });
-  // @ts-expect-error: See https://github.com/webpack/webpack/issues/14814
-  if (typeof React['useInsertionEffect '.trim().toString()] === 'function') {
-    React.useInsertionEffect(() => {
-      ref.current = callback;
-    });
-  } else {
-    useLayoutEffect(() => {
-      ref.current = callback;
-    });
-  }
+  // https://github.com/radix-ui/primitives/issues/3485
+  // See https://github.com/webpack/webpack/issues/14814
+  // if (typeof React['useInsertionEffect '.trim().toString()] === 'function') {
+  //   React.useInsertionEffect(() => {
+  //     ref.current = callback;
+  //   });
+  // } else {
+  //   useLayoutEffect(() => {
+  //     ref.current = callback;
+  //   });
+  // }
+
+  useLayoutEffect(() => {
+    ref.current = callback;
+  });
 
   // https://github.com/facebook/react/issues/19240
   return React.useMemo(() => ((...args) => ref.current?.(...args)) as T, []);
