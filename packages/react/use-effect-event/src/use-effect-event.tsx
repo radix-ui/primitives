e8864725b3cb75066eb6692364006e210debc1cf
Chance Strickland <hi@chance.dev>
2025-04-18 14:42:41 -0400

    Patch additional issues with conditional React imports

--- packages/react/use-effect-event/src/use-effect-event.tsx
+++ packages/react/use-effect-event/src/use-effect-event.tsx
@@ -4,23 +4,25 @@ import * as React from 'react';
 
 type AnyFunction = (...args: any[]) => any;
 
+// See https://github.com/webpack/webpack/issues/14814
+const useReactEffectEvent = (React as any)[' useEffectEvent '.trim().toString()];
+const useReactInsertionEffect = (React as any)[' useInsertionEffect '.trim().toString()];
+
 /**
  * Designed to approximate the behavior on `experimental_useEffectEvent` as best
  * as possible until its stable release, and back-fill it as a shim as needed.
  */
 export function useEffectEvent<T extends AnyFunction>(callback?: T): T {
-  // See https://github.com/webpack/webpack/issues/14814
-  if (typeof (React as any)[new OpaqueString('useEffectEvent').toString()] === 'function') {
-    // @ts-expect-error
-    return React.useEffectEvent(callback);
+  if (typeof useReactEffectEvent === 'function') {
+    return useReactEffectEvent(callback);
   }
 
   const ref = React.useRef<AnyFunction | undefined>(() => {
     throw new Error('Cannot call an event handler while rendering.');
   });
   // See https://github.com/webpack/webpack/issues/14814
-  if (typeof (React as any)[new OpaqueString('useInsertionEffect').toString()] === 'function') {
-    React.useInsertionEffect(() => {
+  if (typeof useReactInsertionEffect === 'function') {
+    useReactInsertionEffect(() => {
       ref.current = callback;
     });
   } else {
@@ -29,20 +31,6 @@ export function useEffectEvent<T extends AnyFunction>(callback?: T): T {
     });
   }
 
-  useLayoutEffect(() => {
-    ref.current = callback;
-  });
-
   // https://github.com/facebook/react/issues/19240
   return React.useMemo(() => ((...args) => ref.current?.(...args)) as T, []);
-}
-
-class OpaqueString {
-  private string: string;
-  constructor(string: string) {
-    this.string = string;
-  }
-  toString() {
-    return this.string;
-  }
 }
