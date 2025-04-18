6a705200c663bd8dbc141e86e0fe2c6a6e4394b5
Chance Strickland <hi@chance.dev>
2025-04-18 14:08:30 -0400

    update obscure import strategy

--- packages/react/use-effect-event/src/use-effect-event.tsx
+++ packages/react/use-effect-event/src/use-effect-event.tsx
@@ -9,27 +9,25 @@ type AnyFunction = (...args: any[]) => any;
  * as possible until its stable release, and back-fill it as a shim as needed.
  */
 export function useEffectEvent<T extends AnyFunction>(callback?: T): T {
-  // https://github.com/radix-ui/primitives/issues/3485
   // See https://github.com/webpack/webpack/issues/14814
-  // if (typeof React['useEffectEvent '.trim().toString()] === 'function') {
-  //   // @ts-expect-error
-  //   return React.useEffectEvent(callback);
-  // }
+  if (typeof (React as any)[new OpaqueString('useEffectEvent').toString()] === 'function') {
+    // @ts-expect-error
+    return React.useEffectEvent(callback);
+  }
 
   const ref = React.useRef<AnyFunction | undefined>(() => {
     throw new Error('Cannot call an event handler while rendering.');
   });
-  // https://github.com/radix-ui/primitives/issues/3485
   // See https://github.com/webpack/webpack/issues/14814
-  // if (typeof React['useInsertionEffect '.trim().toString()] === 'function') {
-  //   React.useInsertionEffect(() => {
-  //     ref.current = callback;
-  //   });
-  // } else {
-  //   useLayoutEffect(() => {
-  //     ref.current = callback;
-  //   });
-  // }
+  if (typeof (React as any)[new OpaqueString('useInsertionEffect').toString()] === 'function') {
+    React.useInsertionEffect(() => {
+      ref.current = callback;
+    });
+  } else {
+    useLayoutEffect(() => {
+      ref.current = callback;
+    });
+  }
 
   useLayoutEffect(() => {
     ref.current = callback;
@@ -37,4 +35,14 @@ export function useEffectEvent<T extends AnyFunction>(callback?: T): T {
 
   // https://github.com/facebook/react/issues/19240
   return React.useMemo(() => ((...args) => ref.current?.(...args)) as T, []);
+}
+
+class OpaqueString {
+  private string: string;
+  constructor(string: string) {
+    this.string = string;
+  }
+  toString() {
+    return this.string;
+  }
 }
