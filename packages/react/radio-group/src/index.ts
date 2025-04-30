b7eaf70a134dee6490039b1f023bdc843864298c
Chance Strickland <hi@chance.dev>
2025-04-30 15:48:57 -0700

    remove ItemBubbleInput export

--- packages/react/radio-group/src/index.ts
+++ packages/react/radio-group/src/index.ts
@@ -6,14 +6,12 @@ export {
   RadioGroupItem,
   RadioGroupItemRoot as unstable_RadioGroupItemRoot,
   RadioGroupItemTrigger as unstable_RadioGroupItemTrigger,
-  RadioGroupItemBubbleInput as unstable_RadioGroupItemBubbleInput,
   RadioGroupIndicator,
   //
   Root,
   Item,
   ItemRoot as unstable_ItemRoot,
   ItemTrigger as unstable_ItemTrigger,
-  ItemBubbleInput as unstable_ItemBubbleInput,
   Indicator,
 } from './radio-group';
 export type {
@@ -21,6 +19,5 @@ export type {
   RadioGroupItemProps,
   RadioGroupItemRootProps as unstable_RadioGroupItemRootProps,
   RadioGroupItemTriggerProps as unstable_RadioGroupItemTriggerProps,
-  RadioGroupItemBubbleInputProps as unstable_RadioGroupItemBubbleInputProps,
   RadioGroupIndicatorProps,
 } from './radio-group';
