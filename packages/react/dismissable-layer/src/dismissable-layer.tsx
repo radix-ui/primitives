31f7bcc515b39648670ba0c0ed9362079f983aa4
Chance Strickland <hi@chance.dev>
2025-07-17 15:33:24 -0700

    axe global access in react-dismissable-layer

--- packages/react/dismissable-layer/src/dismissable-layer.tsx
+++ packages/react/dismissable-layer/src/dismissable-layer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { composeEventHandlers } from '@radix-ui/primitive';
+import { composeEventHandlers, getOwnerDocument } from '@radix-ui/primitive';
 import { Primitive, dispatchDiscreteCustomEvent } from '@radix-ui/react-primitive';
 import { useComposedRefs } from '@radix-ui/react-compose-refs';
 import { useCallbackRef } from '@radix-ui/react-use-callback-ref';
@@ -71,7 +71,10 @@ const DismissableLayer = React.forwardRef<DismissableLayerElement, DismissableLa
     } = props;
     const context = React.useContext(DismissableLayerContext);
     const [node, setNode] = React.useState<DismissableLayerElement | null>(null);
-    const ownerDocument = node?.ownerDocument ?? globalThis?.document;
+    const getOwnerDocumentImpl = React.useCallback(() => {
+      return getOwnerDocument(node);
+    }, [node]);
+
     const [, force] = React.useState({});
     const composedRefs = useComposedRefs(forwardedRef, (node) => setNode(node));
     const layers = Array.from(context.layers);
@@ -88,7 +91,7 @@ const DismissableLayer = React.forwardRef<DismissableLayerElement, DismissableLa
       onPointerDownOutside?.(event);
       onInteractOutside?.(event);
       if (!event.defaultPrevented) onDismiss?.();
-    }, ownerDocument);
+    }, getOwnerDocumentImpl);
 
     const focusOutside = useFocusOutside((event) => {
       const target = event.target as HTMLElement;
@@ -97,7 +100,7 @@ const DismissableLayer = React.forwardRef<DismissableLayerElement, DismissableLa
       onFocusOutside?.(event);
       onInteractOutside?.(event);
       if (!event.defaultPrevented) onDismiss?.();
-    }, ownerDocument);
+    }, getOwnerDocumentImpl);
 
     useEscapeKeydown((event) => {
       const isHighestLayer = index === context.layers.size - 1;
@@ -107,10 +110,11 @@ const DismissableLayer = React.forwardRef<DismissableLayerElement, DismissableLa
         event.preventDefault();
         onDismiss();
       }
-    }, ownerDocument);
+    }, getOwnerDocumentImpl);
 
     React.useEffect(() => {
       if (!node) return;
+      const ownerDocument = getOwnerDocument(node);
       if (disableOutsidePointerEvents) {
         if (context.layersWithOutsidePointerEventsDisabled.size === 0) {
           originalBodyPointerEvents = ownerDocument.body.style.pointerEvents;
@@ -119,7 +123,7 @@ const DismissableLayer = React.forwardRef<DismissableLayerElement, DismissableLa
         context.layersWithOutsidePointerEventsDisabled.add(node);
       }
       context.layers.add(node);
-      dispatchUpdate();
+      dispatchUpdate(ownerDocument);
       return () => {
         if (
           disableOutsidePointerEvents &&
@@ -128,7 +132,7 @@ const DismissableLayer = React.forwardRef<DismissableLayerElement, DismissableLa
           ownerDocument.body.style.pointerEvents = originalBodyPointerEvents;
         }
       };
-    }, [node, ownerDocument, disableOutsidePointerEvents, context]);
+    }, [node, disableOutsidePointerEvents, context]);
 
     /**
      * We purposefully prevent combining this effect with the `disableOutsidePointerEvents` effect
@@ -139,17 +143,19 @@ const DismissableLayer = React.forwardRef<DismissableLayerElement, DismissableLa
     React.useEffect(() => {
       return () => {
         if (!node) return;
+        const ownerDocument = getOwnerDocument(node);
         context.layers.delete(node);
         context.layersWithOutsidePointerEventsDisabled.delete(node);
-        dispatchUpdate();
+        dispatchUpdate(ownerDocument);
       };
     }, [node, context]);
 
     React.useEffect(() => {
       const handleUpdate = () => force({});
-      document.addEventListener(CONTEXT_UPDATE, handleUpdate);
-      return () => document.removeEventListener(CONTEXT_UPDATE, handleUpdate);
-    }, []);
+      const ownerDocument = getOwnerDocument(node);
+      ownerDocument.addEventListener(CONTEXT_UPDATE, handleUpdate);
+      return () => ownerDocument.removeEventListener(CONTEXT_UPDATE, handleUpdate);
+    }, [node]);
 
     return (
       <Primitive.div
@@ -219,14 +225,15 @@ type FocusOutsideEvent = CustomEvent<{ originalEvent: FocusEvent }>;
  * Returns props to pass to the node we want to check for outside events.
  */
 function usePointerDownOutside(
-  onPointerDownOutside?: (event: PointerDownOutsideEvent) => void,
-  ownerDocument: Document = globalThis?.document
+  onPointerDownOutside: ((event: PointerDownOutsideEvent) => void) | undefined,
+  getOwnerDocument: () => Document
 ) {
   const handlePointerDownOutside = useCallbackRef(onPointerDownOutside) as EventListener;
   const isPointerInsideReactTreeRef = React.useRef(false);
   const handleClickRef = React.useRef(() => {});
 
   React.useEffect(() => {
+    const ownerDocument = getOwnerDocument();
     const handlePointerDown = (event: PointerEvent) => {
       if (event.target && !isPointerInsideReactTreeRef.current) {
         const eventDetail = { originalEvent: event };
@@ -279,15 +286,15 @@ function usePointerDownOutside(
      *   })
      * });
      */
-    const timerId = window.setTimeout(() => {
+    const timerId = setTimeout(() => {
       ownerDocument.addEventListener('pointerdown', handlePointerDown);
     }, 0);
     return () => {
-      window.clearTimeout(timerId);
+      clearTimeout(timerId);
       ownerDocument.removeEventListener('pointerdown', handlePointerDown);
       ownerDocument.removeEventListener('click', handleClickRef.current);
     };
-  }, [ownerDocument, handlePointerDownOutside]);
+  }, [getOwnerDocument, handlePointerDownOutside]);
 
   return {
     // ensures we check React component tree (not just DOM tree)
@@ -300,13 +307,14 @@ function usePointerDownOutside(
  * Returns props to pass to the root (node) of the subtree we want to check.
  */
 function useFocusOutside(
-  onFocusOutside?: (event: FocusOutsideEvent) => void,
-  ownerDocument: Document = globalThis?.document
+  onFocusOutside: ((event: FocusOutsideEvent) => void) | undefined,
+  getOwnerDocument: () => Document
 ) {
   const handleFocusOutside = useCallbackRef(onFocusOutside) as EventListener;
   const isFocusInsideReactTreeRef = React.useRef(false);
 
   React.useEffect(() => {
+    const ownerDocument = getOwnerDocument();
     const handleFocus = (event: FocusEvent) => {
       if (event.target && !isFocusInsideReactTreeRef.current) {
         const eventDetail = { originalEvent: event };
@@ -317,7 +325,7 @@ function useFocusOutside(
     };
     ownerDocument.addEventListener('focusin', handleFocus);
     return () => ownerDocument.removeEventListener('focusin', handleFocus);
-  }, [ownerDocument, handleFocusOutside]);
+  }, [getOwnerDocument, handleFocusOutside]);
 
   return {
     onFocusCapture: () => (isFocusInsideReactTreeRef.current = true),
@@ -325,9 +333,9 @@ function useFocusOutside(
   };
 }
 
-function dispatchUpdate() {
+function dispatchUpdate(ownerDocument: Document) {
   const event = new CustomEvent(CONTEXT_UPDATE);
-  document.dispatchEvent(event);
+  ownerDocument.dispatchEvent(event);
 }
 
 function handleAndDispatchCustomEvent<E extends CustomEvent, OriginalEvent extends Event>(
