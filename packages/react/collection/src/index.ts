aedbdaf1fb382d18377ba98380031c55e887a3c0
Chance Strickland <hi@chance.dev>
2025-10-24 10:27:26 -0700

    fix `unstable_CollectionProps` export

--- packages/react/collection/src/index.ts
+++ packages/react/collection/src/index.ts
@@ -3,4 +3,4 @@ export { createCollection } from './collection-legacy';
 export type { CollectionProps } from './collection-legacy';
 
 export { createCollection as unstable_createCollection } from './collection';
-export type { CollectionProps as unstable_CollectionProps } from './collection-legacy';
+export type { CollectionProps as unstable_CollectionProps } from './collection';
